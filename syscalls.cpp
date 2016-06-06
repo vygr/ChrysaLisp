1722d587388c1097b4a2d39a741f6c1f0d841263
Chris Hinsley <chris.hinsley@gmail.com>
2016-06-06 17:15:01 +0100

    extra values

--- syscalls.cpp
+++ syscalls.cpp
@@ -8,55 +8,59 @@
 #include <sys/stat.h>
 
 int main() {
-	printf("\tsys_exit\tequ\t0x%x\n", SYS_exit);
-	printf("\tsys_read\tequ\t0x%x\n", SYS_read);
-	printf("\tsys_write\tequ\t0x%x\n", SYS_write);
-	printf("\tsys_open\tequ\t0x%x\n", SYS_open);
-	printf("\tsys_close\tequ\t0x%x\n", SYS_close);
-	printf("\tsys_unlink\tequ\t0x%x\n", SYS_unlink);
-	printf("\tsys_ftruncate\tequ\t0x%x\n", SYS_ftruncate);
-	printf("\tsys_stat\tequ\t0x%x\n", SYS_stat);
-	printf("\tsys_mmap\tequ\t0x%x\n", SYS_mmap);
-	printf("\tsys_munmap\tequ\t0x%x\n", SYS_munmap);
-	printf("\tsys_mprotect\tequ\t0x%x\n", SYS_mprotect);
-	printf("\tsys_gettimeofday\tequ\t0x%x\n\n", SYS_gettimeofday);
+	printf("\tsys_exit\tequ\t0x%x\n", sys_exit);
+	printf("\tsys_read\tequ\t0x%x\n", sys_read);
+	printf("\tsys_write\tequ\t0x%x\n", sys_write);
+	printf("\tsys_open\tequ\t0x%x\n", sys_open);
+	printf("\tsys_close\tequ\t0x%x\n", sys_close);
+	printf("\tsys_unlink\tequ\t0x%x\n", sys_unlink);
+	printf("\tsys_ftruncate\tequ\t0x%x\n", sys_ftruncate);
+	printf("\tsys_stat\tequ\t0x%x\n", sys_stat);
+	printf("\tsys_mmap\tequ\t0x%x\n", sys_mmap);
+	printf("\tsys_munmap\tequ\t0x%x\n", sys_munmap);
+	printf("\tsys_mprotect\tequ\t0x%x\n", sys_mprotect);
+	printf("\tsys_gettimeofday\tequ\t0x%x\n\n", sys_gettimeofday);
 
-	printf("\tprot_none\tequ\t0x%x\n", prot_none);
-	printf("\tprot_read\tequ\t0x%x\n", prot_read);
-	printf("\tprot_write\tequ\t0x%x\n", prot_write);
-	printf("\tprot_exec\tequ\t0x%x\n\n", prot_exec);
+	printf("\tprot_none\tequ\t0x%x\n", PROT_NONE);
+	printf("\tprot_read\tequ\t0x%x\n", PROT_READ);
+	printf("\tprot_write\tequ\t0x%x\n", PROT_WRITE);
+	printf("\tprot_exec\tequ\t0x%x\n\n", PROT_EXEC);
 
-	printf("\tmap_shared\tequ\t0x%x\n", map_shared);
-	printf("\tmap_private\tequ\t0x%x\n", map_private);
-	printf("\tmap_fixed\tequ\t0x%x\n", map_fixed);
-	printf("\tmap_anon\tequ\t0x%x\n\n", map_anon);
+	printf("\tmap_shared\tequ\t0x%x\n", MAP_SHARED);
+	printf("\tmap_private\tequ\t0x%x\n", MAP_PRIVATE);
+	printf("\tmap_fixed\tequ\t0x%x\n", MAP_FIXED);
+	printf("\tmap_anon\tequ\t0x%x\n\n", MAP_ANON);
 
-	printf("\to_rdonly\tequ\t0x%x\n", o_rdonly);
-	printf("\to_wronly\tequ\t0x%x\n", o_wronly);
-	printf("\to_rdwr\tequ\t0x%x\n", o_rdwr);
-	printf("\to_trunc\tequ\t0x%x\n", o_trunc);
-	printf("\to_append\tequ\t0x%x\n", o_append);
-	printf("\to_nonblock\tequ\t0x%x\n", o_nonblock);
-	printf("\to_creat\tequ\t0x%x\n", o_creat);
-	printf("\to_excl\tequ\t0x%x\n", o_excl);
-	printf("\to_nofollow\tequ\t0x%x\n", o_nofollow);
-	printf("\to_cloexec\tequ\t0x%x\n\n", o_cloexec);
+	printf("\to_rdonly\tequ\t0x%x\n", O_RDONLY);
+	printf("\to_wronly\tequ\t0x%x\n", O_WRONLY);
+	printf("\to_rdwr\tequ\t0x%x\n", O_RDWR);
+	printf("\to_trunc\tequ\t0x%x\n", O_TRUNC);
+	printf("\to_append\tequ\t0x%x\n", O_APPEND);
+	printf("\to_nonblock\tequ\t0x%x\n", O_NONBLOCK);
+	printf("\to_creat\tequ\t0x%x\n", O_CREAT);
+	printf("\to_excl\tequ\t0x%x\n", O_EXCL);
+	printf("\to_nofollow\tequ\t0x%x\n", O_NOFOLLOW);
+	printf("\to_cloexec\tequ\t0x%x\n\n", O_CLOEXEC);
 
-	printf("\ts_irwxu\tequ\t0x%x\n", s_irwxu);
-	printf("\ts_irusr\tequ\t0x%x\n", s_irusr);
-	printf("\ts_iwusr\tequ\t0x%x\n", s_iwusr);
-	printf("\ts_ixusr\tequ\t0x%x\n", s_ixusr);
-	printf("\ts_irwxg\tequ\t0x%x\n", s_irwxg);
-	printf("\ts_irgrp\tequ\t0x%x\n", s_irgrp);
-	printf("\ts_iwgrp\tequ\t0x%x\n", s_iwgrp);
-	printf("\ts_ixgrp\tequ\t0x%x\n", s_ixgrp);
-	printf("\ts_irwxo\tequ\t0x%x\n", s_irwxo);
-	printf("\ts_iroth\tequ\t0x%x\n", s_iroth);
-	printf("\ts_iwoth\tequ\t0x%x\n", s_iwoth);
-	printf("\ts_ixoth\tequ\t0x%x\n", s_ixoth);
-	printf("\ts_isuid\tequ\t0x%x\n", s_isuid);
-	printf("\ts_isgid\tequ\t0x%x\n", s_isgid);
-	printf("\ts_isvtx\tequ\t0x%x\n\n", s_isvtx);
+	printf("\ts_irwxu\tequ\t0x%x\n", S_IRWXU);
+	printf("\ts_irusr\tequ\t0x%x\n", S_IRUSR);
+	printf("\ts_iwusr\tequ\t0x%x\n", S_IWUSR);
+	printf("\ts_ixusr\tequ\t0x%x\n", S_IXUSR);
+	printf("\ts_irwxg\tequ\t0x%x\n", S_IRWXG);
+	printf("\ts_irgrp\tequ\t0x%x\n", S_IRGRP);
+	printf("\ts_iwgrp\tequ\t0x%x\n", S_IWGRP);
+	printf("\ts_ixgrp\tequ\t0x%x\n", S_IXGRP);
+	printf("\ts_irwxo\tequ\t0x%x\n", S_IRWXO);
+	printf("\ts_iroth\tequ\t0x%x\n", S_IROTH);
+	printf("\ts_iwoth\tequ\t0x%x\n", S_IWOTH);
+	printf("\ts_ixoth\tequ\t0x%x\n", S_IXOTH);
+	printf("\ts_isuid\tequ\t0x%x\n", S_ISUID);
+	printf("\ts_isgid\tequ\t0x%x\n", S_ISGID);
+	printf("\ts_isvtx\tequ\t0x%x\n\n", S_ISVTX);
+
+	printf("\ts_ifmt\tequ\t0x%x\n", S_IFMT);
+	printf("\ts_ifdir\tequ\t0x%x\n", S_IFDIR);
+	printf("\ts_ifreg\tequ\t0x%x\n\n", S_IFREG);
 
 	printf("\tstat_dev\tequ\t0x%lx\n", offsetof(struct stat, st_dev));
 	printf("\tstat_mode\tequ\t0x%lx\n", offsetof(struct stat, st_mode));
