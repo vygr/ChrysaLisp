b5c48817897cd19963e361aedb682a4295e7a8da
Andy Stout <astout@baydrift.com>
2016-12-14 09:25:38 +0000

    Report how many link files are being removed

--- stop.sh
+++ stop.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-killall main -KILL
+killall main -KILL --quiet
 
-#clear old link files
+# report how many link files are being removed
+tfiles=`ls -lU /tmp/???-??? 2> /dev/null | wc -l`
+echo "Cleaning up" $tfiles "link files"
+
+# quietly delete old link files
 rm -f /tmp/???-???
