12efadc057a001b9caec334e069a5fac67e7131a
Andy Stout <astout@baydrift.com>
2016-12-21 22:34:57 +0000

    Change the location of the shared memory link files to be user specfic. Having them in /tmp/ was a problem for multi-user systems

--- stop.sh
+++ stop.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 killall main -KILL > /dev/null 2>&1
 
+export TEMP="./tmp/"
+mkdir -p $TEMP
+
 # report how many link files are being removed
-tfiles=`ls -lU /tmp/???-??? 2> /dev/null | wc -l`
+tfiles=`ls -lU $TEMP/???-??? 2> /dev/null | wc -l`
 echo "Cleaning up" $tfiles "link files"
 
 # quietly delete old link files
-rm -f /tmp/???-???
+rm -f $TEMP/???-???
