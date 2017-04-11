f607d82a78c28ee8d32274de479085d9b97b497e
Chris Hinsley <chris.hinsley@gmail.com>
2017-04-11 22:53:23 +0100

    each! with sliceing option

--- cmd/lisp.lisp
+++ cmd/lisp.lisp
@@ -93,27 +93,27 @@
 		(_f (elem _ _l))))
 
 (defun each-mergeable-rev (_f _l)
-	(each! nil _f progn (list _l)))
+	(each! nil nil nil _f progn (list _l)))
 
 (defun each (_f &rest _b)
-	(each! t _f progn _b))
+	(each! t nil nil _f progn _b))
 
 (defun each-rev (_f &rest _b)
-	(each! nil _f progn _b))
+	(each! nil nil nil _f progn _b))
 
 (defun map (_f &rest _b)
 	(defq _l (list))
-	(each! t _f (lambda (_)
+	(each! t nil nil _f (lambda (_)
 		(push _l _)) _b) _l)
 
 (defun map-rev (_f &rest _b)
 	(defq _l (list))
-	(each! nil _f (lambda (_)
+	(each! nil nil nil _f (lambda (_)
 		(push _l _)) _b) _l)
 
 (defun filter (_f _b)
 	(defq _l (list))
-	(each! t _f (lambda (_p)
+	(each! t nil nil _f (lambda (_p)
 		(if _p (push _l (elem _ _b)))) (list _b)) _l)
 
 (defun reduce (_f _l &optional _a)
