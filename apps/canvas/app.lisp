2482a800517c0c66ad339336cddc38de97daca78
Chris Hinsley <christopher.hinsley@prometheanworld.com>
2017-05-12 10:34:51 +0100

    support for odd-even and non-zero winding rules

--- apps/canvas/app.lisp
+++ apps/canvas/app.lisp
@@ -17,7 +17,7 @@
 (defun set-pen-col (_) (setq pen-col _))
 (defun set-brush-col (_) (setq brush-col _))
 
-(defun fpoly (_)
+(defun fpoly (m _)
 	(defq e (list) ys max-long ye min-long)
 	(each (lambda (_)
 		(setq _ (map (lambda (_) (vec-scale-2d _ canvas_scale)) _))
@@ -27,10 +27,10 @@
 			(cond
 				((lt y1 y2)
 					(setq ys (min ys y1) ye (max ye y2))
-					(push e (list x1 y1 y2 (div (sub x2 x1) (sub y2 y1)))))
+					(push e (list x1 y1 y2 (div (sub x2 x1) (sub y2 y1)) 1)))
 				((gt y1 y2)
 					(setq ys (min ys y2) ye (max ye y1))
-					(push e (list x2 y2 y1 (div (sub x1 x2) (sub y1 y2))))))
+					(push e (list x2 y2 y1 (div (sub x1 x2) (sub y1 y2)) -1))))
 			p2) _ (elem -2 _))) _)
 	(sort (lambda (e1 e2)
 		(lt (elem 1 e1) (elem 1 e2))) e)
@@ -44,14 +44,26 @@
 			(lt (elem 0 e1) (elem 0 e2))) e i j)
 		(while (and (ne i (length e)) (eq (elem 0 (elem i e)) min-long))
 			(setq i (inc i)))
-		(defq k (sub i 2))
-		(while (ne (setq k (add k 2)) j)
-			(defq e1 (elem k e) e2 (elem (inc k) e)
-				x1 (elem 0 e1) x2 (elem 0 e2))
-			(elem-set 0 e1 (add x1 (elem 3 e1)))
-			(elem-set 0 e2 (add x2 (elem 3 e2)))
-			(setq x1 (bit-asr x1 fp-shift) x2 (bit-asr x2 fp-shift))
-			(call canvas-set-hline canvas brush-col x1 ys (sub x2 x1)))))
+		(cond
+			(m ;non-zero winding
+				(defq k (dec i))
+				(while (ne (setq k (inc k)) j)
+					(defq e1 (elem k e) x1 (elem 0 e1) w (elem 4 e1))
+					(elem-set 0 e1 (add x1 (elem 3 e1)))
+					(while (ne w 0)
+						(defq k (inc k) e2 (elem k e) x2 (elem 0 e2) w (add w (elem 4 e2)))
+						(elem-set 0 e2 (add x2 (elem 3 e2))))
+					(setq x1 (bit-asr x1 fp-shift) x2 (bit-asr x2 fp-shift))
+					(call canvas-set-hline canvas brush-col x1 ys (sub x2 x1))))
+			(t ;odd-even winding
+				(defq k (sub i 2))
+				(while (ne (setq k (add k 2)) j)
+					(defq e1 (elem k e) e2 (elem (inc k) e)
+						x1 (elem 0 e1) x2 (elem 0 e2))
+					(elem-set 0 e1 (add x1 (elem 3 e1)))
+					(elem-set 0 e2 (add x2 (elem 3 e2)))
+					(setq x1 (bit-asr x1 fp-shift) x2 (bit-asr x2 fp-shift))
+					(call canvas-set-hline canvas brush-col x1 ys (sub x2 x1)))))))
 
 (call canvas-fill canvas 0xff202020)
 (call canvas-set-fbox canvas 0xffffffff
@@ -59,7 +71,7 @@
 	(div (fmul canvas_width canvas_scale 0.5) 1.0) (div (fmul canvas_height canvas_scale 0.5) 1.0))
 
 (set-brush-col 0xff0000ff)
-(fpoly (list (list
+(fpoly nil (list (list
 	(list 0 0)
 	(list (fmul canvas_width 0.25) canvas_height)
 	(list (fmul canvas_width 0.5) 0)
@@ -68,7 +80,7 @@
 	(list (fmul canvas_width 0x0.1) canvas_height))))
 
 (set-brush-col 0xff00ff00)
-(fpoly
+(fpoly nil
 	(stroke-polyline-2d
 		(list)
 		(fmul canvas_width 0x0.1)
@@ -81,7 +93,7 @@
 			(list (sub canvas_width (fmul canvas_width 0o0.1)) (sub canvas_height (fmul canvas_height 0o0.1)))))))
 
 (set-brush-col 0xff00ffff)
-(fpoly
+(fpoly t
 	(stroke-polygon-2d
 		(list)
 		(fmul canvas_width 0.01)
@@ -100,7 +112,7 @@
 				(list (sub canvas_width (fmul canvas_width 0.1)) (fmul canvas_height 0.1)))))))
 
 (set-brush-col 0xffff0000)
-(fpoly
+(fpoly nil
 	(stroke-polygon-2d
 		(list)
 		(fmul canvas_width 0.025)
