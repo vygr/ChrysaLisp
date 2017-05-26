e4da456f9c825dae3b4481cc25b98d2de01eeb33
Chris Hinsley <chris.hinsley@gmail.com>
2017-05-26 22:41:47 +0100

    nearly all in native, couple of miter join bugs to fix...

--- apps/canvas/app.lisp
+++ apps/canvas/app.lisp
@@ -1,6 +1,6 @@
 ;import canvas class method slots
 (defq slot_set_fbox nil slot_set_fpoly nil slot_blend_fpoly nil slot_fill nil slot_swap nil
-	slot_gen_bezier nil slot_gen_arc nil)
+	slot_gen_bezier nil slot_gen_arc nil slot_stroke_polylines nil slot_stroke_polygons nil)
 (within-compile-env (lambda ()
 	(import 'class/canvas/canvas.inc)
 	(import 'class/points/points.inc)
@@ -9,6 +9,8 @@
 		slot_blend_fpoly (method-slot 'canvas 'blend_fpoly)
 		slot_fill (method-slot 'canvas 'fill)
 		slot_swap (method-slot 'canvas 'swap)
+		slot_stroke_polylines (method-slot 'points 'stroke_polylines)
+		slot_stroke_polygons (method-slot 'points 'stroke_polygons)
 		slot_gen_bezier (method-slot 'points 'gen_bezier)
 		slot_gen_arc (method-slot 'points 'gen_arc))))
 
@@ -17,70 +19,69 @@
 
 (defq canvas_scale (pop argv) canvas_height (pop argv) canvas_width (pop argv) canvas (pop argv))
 
-(defun fpoly (col m _)
-	(defq polygons (list))
+(defun as-points (_)
+	(defq polygon (points))
 	(each (lambda (_)
-		(defq polygon (array))
-		(each (lambda (_)
-			(setq _ (vec-scale-2d _ canvas_scale))
-			(push polygon (bit-or (bit-shl (elem 1 _) 32) (bit-and 0xffffffff (elem 0 _))))) _)
-		(push polygons polygon)) _)
-	(call slot_set_fpoly canvas polygons col m))
+		(push polygon (bit-or (bit-shl (elem 1 _) 32) (bit-and 0xffffffff (elem 0 _))))) _) polygon)
 
-(defun bpoly (col m _)
-	(defq polygons (list))
+(defun scale-points (_)
+	(defq polygon (points))
 	(each (lambda (_)
-		(defq polygon (array))
-		(each (lambda (_)
-			(setq _ (vec-scale-2d _ canvas_scale))
-			(push polygon (bit-or (bit-shl (elem 1 _) 32) (bit-and 0xffffffff (elem 0 _))))) _)
-		(push polygons polygon)) _)
-	(call slot_blend_fpoly canvas polygons col m))
+		(push polygon (bit-or (bit-shl (fmul (bit-asr _ 32) canvas_scale) 32)
+			(bit-and 0xffffffff (fmul (bit-asr (bit-shl _ 32) 32) canvas_scale))))) _) polygon)
+
+(defun fpoly (col mode _)
+	(call slot_set_fpoly canvas (map scale-points _) col mode))
+
+(defun bpoly (col mode _)
+	(call slot_blend_fpoly canvas (map scale-points _) col mode))
 
 (call slot_fill canvas 0x00000000)
 (call slot_set_fbox canvas 0xffffffff
 	(div (fmul canvas_width canvas_scale 0.1) 1.0) (div (fmul canvas_height canvas_scale 0.05) 1.0)
 	(div (fmul canvas_width canvas_scale 0.5) 1.0) (div (fmul canvas_height canvas_scale 0.5) 1.0))
 
-(fpoly 0xff0000ff 0 (list (list
+(fpoly 0xff0000ff 0 (list (as-points (list
 	(list 0 0)
 	(list (fmul canvas_width 0.25) canvas_height)
 	(list (fmul canvas_width 0.5) 0)
 	(list (add (fmul canvas_width 0.5) (fmul canvas_width 0.25)) canvas_height)
 	(list canvas_width 0)
-	(list (fmul canvas_width 0x0.1) canvas_height))))
+	(list (fmul canvas_width 0x0.1) canvas_height)))))
 
-(bpoly 0xc000ff00 0
-	(stroke-polyline-2d
-		(list)
-		(fmul canvas_width 0x0.1)
+(bpoly 0xc000ff00 1
+	(call slot_stroke_polylines (list)
+		(list (as-points (list
+			(list (fmul canvas_width 0o0.1) (fmul canvas_height 0o0.1))
+			(list (sub canvas_width (fmul canvas_width 0.25)) (fmul canvas_height 0.166))
+			(list (sub canvas_width (fmul canvas_width 0o0.1)) (sub canvas_height (fmul canvas_height 0o0.1))))))
+		(array)
 		join-round
 		cap-round
 		cap-round
-		(list (list
-			(list (fmul canvas_width 0o0.1) (fmul canvas_height 0o0.1))
-			(list (sub canvas_width (fmul canvas_width 0.25)) (fmul canvas_height 0.166))
-			(list (sub canvas_width (fmul canvas_width 0o0.1)) (sub canvas_height (fmul canvas_height 0o0.1)))))))
+		(fmul canvas_width 0x0.1)
+		2.0))
 
+(when nil
 (fpoly 0xff00ffff 1
-	(stroke-polygon-2d
-		(list)
-		(fmul canvas_width 0.01)
-		join-miter
-		(stroke-polyline-2d
-			(list)
-			(fmul canvas_width 0.033)
+	(call slot_stroke_polygons (list)
+		(call slot_stroke_polylines (list)
+			(list (call slot_gen_bezier (points) (array)
+				(fmul canvas_width 0x0.1) (sub canvas_height (fmul canvas_height 0x0.1))
+				(fmul canvas_width 0o0.1) (fmul canvas_height 0x0.1)
+				(fmul canvas_width 0.25) (fmul canvas_height 0.33)
+				(sub canvas_width (fmul canvas_width 0.1)) (fmul canvas_height 0.1)
+				10.0))
+			(array)
 			join-bevel
-			cap-round
-			cap-arrow
-			(list (map (lambda (_)
-				(list (bit-asr (bit-shl _ 32) 32) (bit-asr _ 32)))
-				(call slot_gen_bezier (points) (array)
-					(fmul canvas_width 0x0.1) (sub canvas_height (fmul canvas_height 0x0.1))
-					(fmul canvas_width 0o0.1) (fmul canvas_height 0x0.1)
-					(fmul canvas_width 0.25) (fmul canvas_height 0.33)
-					(sub canvas_width (fmul canvas_width 0.1)) (fmul canvas_height 0.1)
-					2.0))))))
+			cap-butt
+			cap-butt
+			(fmul canvas_width 0.033)
+			2.0)
+		(array)
+		join-bevel
+		(fmul canvas_width 0.01)
+		2.0))
 
 (bpoly 0xc0ff0000 0
 	(stroke-polygon-2d
@@ -106,5 +107,5 @@
 						(add (fmul canvas_width 0.33) (fmul canvas_width 0x0.1))
 						(add (fmul canvas_height 0.5) (fmul canvas_height 0o0.1))
 						4.0 2.0 (fmul canvas_width 0o0.1) 2.0))))))
-
+)
 (call slot_swap canvas)
