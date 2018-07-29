61aee39d62ce2283c74042e71a27c76264ceba59
Chris Hinsley <chris.hinsley@gmail.com>
2018-07-29 12:15:34 +0100

    enum events

--- apps/calculator/app.lisp
+++ apps/calculator/app.lisp
@@ -2,7 +2,11 @@
 (run 'apps/sys.inc)
 (run 'apps/ui.inc)
 
-(defq win_close 0 win_min 1 win_max 2 win_button 3)
+(structure 'event 0
+	(byte 'win_close)
+	(byte 'win_min)
+	(byte 'win_max)
+	(byte 'win_button))
 
 (ui-tree window (create-window (add window_flag_close window_flag_min window_flag_max)) nil
 	(ui-element _ (create-flow) ('flow_flags (bit-or flow_flag_down flow_flag_fillw flow_flag_lasth))
@@ -11,12 +15,12 @@
 			(each (lambda (text)
 				(slot connect_click
 					(ui-element _ (create-button) ('text (if (eql text "C") "AC" text)))
-					win_button)) "789/456*123-0=C+"))))
+					event_win_button)) "789/456*123-0=C+"))))
 
 (slot set_title window "Calculator")
-(slot connect_close window win_close)
-(slot connect_min window win_min)
-(slot connect_max window win_max)
+(slot connect_close window event_win_close)
+(slot connect_min window event_win_min)
+(slot connect_max window event_win_max)
 (bind '(w h) (slot pref_size window))
 (slot change window 920 48 w h)
 (slot gui_add window)
@@ -36,7 +40,7 @@
 (defq id t accum 0 value 0 num 0 lastop nil)
 (while id
 	(cond
-		((ge (setq id (read-long ev_msg_target_id (defq msg (mail-mymail)))) win_button)
+		((ge (setq id (read-long ev_msg_target_id (defq msg (mail-mymail)))) event_win_button)
 			(defq op (get (slot find_id window (read-long ev_msg_action_source_id msg)) 'text))
 			(cond
 				((eql op "AC")
@@ -56,17 +60,17 @@
 			(set display 'text (str value))
 			(slot layout display)
 			(slot dirty display))
-		((eq id win_close)
+		((eq id event_win_close)
 			;close button
 			(setq id nil))
-		((eq id win_min)
+		((eq id event_win_min)
 			;min button
 			(slot dirty window)
 			(bind '(x y _ _) (slot get_bounds window))
 			(bind '(w h) (slot pref_size window))
 			(slot change window x y w h)
 			(slot dirty_all window))
-		((eq id win_max)
+		((eq id event_win_max)
 			;max button
 			(slot dirty window)
 			(bind '(x y _ _) (slot get_bounds window))
