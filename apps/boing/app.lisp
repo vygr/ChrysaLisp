a04b77078a9659d2e7c3e82f5a7087019db0baf8
Chris Hinsley <chris.hinsley@gmail.com>
2018-08-06 21:33:18 +0100

    custom backdrop class
    Uses no memory for a canvas !

--- apps/boing/app.lisp
+++ apps/boing/app.lisp
@@ -2,30 +2,26 @@
 (run 'sys/lisp.inc)
 (run 'gui/lisp.inc)
 
-(structure 'event 0
-	(byte 'win_close))
+(ffi create-backdrop "apps/boing/backdrop/lisp_create" 0)
 
-(defq id t index 0 canvas_width 640 canvas_height 480 frames (map (lambda (_)
+(structure 'event 0
+	(byte 'win_close)
+	(byte 'win_min)
+	(byte 'win_max))
+
+(defq id t index 0 frames (map (lambda (_)
 	(canvas-load (cat "apps/boing/taoball_" (str _) ".cpm"))) (range 1 12)))
 
-(ui-tree window (create-window window_flag_close) nil
-	(ui-element canvas (create-canvas canvas_width canvas_height 1.0) nil
+(ui-tree window (create-window (add window_flag_close window_flag_min window_flag_max)) nil
+	(ui-element backdrop (create-backdrop) ('color 0xff000000)
 		(ui-element frame (elem 0 frames))))
-
-(canvas-fill canvas 0xff000000)
-(defq x 0 y 0 w 32)
-(while (lt x canvas_width)
-	(canvas-set-fbox canvas 0xffffffff x 0 1 canvas_height)
-	(setq x (add x w)))
-(while (lt y canvas_height)
-	(canvas-set-fbox canvas 0xffffffff 0 y canvas_width 1)
-	(setq y (add y w)))
 
 (window-set-title window "Boing")
 (window-connect-close window event_win_close)
+(window-connect-min window event_win_min)
+(window-connect-max window event_win_max)
 (bind '(w h) (view-pref-size window))
-(view-change window 512 256 w h)
-(view-opaque canvas)
+(view-change window 64 64 w h)
 (gui-add window)
 
 (bind '(w h) (view-pref-size frame))
@@ -33,20 +29,35 @@
 (defq xv 4 yv 0)
 
 (while id
+	(bind '(_ _ backdrop_width backdrop_height) (view-get-bounds backdrop))
 	(task-sleep 40000)
 	(defq index (mod (inc index) (length frames)) old_frame frame frame (elem index frames))
 	(bind '(x y w h) (view-get-bounds old_frame))
-	(view-add-dirty canvas x y w h)
+	(view-add-dirty backdrop x y w h)
 	(setq x (add x xv) y (add y yv) yv (inc yv))
-	(if (gt y (sub canvas_height h)) (setq y (sub canvas_height h) yv -22))
+	(if (gt y (sub backdrop_height h)) (setq y (sub backdrop_height h) yv -22))
 	(if (lt x 0) (setq x 0 xv (abs xv)))
-	(if (gt x (sub canvas_width w)) (setq x (sub canvas_width w) xv (neg (abs xv))))
+	(if (gt x (sub backdrop_width w)) (setq x (sub backdrop_width w) xv (neg (abs xv))))
 	(view-set-bounds frame x y w h)
 	(view-sub old_frame)
-	(view-add-back canvas frame)
+	(view-add-back backdrop frame)
 	(view-dirty frame)
 	(while (defq msg (mail-trymail))
 		(cond
 			((eq (setq id (read-long ev_msg_target_id msg)) event_win_close)
 				(setq id nil))
+			((eq id event_win_min)
+				;min button
+				(view-dirty window)
+				(bind '(x y _ _) (view-get-bounds window))
+				(bind '(w h) (view-pref-size window))
+				(view-change window x y w h)
+				(view-dirty-all window))
+			((eq id event_win_max)
+				;max button
+				(view-dirty window)
+				(bind '(x y _ _) (view-get-bounds window))
+				(bind '(w h) (view-pref-size window))
+				(view-change window x y (fmul w 1.5) (fmul h 1.5))
+				(view-dirty-all window))
 			(t (view-event window msg)))))
