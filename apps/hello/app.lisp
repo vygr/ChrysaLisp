5464a0eadb6b16816dd4ba0b4d8f7516d20b54a7
Devine Lu Linvega <aliceffekt@gmail.com>
2018-10-02 07:09:28 +1200

    Fixed errors

--- apps/hello/app.lisp
+++ apps/hello/app.lisp
@@ -1,30 +1,29 @@
-; the upmost basic requirements to an app.
-; import settings
+;import settings
 (run 'sys/lisp.inc)
 (run 'gui/lisp.inc)
 
-; add close event
+;add close event
 (structure 'event 0
 	(byte 'win_close))
 
-; add window id
+;add event id
 (defq id t)
 
-; create a window with a label
+;create a window with a label
 (ui-tree window (create-window window_flag_close) nil
 	(ui-element display (create-label) ('text "ChrysaLisp" 'color argb_white 'flow_flags flow_flag_align_hleft 'font (create-font "fonts/OpenSans-Regular.ttf" 18))))
 
-; set a name to the window
+;set a name to the window
 (window-set-title window "Hello")
 
-; bind close event
+;bind close event
 (window-connect-close window event_win_close)
 
-; window should be at last 200 wide
+;window with a 200w wide padding
 (bind '(w h) (view-pref-size window))
 (gui-add (view-change window 256 128 (add w 200) h))
 
-; main app loop
+;main app loop
 (while id
 	(cond
 		((eq (setq id (read-long ev_msg_target_id (defq msg (mail-mymail)))) event_win_close)
