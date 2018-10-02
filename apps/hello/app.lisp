6b7840ed9e6b7d26c80412575b098d91da5be3b5
Devine Lu Linvega <aliceffekt@gmail.com>
2018-10-03 09:50:35 +1200

    Clicking button now changes color.

--- apps/hello/app.lisp
+++ apps/hello/app.lisp
@@ -4,21 +4,24 @@
 
 ;add close event
 (structure 'event 0
-	(byte 'win_close))
+  (byte 'win_close))
+(structure 'event 1
+  (byte 'click))
 
 ;add event id
 (defq id t)
 
 ;create a window with a label
 (ui-tree window (create-window window_flag_close) nil	
-  (ui-element display (create-button) ('text "Press Me" 'text_color argb_blue 'color argb_green 'flow_flags flow_flag_align_hleft 'font (create-font "fonts/OpenSans-Regular.ttf" 18)))
+  (ui-element button (create-button) ('text "Press Me" 'text_color argb_blue 'color argb_green 'flow_flags flow_flag_align_hleft 'font (create-font "fonts/OpenSans-Regular.ttf" 18)))
   (ui-element display (create-label) ('text "ChrysaLisp" 'color argb_white 'flow_flags flow_flag_align_hleft 'font (create-font "fonts/OpenSans-Regular.ttf" 18))))
 
 ;set a name to the window
 (window-set-title window "Hello")
 
-;bind close event
+;bind events
 (window-connect-close window event_win_close)
+(button-connect-click button event_click) 
 
 ;window with a 200w wide padding
 (bind '(w h) (view-pref-size window))
@@ -29,4 +32,7 @@
 	(cond
 		((eq (setq id (read-long ev_msg_target_id (defq msg (mail-mymail)))) event_win_close)
 			(setq id nil))
+    ((eq id event_click)
+      (set button 'color argb_red)
+      (view-dirty button))
 		(t (view-event window msg))))
