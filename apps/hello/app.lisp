c31a6e7338201b6364669b266cde908b1fd0da3c
Лu Лinveгa <aliceffekt@gmail.com>
2018-10-05 05:55:16 +1200

    Added a button to example (#20)
    * Fixed errors
    
    * Added a button to the example application
    
    * Clicking button now changes color.
    
    * Make the label take the full width

--- apps/hello/app.lisp
+++ apps/hello/app.lisp
@@ -1,32 +1,40 @@
-; the upmost basic requirements to an app.
-; import settings
+;import settings
 (run 'sys/lisp.inc)
 (run 'gui/lisp.inc)
 
-; add close event
+;add close event
 (structure 'event 0
-	(byte 'win_close))
+  (byte 'win_close))
+(structure 'event 1
+  (byte 'click))
 
-; add window id
+;add event id
 (defq id t)
 
-; create a window with a label
-(ui-tree window (create-window window_flag_close) nil
-	(ui-element display (create-label) ('text "ChrysaLisp" 'color argb_white 'flow_flags flow_flag_align_hleft 'font (create-font "fonts/OpenSans-Regular.ttf" 18))))
+;create a window with a label
+(ui-tree window (create-window window_flag_close) nil	
+  (ui-element button (create-button) ('text "Press Me" 'text_color argb_blue 'color argb_green 'flow_flags flow_flag_fillw  'font (create-font "fonts/OpenSans-Regular.ttf" 18)))
+  (ui-element label (create-label) ('text "ChrysaLisp" 'color argb_white 'flow_flags flow_flag_fillw 'font (create-font "fonts/OpenSans-Regular.ttf" 18))))
 
-; set a name to the window
+;set a name to the window
 (window-set-title window "Hello")
 
-; bind close event
+;bind events
 (window-connect-close window event_win_close)
+(button-connect-click button event_click) 
 
-; window should be at last 200 wide
+;window with a 200w wide padding
 (bind '(w h) (view-pref-size window))
 (gui-add (view-change window 256 128 (add w 200) h))
 
-; main app loop
+;main app loop
 (while id
 	(cond
 		((eq (setq id (read-long ev_msg_target_id (defq msg (mail-mymail)))) event_win_close)
 			(setq id nil))
+    ((eq id event_click)
+      (set button 'color argb_red)
+      (view-dirty button)
+      (set label 'text "The label is red!")
+      (view-dirty label))
 		(t (view-event window msg))))
