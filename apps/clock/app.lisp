611ea33a68abbf3c19c800b85ce9ac9480001d4d
Chris Hinsley <chris.hinsley@gmail.com>
2018-10-06 10:09:04 +0100

    ticking clock

--- apps/clock/app.lisp
+++ apps/clock/app.lisp
@@ -5,18 +5,13 @@
 ;add event id
 (defq id t)
 
-(defun make_time ()
-	(defq sec (div (time) 1000000) seconds (mod sec 60)
-		minutes (mod (div sec 60) 60) hours (mod (div sec 60 60) 24))
-	(cat (pad hours 2 "0") ":" (pad minutes 2 "0") ":" (pad seconds 2 "0")))
-
 ;define events we will use
 (structure 'event 0
 	(byte 'close))
 
 ;create a window with a label
 (ui-tree window (create-window window_flag_close) nil
-	(ui-element label (create-label) ('text (make_time) 'color argb_yellow 'flow_flags flow_flag_fillw
+	(ui-element display (create-label) ('text "00:00:00" 'color argb_yellow 'flow_flags flow_flag_fillw
 		'font (create-font "fonts/Hack-Regular.ttf" 32))))
 
 ;set a name to the window
@@ -29,9 +24,13 @@
 (bind '(w h) (view-pref-size window))
 (gui-add (view-change window 290 16 w h))
 
+;create child and send args
+(mail-send (list display) (defq child_id (open-child "apps/clock/child.lisp" kn_call_open)))
+
 ;main app loop
 (while id
 	(cond
 		((eq (setq id (read-long ev_msg_target_id (defq msg (mail-mymail)))) event_close)
+			(mail-send "" child_id)
 			(setq id nil))
 		(t (view-event window msg))))
---
+++ apps/clock/child.lisp
@@ -0,0 +1,16 @@
+;import settings
+(run 'sys/lisp.inc)
+(run 'gui/lisp.inc)
+
+(defun make-time ()
+	(defq sec (div (time) 1000000) seconds (mod sec 60)
+		minutes (mod (div sec 60) 60) hours (mod (div sec 60 60) 24))
+	(cat (pad hours 2 "0") ":" (pad minutes 2 "0") ":" (pad seconds 2 "0")))
+
+;read args from parent
+(bind '(display) (mail-mymail))
+
+;while not told to quit
+(until (mail-trymail)
+	(set (view-dirty display) 'text (make-time))
+	(task-sleep 1000000))
