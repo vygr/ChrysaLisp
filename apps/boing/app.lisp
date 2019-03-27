ecffe6e7921c77419e93451b04cc74485f136dfc
Chris Hinsley <chris.hinsley@gmail.com>
2019-03-27 15:20:58 +0000

    working shadow

--- apps/boing/app.lisp
+++ apps/boing/app.lisp
@@ -9,11 +9,13 @@
 	(byte 'win_min)
 	(byte 'win_max))
 
-(defq id t index 0 xv 4 yv 0 frames (map (lambda (_)
-	(canvas-load (cat "apps/boing/taoball_" (str _) ".cpm") load_flag_shared)) (range 1 12)))
+(defq id t index 0 xv 4 yv 0
+	frames (map (lambda (_) (canvas-load (cat "apps/boing/taoball_" (str _) ".cpm") load_flag_shared)) (range 1 12))
+	sframes (map (lambda (_) (canvas-load (cat "apps/boing/taoball_s_" (str _) ".cpm") load_flag_shared)) (range 1 12)))
 
 (ui-tree window (create-window (add window_flag_close window_flag_min window_flag_max)) nil
 	(ui-element backdrop (create-backdrop) ('color argb_black 'ink_color argb_white)
+		(ui-element sframe (elem 0 sframes))
 		(ui-element frame (elem 0 frames))))
 
 (window-set-title window "Boing")
@@ -26,16 +28,22 @@
 (while id
 	(bind '(_ _ backdrop_width backdrop_height) (view-get-bounds backdrop))
 	(task-sleep 40000)
-	(defq index (mod (inc index) (length frames)) old_frame frame frame (elem index frames))
+	(defq index (mod (inc index) (length frames))
+		old_frame frame frame (elem index frames)
+		old_sframe sframe sframe (elem index sframes))
 	(bind '(x y w h) (view-get-bounds old_frame))
-	(view-add-dirty backdrop x y w h)
+	(bind '(sx sy sw sh) (view-get-bounds old_sframe))
+	(view-add-dirty (view-add-dirty backdrop sx sy sw sh) x y w h)
 	(setq x (add x xv) y (add y yv) yv (inc yv))
 	(if (gt y (sub backdrop_height h)) (setq y (sub backdrop_height h) yv -22))
 	(if (lt x 0) (setq x 0 xv (abs xv)))
 	(if (gt x (sub backdrop_width w)) (setq x (sub backdrop_width w) xv (neg (abs xv))))
 	(view-set-bounds frame x y w h)
+	(view-set-bounds sframe (add x 16) (add y 16) sw sh)
+	(view-sub old_sframe)
 	(view-sub old_frame)
-	(view-add-back backdrop frame)
+	(view-add-front (view-add-back backdrop sframe) frame)
+	(view-dirty sframe)
 	(view-dirty frame)
 ;	(debug "Pos = " x "," y)
 	(while (defq msg (mail-trymail))
