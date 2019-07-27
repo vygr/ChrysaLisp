3f49ab40a4555c16c9f6b2c9fa5165b0c42f4d77
Chris Hinsley <chris.hinsley@gmail.com>
2019-07-27 16:24:34 +0100

    Update app.lisp

--- apps/docs/app.lisp
+++ apps/docs/app.lisp
@@ -6,68 +6,76 @@
 	(byte 'win_close)
 	(byte 'win_button))
 
-(defq id t doc_list '("VM" "ASSIGNMENT" "STRUCTURE" "FUNCTIONS" "LISP" "SYNTAX" "CLASSES" "TODO"))
+(defq id t doc_list '("VM" "ASSIGNMENT" "STRUCTURE" "FUNCTIONS" "LISP" "SYNTAX" "CLASSES" "TODO")
+	space_width 8 tab_width (mul space_width 4) margin_width (mul space_width 3))
+
+(defun-bind normal-line ()
+	(cond
+		((starts-with "```" line_str)
+			(setq state 'code))
+		((starts-with "####" line_str)
+			(def (setq line_widget (create-label)) 'text (slice 4 -1 line_str)
+				'font (create-font "fonts/OpenSans-Regular.ttf" 22)))
+		((starts-with "###" line_str)
+			(def (setq line_widget (create-label)) 'text (slice 3 -1 line_str)
+				'font (create-font "fonts/OpenSans-Regular.ttf" 26)))
+		((starts-with "##" line_str)
+			(def (setq line_widget (create-label)) 'text (slice 2 -1 line_str)
+				'font (create-font "fonts/OpenSans-Regular.ttf" 30)))
+		((starts-with "#" line_str)
+			(def (setq line_widget (create-label)) 'text (slice 1 -1 line_str)
+				'font (create-font "fonts/OpenSans-Regular.ttf" 34)))
+		((find "`" line_str)
+			(def (setq line_widget (create-flow)) 'flow_flags (logior flow_flag_right flow_flag_fillh flow_flag_lastw))
+			(defq word_lst (split (cat " " line_str " ") "`"))
+			(each (lambda (word)
+				(setq word_cnt (inc word_cnt))
+				(unless (eql word " ")
+					(cond
+						((eq 0 (logand word_cnt 1))
+							(def (defq space_widget1 (create-label)) 'min_width space_width)
+							(def (defq word_widget (create-label)) 'text word 'ink_color argb_blue)
+							(def (defq space_widget2 (create-label)) 'min_width space_width)
+							(view-add-back line_widget space_widget1)
+							(view-add-back line_widget word_widget)
+							(view-add-back line_widget space_widget2))
+						(t
+							(def (defq word_widget (create-label)) 'text word 'ink_color argb_black)
+							(view-add-back line_widget word_widget))))) word_lst)
+			(setq word_cnt (inc word_cnt)))
+		(t
+			(def (setq line_widget (create-label)) 'text line_str))))
+
+(defun-bind code-line ()
+	(cond
+		((starts-with "```" line_str)
+			(setq state 'normal word_cnt 0))
+		((defq tab_pos (find (ascii-char 9) line_str))
+			(def (setq line_widget (create-flow)) 'flow_flags (logior flow_flag_right flow_flag_fillh flow_flag_lastw))
+			(def (defq tab_widget (create-label)) 'min_width (mul (inc tab_pos) tab_width))
+			(def (defq code_widget (create-label)) 'text (slice (inc tab_pos) -1 line_str)
+				'font (create-font "fonts/Hack-Regular.ttf" 16) 'ink_color argb_blue)
+			(view-add-back line_widget tab_widget)
+			(view-add-back line_widget code_widget))
+		(t
+			(def (setq line_widget (create-label)) 'text line_str
+				'font (create-font "fonts/Hack-Regular.ttf" 16) 'ink_color argb_blue))))
 
 (defun-bind populate-page (file)
-	(def (defq page_flow (create-flow)) 'flow_flags (logior flow_flag_right flow_flag_fillh)
-		'font (create-font "fonts/OpenSans-Regular.ttf" 18) 'color argb_white)
-	(def (defq page_widget (create-flow)) 'flow_flags (logior flow_flag_down flow_flag_fillw))
+	(ui-tree page_flow (create-flow) ('flow_flags (logior flow_flag_right flow_flag_fillh)
+			'font (create-font "fonts/OpenSans-Regular.ttf" 18) 'color argb_white)
+		(ui-element _ (create-label) ('min_width margin_width))
+		(ui-element page_widget (create-flow) ('flow_flags (logior flow_flag_down flow_flag_fillw)))
+		(ui-element _ (create-label) ('min_width margin_width)))
 	(defq state 'normal word_cnt 0)
 	(each-line (lambda (line_str)
-		(while nil)
-		(defq line_str (trim-end line_str (ascii-char 13)) line_widget nil)
+		(defq line_str (trim-end line_str (ascii-char 13)) line_widget (while nil))
 		(case state
-			(normal
-				(cond
-					((starts-with "```" line_str)
-						(setq state 'code))
-					((starts-with "####" line_str)
-						(def (setq line_widget (create-label)) 'text (slice 4 -1 line_str)
-							'font (create-font "fonts/OpenSans-Regular.ttf" 22)))
-					((starts-with "###" line_str)
-						(def (setq line_widget (create-label)) 'text (slice 3 -1 line_str)
-							'font (create-font "fonts/OpenSans-Regular.ttf" 26)))
-					((starts-with "##" line_str)
-						(def (setq line_widget (create-label)) 'text (slice 2 -1 line_str)
-							'font (create-font "fonts/OpenSans-Regular.ttf" 30)))
-					((starts-with "#" line_str)
-						(def (setq line_widget (create-label)) 'text (slice 1 -1 line_str)
-							'font (create-font "fonts/OpenSans-Regular.ttf" 34)))
-					(t
-						(cond
-							((defq s (find "`" line_str))
-								(def (setq line_widget (create-flow)) 'flow_flags (logior flow_flag_right flow_flag_fillh flow_flag_lastw))
-								(defq word_lst (split (cat " " line_str " ") "`"))
-								(each (lambda (word)
-									(if (eq 0 (logand (setq word_cnt (inc word_cnt)) 1))
-										(def (defq word_widget (create-label)) 'text (cat "`" word "`") 'ink_color argb_blue)
-										(def (defq word_widget (create-label)) 'text word 'ink_color argb_black))
-									(view-add-back line_widget word_widget)) word_lst)
-								(setq word_cnt (inc word_cnt)))
-							(t
-								(def (setq line_widget (create-label)) 'text line_str))))))
-			(code
-				(cond
-					((starts-with "```" line_str)
-						(setq state 'normal word_cnt 0))
-					((defq tab_pos (find (ascii-char 9) line_str))
-						(def (setq line_widget (create-flow)) 'flow_flags (logior flow_flag_right flow_flag_fillh flow_flag_lastw))
-						(def (defq tab_widget (create-label)) 'min_width (mul (inc tab_pos) 48))
-						(def (defq code_widget (create-label)) 'text (slice (inc tab_pos) -1 line_str)
-							'font (create-font "fonts/Hack-Regular.ttf" 16) 'ink_color argb_blue)
-						(view-add-back line_widget tab_widget)
-						(view-add-back line_widget code_widget))
-					(t
-						(def (setq line_widget (create-label)) 'text line_str
-							'font (create-font "fonts/Hack-Regular.ttf" 16) 'ink_color argb_blue)))))
+			(normal (normal-line))
+			(code (code-line)))
 		(if line_widget (view-add-child page_widget line_widget))) (cat "doc/" file ".md"))
-	(def (defq left_margin (create-label)) 'min_width 32)
-	(def (defq right_margin (create-label)) 'min_width 32)
-	(view-add-child page_flow left_margin)
-	(view-add-child page_flow page_widget)
-	(view-add-child page_flow right_margin)
-	(bind '(w h) (view-pref-size page_flow))
-	(view-layout (view-add-child page_scroll (view-change page_flow 0 0 w h)))
+	(apply view-change (cat (list page_flow 0 0) (view-pref-size page_flow)))
+	(view-layout (view-add-child page_scroll page_flow))
 	(view-dirty-all (view-layout doc_flow)))
 
 (ui-tree window (create-window window_flag_close) nil
@@ -80,10 +88,8 @@
 		(ui-element page_scroll (create-scroll scroll_flag_vertical) ('min_width 848 'min_height 800 'color argb_green))))
 
 (populate-page (elem 0 doc_list))
-(window-set-title window "Docs")
-(window-connect-close window event_win_close)
-(bind '(w h) (view-pref-size window))
-(gui-add (view-change window 280 64 w h))
+(gui-add (apply view-change (cat (list window 280 64)
+	(view-pref-size (window-set-title (window-connect-close window event_win_close) "Docs")))))
 
 (while id
 	(cond
