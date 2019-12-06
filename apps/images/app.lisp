a6b6ef8b172689c7f6ad4464ba51b7be201ed6f0
Nagasaki Nightmare <nuclearfall@protonmail.com>
2019-12-06 06:33:23 -0600

    Fixed resize in Images app.

--- apps/images/app.lisp
+++ apps/images/app.lisp
@@ -14,22 +14,24 @@
 	apps/images/sharpend.cpm apps/images/stairs.cpm apps/images/temple.cpm
 	apps/images/vermin.cpm) index 0 id t)
 
-(ui-tree window (create-window window_flag_close) nil
-	(ui-element _ (create-flow) ('flow_flags (logior flow_flag_down flow_flag_fillw flow_flag_lasth)
-			'color toolbar_col)
-		(ui-element _ (create-flow) ('flow_flags (logior flow_flag_right flow_flag_fillh)
-				'font (create-font "fonts/Entypo.otf" 32))
-			(component-connect (ui-element _ (create-button) ('text "")) event_win_prev)
-			(component-connect (ui-element _ (create-button) ('text "")) event_win_next))
-		(ui-element image_scroll (create-scroll (logior scroll_flag_vertical scroll_flag_horizontal))
-			('min_width 400 'min_height 400 'color slider_col))))
+	(ui-tree window (create-window window_flag_close) nil
+		(ui-element image_flow (create-flow) ('flow_flags (logior flow_flag_down flow_flag_fillw))
+			(ui-element _ (create-flow) ('flow_flags (logior flow_flag_right flow_flag_fillh)
+					'color toolbar_col 'font (create-font "fonts/Entypo.otf" 32))
+				(component-connect (ui-element _ (create-button) ('text "")) event_win_prev)
+				(component-connect (ui-element _ (create-button) ('text "")) event_win_next))
+			(ui-element frame (canvas-load (elem index images) 0))))
 
-(defun win-refresh (_)
-	(view-layout (view-add-child image_scroll (canvas-load (elem (setq index _) images) 0)))
-	(view-dirty-all (view-layout (window-set-title window (elem _ images)))))
+	(gui-add (apply view-change (cat (list window 200 200)
+		(view-pref-size (window-set-title (window-connect-close window event_win_close) (elem index images))))))
 
-(gui-add (apply view-change (cat (list window 64 64)
-	(view-pref-size (window-connect-close (win-refresh index) event_win_close)))))
+	(defun win-refresh (_)
+		(view-sub frame)
+		(setq index _ frame (canvas-load (elem index images) load_flag_film))
+		(view-layout (view-add-back image_flow frame))
+		(bind '(x y _ _) (view-get-bounds (window-set-title window (elem index images))))
+		(bind '(w h) (view-pref-size window))
+		(view-change-dirty window x y w h))
 
 (while id
 	(cond
