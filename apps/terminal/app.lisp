7f2d14632707b7e1ba7159c220ba01b3394577d9
Chris Hinsley <chris.hinsley@gmail.com>
2019-12-07 18:11:25 +0000

    seperate out input line buffering
    Once I get the windows laptop I'll make the TUIs do the history buffer too, but for now just the GUI terminal.

--- apps/terminal/app.lisp
+++ apps/terminal/app.lisp
@@ -1,11 +1,12 @@
 ;imports
 (import 'gui/lisp.inc)
 (import 'apps/terminal/pipe.inc)
+(import 'apps/terminal/input.inc)
 
 (structure 'event 0
 	(byte 'win_close))
 
-(defq id t cmd nil buffer "" history (list) history_index 0 vdu_width 60 vdu_height 40)
+(defq id t cmd nil vdu_width 60 vdu_height 40)
 
 (ui-tree window (create-window (logior window_flag_close window_flag_status)) ('color 0xc0000000)
 	(ui-element vdu (create-vdu) ('vdu_width vdu_width 'vdu_height vdu_height 'ink_color argb_green
@@ -18,72 +19,36 @@
 
 (defun-bind terminal-output (c)
 	(if (= c 13) (setq c 10))
-	(cond
-		;cursor up/down
-		((<= 0x40000051 c 0x40000052)
-			(vdu-print vdu (const (ascii-char 129))))
-		;print char
-		(t	(vdu-print vdu (char c)))))
+	(vdu-print vdu (char c)))
 
 (defun-bind terminal-input (c)
-	;echo char to terminal unless backspace into prompt.
-	(unless (and (= c 8) (= (length buffer) 0))
-		(terminal-output c))
+	(line-input c)
 	(cond
-		;send line ?
 		((or (= c 10) (= c 13))
-			;what state ?
+			;enter key
+			(vdu-print vdu (const (ascii-char 10)))
 			(cond
 				(cmd
 					;feed active pipe
-					(pipe-write cmd (cat buffer (const (ascii-char 10)))))
-				(t	;start new pipe
-					(cond
-						((/= (length buffer) 0)
-							;push new history entry if not same as last entry
-							(and (> (length (push history buffer)) 1)
-								(eql (elem -3 history) buffer)
-								(pop history))
-							(setq history_index (length history))
-							;new pipe
-							(catch (setq cmd (pipe-open buffer)) (progn (setq cmd nil) t))
-							(if cmd
-								(view-dirty-all (window-set-status window "Busy"))
-								(vdu-print vdu (const (cat "Pipe Error !" (ascii-char 10) ">")))))
-						(t (vdu-print vdu ">")))))
-			(setq buffer ""))
+					(pipe-write cmd (cat *line_buf* (const (ascii-char 10)))))
+				((/= (length *line_buf*) 0)
+					;new pipe
+					(catch (setq cmd (pipe-open *line_buf*)) (progn (setq cmd nil) t))
+					(if cmd
+						(view-dirty-all (window-set-status window "Busy"))
+						(vdu-print vdu (const (cat "Pipe Error !" (ascii-char 10) ">"))))))
+			(setq *line_buf* ""))
 		((= c 27)
-			;esc
+			;esc key
 			(when cmd
 				;feed active pipe, then EOF
-				(when (/= (length buffer) 0)
-					(pipe-write cmd buffer))
+				(when (/= (length *line_buf*) 0)
+					(pipe-write cmd *line_buf*))
 				(pipe-close cmd)
-				(setq cmd nil buffer "")
-				(vdu-print vdu (const (cat (ascii-char 10) ">")))
-				(view-dirty-all (window-set-status window "Ready"))))
-		((= c 0x40000052)
-			;cursor up
-			(unless cmd
-				(vdu-print vdu ">")
-				(setq buffer "" history_index (if (<= history_index 0) 0 (dec history_index)))
-				(when (< history_index (length history))
-					(setq buffer (elem history_index history))
-					(vdu-print vdu buffer))))
-		((= c 0x40000051)
-			;cursor down
-			(unless cmd
-				(vdu-print vdu ">")
-				(setq buffer "" history_index (min (inc history_index) (length history)))
-				(when (< history_index (length history))
-					(setq buffer (elem history_index history))
-					(vdu-print vdu buffer))))
-		((and (= c 8) (/= (length buffer) 0))
-			;backspace
-			(setq buffer (slice 0 -2 buffer)))
-		((<= 32 c 127)
-			;buffer the char
-			(setq buffer (cat buffer (char c))))))
+				(setq cmd nil *line_buf* "")
+				(vdu-print vdu (const (ascii-char 10)))
+				(view-dirty-all (window-set-status window "Ready")))))
+	(vdu-print vdu (cat (const (ascii-char 129)) (if cmd "" ">") *line_buf*)))
 
 (while id
 	(defq data t)
