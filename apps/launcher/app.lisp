7aefcdf8b7eaf34a57dc788ae94711677c60b529
Chris Hinsley <chris.hinsley@gmail.com>
2019-12-12 19:29:31 +0000

    Launcher (app-path) function

--- apps/launcher/app.lisp
+++ apps/launcher/app.lisp
@@ -31,8 +31,11 @@
 ; TODO - change above into default lists in a seperate file
 ; with a personalized pupa file for users.
 
+(defun-bind app-path (_)
+	(cat "apps/" _ "/app.lisp"))
+
 (each (lambda (_)
-	(open-child (cat "apps/" _ "/app.lisp") kn_call_open)) auto_app_list)
+	(open-child (app-path _) kn_call_open)) auto_app_list)
 
 (ui-tree window (create-window 0) nil
 	(ui-element _ (create-flow) ('flow_flags (logior flow_flag_down flow_flag_fillw) 'color toolbar_col)
@@ -46,7 +49,7 @@
 (while t
 	(cond
 		((= (get-long (defq msg (mail-read (task-mailbox))) ev_msg_target_id) event_win_button)
-			(open-child (cat "apps/" (get (view-find-id window (get-long msg ev_msg_action_source_id)) 'text) "/app.lisp") kn_call_open))
+			(open-child (app-path (get (view-find-id window (get-long msg ev_msg_action_source_id)) 'text)) kn_call_open))
 		(t (view-event window msg))))
 
 (view-hide window)
