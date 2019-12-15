4e672f8e6c154cc8a9f6ada8b943bebcd67cb481
nuclearfall <nuclearfall@protonmail.com>
2019-12-15 08:47:36 -0600

    bug fix: echo argument is passed as one argument

--- cmd/echo.lisp
+++ cmd/echo.lisp
@@ -11,4 +11,4 @@
 
 ;initialize pipe details and command args, abort on error
 (when (and (defq slave (create-slave)) (defq args (options slave usage)))
-	(each print (slice 1 -1 args)))
+	(print (apply cat (map (lambda (_) (cat _ " ")) (slice 1 -1 args)))))
