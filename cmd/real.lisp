56badb42c6d230c31fab85b7a849f66518074540
Chris Hinsley <chris.hinsley@gmail.com>
2020-01-08 10:34:55 +0000

    real-to-int, real-to-fixed etc

--- cmd/real.lisp
+++ cmd/real.lisp
@@ -12,13 +12,13 @@
 	,(bind-fun (lambda (o f) (setq mbits (to-num f)))))
 ))
 
-(defun-bind unpack-real (n)
+(defun-bind real-unpack (n)
 	(list (>>> (<< n (- 64 ebits)) (- 64 ebits)) (>>> n ebits)))
 
-(defun-bind pack-real (e m)
-	(+ (<< m ebits) (logand e (dec (<< 1 ebits)))))
+(defun-bind real-pack (e m)
+	(+ (<< m ebits) (>> (<< e (- 64 ebits)) (- 64 ebits))))
 
-(defun-bind norm-real (e m)
+(defun-bind real-norm (e m)
 	(cond
 		((> m 0)
 			;positive
@@ -36,72 +36,91 @@
 			(setq e 0)))
 	(list e m))
 
-(defun-bind add-real (n1 n2)
+(defun-bind real-add (n1 n2)
 	;unpack
-	(bind '(e1 m1) (unpack-real n1))
-	(bind '(e2 m2) (unpack-real n2))
+	(bind '(e1 m1) (real-unpack n1))
+	(bind '(e2 m2) (real-unpack n2))
 	(cond
-		((= (defq s (- e1 e2)) 0)
-			;exponents the same
-			(setq m1 (+ m1 m2)))
-		((> s 0)
-			;need to align m2
-			(setq m1 (+ m1 (>>> m2 s))))
-		(t
-			;need to align m1
-			(setq m1 (+ (>>> m1 (neg s)) m2) e1 e2)))
+		;exponents the same
+		((= (defq s (- e1 e2)) 0) (setq m1 (+ m1 m2)))
+		;need to align m2
+		((> s 0) (setq m1 (+ m1 (>>> m2 s))))
+		;need to align m1
+		(t (setq m1 (+ (>>> m1 (neg s)) m2) e1 e2)))
 	;normalise and pack
-	(apply pack-real (norm-real e1 m1)))
+	(apply real-pack (real-norm e1 m1)))
 
-(defun-bind sub-real (n1 n2)
+(defun-bind real-sub (n1 n2)
 	;unpack
-	(bind '(e1 m1) (unpack-real n1))
-	(bind '(e2 m2) (unpack-real n2))
+	(bind '(e1 m1) (real-unpack n1))
+	(bind '(e2 m2) (real-unpack n2))
 	(cond
-		((= (defq s (- e1 e2)) 0)
-			;exponents the same
-			(setq m1 (- m1 m2)))
-		((> s 0)
-			;need to align m2
-			(setq m1 (- m1 (>>> m2 s))))
-		(t
-			;need to align m1
-			(setq m1 (- (>>> m1 (neg s)) m2) e1 e2)))
+		;exponents the same
+		((= (defq s (- e1 e2)) 0) (setq m1 (- m1 m2)))
+		;need to align m2
+		((> s 0) (setq m1 (- m1 (>>> m2 s))))
+		;need to align m1
+		(t (setq m1 (- (>>> m1 (neg s)) m2) e1 e2)))
 	;normalise and pack
-	(apply pack-real (norm-real e1 m1)))
+	(apply real-pack (real-norm e1 m1)))
 
-(defun-bind mul-real (n1 n2)
+(defun-bind real-mul (n1 n2)
 	;unpack
-	(bind '(e1 m1) (unpack-real n1))
-	(bind '(e2 m2) (unpack-real n2))
+	(bind '(e1 m1) (real-unpack n1))
+	(bind '(e2 m2) (real-unpack n2))
 	;normalise and pack
-	(apply pack-real (norm-real (+ e1 e2 (neg mbits)) (* m1 m2))))
+	(apply real-pack (real-norm (+ e1 e2 (neg mbits)) (* m1 m2))))
 
-(defun-bind div-real (n1 n2)
+(defun-bind real-div (n1 n2)
 	;unpack
-	(bind '(e1 m1) (unpack-real n1))
-	(bind '(e2 m2) (unpack-real n2))
+	(bind '(e1 m1) (real-unpack n1))
+	(bind '(e2 m2) (real-unpack n2))
 	;normalise and pack
-	(apply pack-real (norm-real (- e1 e2 (inc (* (- 31 mbits) 2))) (/ (<< m1 (- 63 mbits)) m2))))
+	(apply real-pack (real-norm (- e1 e2 (inc (* (- 31 mbits) 2))) (/ (<< m1 (- 63 mbits)) m2))))
 
-(defun-bind int-real (n)
-	(apply pack-real (norm-real mbits n)))
+(defun-bind int-to-real (n)
+	(apply real-pack (real-norm mbits n)))
 
-(defun-bind fixed-real (n)
-	(apply pack-real (norm-real (- mbits fp_shift) n)))
+(defun-bind fixed-to-real (n)
+	(apply real-pack (real-norm (- mbits fp_shift) n)))
+
+(defun-bind real-to-int (n)
+	;unpack
+	(bind '(e m) (real-unpack n))
+	;norm to int
+	(cond
+		((= (setq e (- mbits e)) 0) m)
+		((> e 0) (>>> m e))
+		(t (<< m (neg e)))))
+
+(defun-bind real-to-fixed (n)
+	;unpack
+	(bind '(e m) (real-unpack n))
+	;norm to fixed
+	(cond
+		((= (setq e (- mbits fp_shift e)) 0) m)
+		((> e 0) (>>> m e))
+		(t (<< m (neg e)))))
+
+(defun-bind fixed-to-str (n)
+	(defq s "")
+	(when (< n 0)
+		(setq s "-" n (neg n)))
+	(defq i (>> n fp_shift) f (fmul (- n (<< i fp_shift)) 100000))
+	(cat s (str i) "." (pad f 5 "00000")))
 
 ;initialize pipe details and command args, abort on error
 (when (and (defq slave (create-slave)) (defq mbits 31 args (options slave usage)))
 	(defq ebits (- 63 mbits))
 
-	(defq n1 (fixed-real 2.571) n2 (fixed-real 45.599) n3 (int-real 0) n4 (div-real (int-real 1024) (int-real 4)))
+	(defq n1 (fixed-to-real 2.571) n2 (fixed-to-real -45.599) n3 (int-to-real 0) n4 (real-div (int-to-real 1024) (int-to-real 4)))
 	(print "Nums: " (array n1 n2 n3 n4))
-	(print "Add: " (array (add-real n2 n1) (add-real n1 n2)))
-	(print "Sub: " (array (sub-real n1 n2) (sub-real n2 n1) (sub-real n1 n1)))
-	(print "Mul: " (array (mul-real n2 n1) (mul-real n2 n1) (mul-real n1 n3) (mul-real n2 n4)))
+	(print "Add: " (array (real-add n2 n1) (real-add n1 n2)))
+	(print "Sub: " (array (real-sub n1 n2) (real-sub n2 n1) (real-sub n1 n1)))
+	(print "Mul: " (array (real-mul n2 n1) (real-mul n2 n1) (real-mul n1 n3) (real-mul n2 n4)))
 
-	(print "Result Mul: " (array (add-real n1 (mul-real n2 n4))))
+	(print "Result Mul: " (array (real-add n1 (real-mul n2 n4))))
 	(each (lambda (_)
-		(setq n1 (add-real n1 n2))) (range 0 256))
-	(print "Result Acc: " (array n1))
+		(setq n1 (real-add n1 n2))) (range 0 256))
+	(print "Result Acc: " (array n1) " " (fixed-to-str (real-to-fixed n1)))
 )
