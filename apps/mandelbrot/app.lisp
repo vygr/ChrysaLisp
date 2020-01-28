2dc0f31fe49510bd813562578db1cd54ac5fc5c4
Chris Hinsley <chris.hinsley@gmail.com>
2020-01-28 14:53:57 +0000

    left/right button click for zoom/unzoom
    Shows up the issue with 16.16 fixed point nicley ! Which is why this is a good test bed for the 'real' format...

--- apps/mandelbrot/app.lisp
+++ apps/mandelbrot/app.lisp
@@ -8,7 +8,8 @@
 (structure 'event 0
 	(byte 'win_close))
 
-(defq canvas_width 800 canvas_height 800 id t in nil then (time) total 0 select nil)
+(defq canvas_width 800 canvas_height 800 id t in nil then (time) total 0 select nil
+	center_x -0.5 center_y 0.0 zoom 1.0)
 
 (ui-tree window (create-window window_flag_close) nil
 	(ui-element canvas (create-canvas canvas_width canvas_height 1)))
@@ -21,7 +22,8 @@
 	(canvas-swap (canvas-fill canvas argb_black))
 	(setq in (in-stream) then (time) select (array (task-mailbox) (in-mbox in))
 		total (* canvas_width canvas_height))
-	(mail-send (array (elem 1 select) 0 0 canvas_width canvas_height canvas_width canvas_height)
+	(mail-send (array (elem 1 select) 0 0 canvas_width canvas_height
+		canvas_width canvas_height center_x center_y zoom)
 		(open-child "apps/mandelbrot/child.lisp" kn_call_child)))
 
 (reset)
@@ -37,8 +39,12 @@
 					(setq id nil))
 				((and (= id (component-get-id canvas))
 						(= (get-long msg ev_msg_type) ev_type_mouse)
-						(= (get-int msg ev_msg_mouse_buttons) 0))
+						(/= (get-int msg ev_msg_mouse_buttons) 0))
+					(print (get-int msg ev_msg_mouse_buttons))
 					;mouse click on the canvas
+					(setq center_x (+ center_x (fmul zoom (/ (* (- (get-int msg ev_msg_mouse_rx) (/ canvas_width 2)) 2.0) canvas_width)))
+						center_y (+ center_y (fmul zoom (/ (* (- (get-int msg ev_msg_mouse_ry) (/ canvas_height 2)) 2.0) canvas_height)))
+						zoom (fmul zoom (if (= 0 (logand (get-int msg ev_msg_mouse_buttons) 2)) 0.5 1.5)))
 					(reset))
 				(t (view-event window msg))))
 		(t	;child msg
--- apps/mandelbrot/child.lisp
+++ apps/mandelbrot/child.lisp
@@ -12,7 +12,7 @@
 ;native versions
 (ffi depth "apps/mandelbrot/depth" 0)
 
-(defun-bind mandel (xp y x1 y1 w h)
+(defun-bind mandel (xp y x1 y1 w h cx cy z)
 	(defq reply (string-stream (cat "")))
 	(write reply (cat
 		(char x (const int_size)) (char y (const int_size))
@@ -21,26 +21,30 @@
 	(while (/= (setq y (inc y)) y1)
 		(defq x (dec xp))
 		(while (/= (setq x (inc x)) x1)
-			(write reply (char (depth (/ (* (- x (/ w 2) (/ w 4)) 2.0) w) (/ (* (- y (/ h 2)) 2.0) h)))))
+			(write reply (char (depth
+				(+ (fmul z (/ (* (- x (/ w 2)) 2.0) w)) cx)
+				(+ (fmul z (/ (* (- y (/ h 2)) 2.0) h)) cy)))))
 		(task-sleep 0))
 	(mail-send (str reply) mbox))
 
-(defun-bind rect (mbox x y x1 y1 w h)
+(defun-bind rect (mbox x y x1 y1 w h cx cy z)
 	(cond
 		((>= (* (- x1 x) (- y1 y)) (const (* 129 129)))
 			;split into more tasks
 			(defq farm (open-farm "apps/mandelbrot/child.lisp" 3 kn_call_child)
 				x2 (+ x (/ (- x1 x) 2)) y2 (+ y (/ (- y1 y) 2)))
-			(mail-send (array mbox x y x2 y2 w h) (elem 0 farm))
-			(mail-send (array mbox x2 y x1 y2 w h) (elem 1 farm))
-			(mail-send (array mbox x y2 x2 y1 w h) (elem 2 farm))
-			(rect mbox x2 y2 x1 y1 w h))
+			(mail-send (array mbox x y x2 y2 w h cx cy z) (elem 0 farm))
+			(mail-send (array mbox x2 y x1 y2 w h cx cy z) (elem 1 farm))
+			(mail-send (array mbox x y2 x2 y1 w h cx cy z) (elem 2 farm))
+			(rect mbox x2 y2 x1 y1 w h cx cy z))
 		(t	;do here
-			(mandel x y x1 y1 w h))))
+			(mandel x y x1 y1 w h cx cy z))))
 
 ;read work request
 (defq msg (string-stream (mail-read (task-mailbox))))
 (rect (read-char msg (const long_size))
 	(read-char msg (const long_size)) (read-char msg (const long_size))
 	(read-char msg (const long_size)) (read-char msg (const long_size))
-	(read-char msg (const long_size)) (read-char msg (const long_size)))
+	(read-char msg (const long_size)) (read-char msg (const long_size))
+	(read-char msg (const long_size)) (read-char msg (const long_size))
+	(read-char msg (const long_size)))
