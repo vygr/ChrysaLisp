8d061fbe3a828309083ed036afe4cd655ef2c6ab
Chris Hinsley <chris.hinsley@gmail.com>
2020-02-25 17:14:28 +0000

    8 symbols width and toolbar_col background

--- apps/entypo/app.lisp
+++ apps/entypo/app.lisp
@@ -19,13 +19,13 @@
 		(to-base-char (logand 0xf _))))
 
 (defq id t range_start 0xe900 range_end 0xea50
-	grid_width 6 grid_height (/ (- range_end range_start) grid_width))
+	grid_width 8 grid_height (/ (- range_end range_start) grid_width))
 
 (ui-tree window (create-window window_flag_close) nil
 	(ui-element symbol_scroll (create-scroll scroll_flag_vertical)
 			('color slider_col 'font (create-font "fonts/Entypo.ctf" 42))
 		(ui-element symbol_grid (create-grid) ('grid_width (inc grid_width) 'grid_height grid_height
-				'color argb_white)
+				'color toolbar_col)
 			(each (lambda (c)
 				(setq c (+ range_start (* c grid_width)))
 				(ui-element _ (create-label) ('font (create-font "fonts/Hack-Regular.ctf" 12) 'text (to-hex c)))
