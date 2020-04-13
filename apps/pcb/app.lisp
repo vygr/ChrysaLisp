91407357ce3aee2d681b90958ae397ed8a16e240
Chris Hinsley <chris.hinsley@gmail.com>
2020-04-13 12:28:27 +0100

    be consistant in the order of properties

--- apps/pcb/app.lisp
+++ apps/pcb/app.lisp
@@ -16,7 +16,7 @@
 	(ui-title-bar window_title "" (0xea19) (const event_win_close))
 	(ui-tool-bar _ ()
 		(ui-buttons (0xe91d 0xe91e 0xea00 0xea01 0xe9ac 0xe9ad) (const event_win_prev))
-		(ui-buttons ("0" "1" "2" "3" "4") (const event_win_show_all) nil
+		(ui-buttons ("0" "1" "2" "3" "4") (const event_win_show_all)
 			('color (const *env_toolbar2_col*) 'font (const (create-font "fonts/OpenSans-Regular.ctf" 24)))))
 	(ui-scroll pcb_scroll (logior scroll_flag_vertical scroll_flag_horizontal) ('min_width 512 'min_height 256)))
 
