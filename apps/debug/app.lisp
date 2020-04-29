17fe43c182fe532ec21880212a20ab06a637c1fe
Chris Hinsley <chris.hinsley@gmail.com>
2020-04-29 09:35:04 +0100

    be friendly do the debugger for now
    Do a Chysalisp Terminal 'make boot' !

--- apps/debug/app.lisp
+++ apps/debug/app.lisp
@@ -68,7 +68,7 @@
 			(clear buf_keys)
 			(setq buf_index nil)
 			(vdu-load vdu '(
-				"ChrysaLisp Debug 0.3 (BROKEN WILL FIX SOON !)"
+				"ChrysaLisp Debug 0.3"
 				"Toolbar1 buttons act on a single task."
 				"Toolbar2 buttons act on all tasks."
 				"Slider to switch between tasks."
