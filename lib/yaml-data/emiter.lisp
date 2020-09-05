536ac51c3b1abac6602b6bc6d9699c4e65b8f8b6
Frank V. Castellucci <frankiecast@gmail.com>
2020-09-05 06:47:10 -0400

    AST for serialization

--- lib/yaml-data/emiter.lisp
+++ lib/yaml-data/emiter.lisp
@@ -9,6 +9,11 @@
 
 (defq ywcntrl nil)
 
+; Breaks out properties to explicitly tagged
+;   :entries
+;     :key
+;     :value
+; May prommote to 'xtras'
 ;
 ; (:clz :me_key :value k)
 ; (:clz :me_value :value v)
@@ -40,46 +45,91 @@
                 (make-me-value (second el))))))
     (prop-entries p) (list)))
 
-(defun build-nodes (idata)
-  ; (build-nodes idata) -> nil
+(defun set-context! (n)
+  ; (set-context! node) -> node
+  ; Makes the current context 'node'
+  ; after adding 'node' to current's children
+  (defq crn (getp ywcntrl :current))
+  ; Stack node in path for un-setting
+  (push (getp ywcntrl :path) n)
+  ; If first
+  (when crn
+    (add-child-node! crn n))
+  (setp! ywcntrl :current n)
+  n)
+
+(defun unset-context! ()
+  ; (unset-context!) -> node | nil
+  ; Set's context to most recent in path
+  (defq
+    npath (getp ywcntrl :path)  ; Setup
+    lnode (pop npath))          ; Pop path stack
+  (when (truthy? npath)
+    (setp! ywcntrl :current (last npath)))
+  nil)
+
+(defun add-to-context! (n)
+  ; (add-to-context! node) -> node | nil
+  (when (truthy? (defq crn (getp ywcntrl :current)))
+    (add-child-node! crn n)))
+
+(defun build-nodes! (idata)
+  ; (build-nodes! idata) -> nil
+  ; Generates AST for inbound data
   (cond
     ((lst? idata)
         (cond
           ; Map
           ((props? idata)
-           (print "Properties -> Map")
-           (each build-nodes (split-entries idata)))
+           (set-context! (MapNode))
+           (each build-nodes! (split-entries idata))
+           (unset-context!))
           ; Map Entry
           ((prop-me? idata)
-           (print "Map Entry")
-           (each build-nodes (getp idata :entries)))
+           (set-context! (MapEntryNode))
+           (each build-nodes! (getp idata :entries))
+           (unset-context!))
           ; Key
           ((me-key? idata)
-           (print "Key")
-           (build-nodes (getp idata :value)))
+           (set-context! (KeyNode))
+           (build-nodes! (getp idata :value))
+           (unset-context!))
           ; Value
           ((me-value? idata)
-           (print "Value")
-           (build-nodes (getp idata :value)))
+           (set-context! (ValueNode))
+           (build-nodes! (getp idata :value))
+           (unset-context!))
           ; Sequence
           (t
-            (print "List -> seq " idata)
-            (each build-nodes idata))))
-    (
-     (or
-       (kw? idata)
-       (sym? idata)
-       (str? idata)
-       (num? idata))
-     (print "Scalar " idata))))
+            (set-context! (SequenceNode))
+            (each build-nodes! idata)
+            (unset-context!))))
+    ((kw? idata)
+     (add-to-context! (ScalarNode :keyword idata)))
+    ((sym? idata)
+     (add-to-context! (ScalarNode :symbol idata)))
+    ((str? idata)
+     (add-to-context! (ScalarNode :string idata)))
+    ((num? idata)
+     (add-to-context! (ScalarNode :number idata)))
+    (t
+     (throw "Unknown type found in build-nodes!" idata))))
 
 (defun-bind emit (stream data in-args)
   ; (emit stream data options) -> stream
   ; Converts data to strings and writes to streams
   (setq ywcntrl (pmerge in-args (properties
                                   :level  -1
-                                  :indent 0
+                                  :indent -1
                                   :stream stream
-                                  :nodes  (DocStartNode))))
-  (build-nodes data)
-  (write stream (str (getp ywcntrl :nodes))))
+                                  :root  (DocStartNode)
+                                  :path  (list)
+                                  :current nil)))
+  (set-context! (getp ywcntrl :root))
+  (build-nodes! data)
+  (add-to-context! (DocEndNode))
+  (unset-context!)
+  ; (walk-tree (getp ywcntrl :root) print)
+  ; (pre-walk-recur print (getp ywcntrl :root))
+  ; (write stream (str (getp ywcntrl :root)))
+  stream)
--- lib/yaml-data/nodes.lisp
+++ lib/yaml-data/nodes.lisp
@@ -8,45 +8,34 @@
   (properties
       :type         ttype
       :value        value
-      :indent       0
-      :parent       nil
-      :representer  nil
       :children     (list)))
 
-(defun cr_present (&optional i n)
-  (setd i 0 n "")
-  (str (pad "" (* i 2)) n (char 0x0a)))
-
-(defun cr_block_present (i n)
-  )
-
-(defun cr_flow_present (i n)
-  )
-
 (defun DocStartNode ()
-  (setp! (Node :docstart "---") :representer cr_present))
+  (Node :docstart "---"))
 
 (defun DocEndNode ()
-  (seto! (Node :docstart "...") :representer cr_present))
+  (Node :docend "..."))
+
+(defun MapNode ()
+  (Node :map))
+
+(defun MapEntryNode ()
+  (Node :map_entry))
+
+(defun KeyNode ()
+  (Node :key))
+
+(defun ValueNode ()
+  (Node :value))
+
+(defun SequenceNode ()
+  (Node :seq))
 
 (defun ScalarNode (stype value)
-  (setp! (Node :scalar value) :stype stype))
+  (setp! (Node :scalar value) :stype stype t))
 
-(defun KeyNode (stype value)
-  (setp! (Node :key value) :stype stype))
-
-(defun ValueNode (stype value)
-  (setp! (Node :value value) :stype stype))
-
-(defun SequenceNode ())
-(defun BlockSequenceNode ())
-(defun FlowSequenceNode ())
-(defun MapNode ())
-(defun BlockMapNode ())
-(defun FlowMapNode ())
-
-(defun node-children (_)
-  (getp _ :children))
-
-(defun add-child (p c)
-  (push (node-children p) (setp! c :parent p)))
+(defun add-child-node! (p c)
+  (when (not (lst? (defq chs (getp p :children))))
+    (throw "Children of node is not a list" p))
+  (push chs c)
+  c)
