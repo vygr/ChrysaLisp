e386cf3d95600d5fc5915c91b430ed401255a5e7
Frank V. Castellucci <frankiecast@gmail.com>
2020-09-07 07:00:55 -0400

    Update emiter.lisp

--- lib/yaml-data/emiter.lisp
+++ lib/yaml-data/emiter.lisp
@@ -44,22 +44,6 @@
 ; Not multi-threaded safe
 
 (defq ywcntrl       nil)
-(defq indent_space  2)
-(defq max_level     0)
-
-(defun inc_indent()
-  (defq ci (getp ywcntrl :indent))
-  (setp! ywcntrl :indent (inc ci)))
-
-(defun dec_indent()
-  (defq ci (getp ywcntrl :indent))
-  (setp! ywcntrl :indent (dec ci)))
-
-(defun pad_indent()
-  (pad "" (* indent_space (getp ywcntrl :indent))))
-
-(defun default-writer (v)
-  (write (getp ywcntrl :stream) (str (pad_indent) v (char 0x0a))))
 
 ; AST Context Stack
 
@@ -134,77 +118,139 @@
     (t
      (throw "Unknown type found in build-nodes!" odata))))
 
-; Realize AST to stream
+
+; Node path control
+
+(defq container-nodes (list :docstart :seq :map :key :value))
+
+(defun push-npath (n)
+  (push (getp ywcntrl :npath) n))
+
+(defun pop-npath ()
+  (pop (getp ywcntrl :npath)))
+
+(defun last-node ()
+  (last (getp ywcntrl :npath)))
+
+(defun unwind-to-node (ntype)
+  (while (neql? (last-node) ntype)
+    (pop-npath)))
+
+(defun mr-container ()
+  (defq
+    res  nil
+    np   (getp ywcntrl :npath)
+    rpos (dec (length np)))
+  (while (>= rpos 0)
+    (defq n (elem rpos np))
+    (if (find n container-nodes)
+      (setq
+        res  n
+        rpos -1)
+      (setq rpos (dec rpos))))
+  res)
+
+; Writer utilities
+
+(defq indent_space  2)
+
+(defun inc_indent()
+  (defq ci (getp ywcntrl :indent))
+  (setp! ywcntrl :indent (inc ci)))
+
+(defun dec_indent()
+  (defq ci (getp ywcntrl :indent))
+  (setp! ywcntrl :indent (dec ci)))
+
+(defun pad_indent()
+  (pad "" (* indent_space (getp ywcntrl :indent))))
+
+; Writers
+
+(defun default-writer (v)
+  (write (getp ywcntrl :stream) (str (pad_indent) v (char 0x0a))))
 
 (defun key-writer (v)
-  (write (getp ywcntrl :stream)
-         (str (pad_indent) v ":")))
+  (write (getp ywcntrl :stream) (str (pad_indent) v ": ")))
 
 (defun value-writer (v)
-  (write (getp ywcntrl :stream)
-         (str (pad_indent) v (char 0x0a))))
+  (write (getp ywcntrl :stream) (str (pad_indent) v (char 0x0a))))
 
-(defun seq-write-start ()
-  (write (getp ywcntrl :stream)
-         (str (pad_indent) "- " (char 0x0a))))
+(defun seq-start-writer ()
+  (defq
+    lnd  (last-node)
+    mrc  (mr-container)
+    strm (getp ywcntrl :stream))
+  (print "seq-start lnd " lnd " mrc " mrc)
+  (cond
+    ((eql lnd :docstart)
+     (write strm (str (pad_indent) "- ")))
+    ((eql lnd :seq)
+     (write strm (str (char 0x0a) (pad_indent) "- ")))
+    ((and (eql lnd :scalar) (eql mrc :seq))
+     (dec_indent)
+     (write strm (str (pad_indent) "-" (char 0x0a)))
+     (inc_indent)
+     (write strm (str (pad_indent) "- ")))
+    (t
+      (throw "Unknown " (list lnd mrc)))))
 
-(defun seq-writer (v)
-  (write (getp ywcntrl :stream)
-         (str (pad_indent) "- " v (char 0x0a))))
+(defun seq-value-writer (v)
+  (defq
+    strm (getp ywcntrl :stream)
+    lnd  (last-node))
+  (case lnd
+    ((:seq)
+     (write strm (str v (char 0x0a))))
+    ((:scalar)
+     (write strm (str (pad_indent) "- " v (char 0x0a))))
+    (t
+      (throw "Unknown " lnd))))
 
 (defun node-to-stream (ast &optional pwrt)
   ; (gen-stream stream ast) -> nil
   (setd pwrt default-writer)
   (case (getp ast :type)
     ((:docstart)
+     (push-npath :docstart)
      (setp! ywcntrl :indent 0)
      (pwrt (getp ast :value))
      (setp! ywcntrl :indent -1)
      (each node-to-stream (getp ast :children)))
     ((:map)
      (inc_indent)
+     (push-npath :map)
      (each node-to-stream (getp ast :children))
-     (dec_indent))
+     (dec_indent)
+     (pop-npath))
     ((:seq)
-     (defq
-       first_type (getp (first (getp ast :children)) :type)
-       indent_lvl (getp ywcntrl :indent))
      (inc_indent)
-     (cond
-       ((eql first_type :seq)
-        (seq-write-start)
-        (defq pf 0)
-        (each
-          (lambda (n)
-            (case (getp n :type)
-              ((:seq)
-               (when (> pf 0)
-                 (seq-write-start)
-                 (inc_indent))
-               (node-to-stream n seq-writer)
-               (when (> pf 0)
-                 (dec_indent))
-               (setq pf (inc pf)))
-              (t
-                (node-to-stream n seq-writer)))
-            ) (getp ast :children)))
-       (t
-         (each (#(node-to-stream %0 seq-writer)) (getp ast :children))))
-     (dec_indent))
+     (seq-start-writer)
+     (push-npath :seq)
+     (each (#(node-to-stream %0 seq-value-writer)) (getp ast :children))
+     (dec_indent)
+     (unwind-to-node :seq)
+     (pop-npath))
     ((:map_entry)
      (each node-to-stream (getp ast :children)))
     ((:key)
-     (each (#(node-to-stream %0 key-writer)) (getp ast :children)))
+     (push-npath :key)
+     (each (#(node-to-stream %0 key-writer)) (getp ast :children))
+     (pop-npath))
     ((:value)
+     (push-npath :value)
      (when (find (getp (first (getp ast :children)) :type) (list :seq :map))
          (write (getp ywcntrl :stream) (char 0x0a)))
-     (each (#(node-to-stream %0 value-writer)) (getp ast :children)))
+     (each (#(node-to-stream %0 value-writer)) (getp ast :children))
+     (pop-npath))
      ; (each node-to-stream (getp ast :children)))
     ((:scalar)
-     (pwrt (getp ast :value)))
+     (pwrt (getp ast :value))
+     (push-npath :scalar))
     ((:docend)
      (setp! ywcntrl :indent 0)
-     (pwrt (getp ast :value)))
+     (pwrt (getp ast :value))
+     (pop-npath))
     (t
       ; (throw "yaml-emit: Unknown Node Type" (getp ast :type))
       )
@@ -217,6 +263,7 @@
   (setq ywcntrl (pmerge in-args (properties
                                   :stream stream
                                   :root  (DocStartNode)
+                                  :npath (list)
                                   :path  (list)
                                   :current nil
                                   :indent -1)))
