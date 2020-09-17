bcd6592ad77fdd555796f3e123cebb904250b144
Chris Hinsley <chris.hinsley@gmail.com>
2020-09-17 18:09:39 +0100

    restore working whiteboard after merge problems...

--- apps/whiteboard/child.lisp
+++ apps/whiteboard/child.lisp
@@ -2,7 +2,7 @@
 (import 'apps/whiteboard/app.inc)
 
 ;read args from parent (shared dlist tuple)
-
+(defq dlist (mail-read (task-mailbox)) flatten (elem +dlist_flatten+ dlist))
 
 (defun-bind fpoly (canvas col mode _)
 	;draw a polygon on a canvas
@@ -11,15 +11,17 @@
 
 (defun-bind redraw (dlist)
 	;redraw layer/s
-
+	(when (/= 0 (logand (elem +dlist_mask+ dlist) 1))
+		(defq canvas (elem +dlist_commited_canvas+ dlist))
 		(canvas-fill canvas 0)
-
+		(each (lambda ((col poly)) (fpoly canvas col 1 poly)) (elem +dlist_commited_polygons+ dlist))
 		(canvas-swap canvas))
-
+	(when (/= 0 (logand (elem +dlist_mask+ dlist) 2))
+		(defq canvas (elem +dlist_overlay_canvas+ dlist))
 		(canvas-fill canvas 0)
 		(each (lambda (p)
 			(bind '(col poly) (flatten p))
-
+			(fpoly canvas col 1 poly)) (elem +dlist_overlay_paths+ dlist))
 		(canvas-swap canvas))
 	(elem-set +dlist_mask+ dlist 0))
 
@@ -27,4 +29,4 @@
 	;until quit
 	(until (mail-poll (array (task-mailbox)))
 		(redraw dlist)
-
+		(task-sleep (elem +dlist_rate+ dlist))))
