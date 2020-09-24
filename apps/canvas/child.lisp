0fb10e695ec54b2b16cd05fdd2de099bb873860d
Chris Hinsley <chris.hinsley@gmail.com>
2020-09-24 18:23:31 +0100

    uses +const+ symbols

--- apps/canvas/child.lisp
+++ apps/canvas/child.lisp
@@ -7,7 +7,7 @@
 ;read args from parent
 (bind '(canvas canvas_width canvas_height canvas_scale) (mail-read (task-mailbox)))
 
-(defq eps 0.25 angle 0.0 font (create-font "fonts/OpenSans-Regular.ctf" 36)
+(defq +eps+ 0.25 angle 0.0 font (create-font "fonts/OpenSans-Regular.ctf" 36)
 	fp1 (font-glyph-paths font "__Glyphs!")
 	fp2 (font-glyph-paths font "__Easy!")
 	fp3 (font-glyph-paths font "__Simple!")
@@ -51,46 +51,46 @@
 		(path -0.5 -0.5 -0.25 0.5 0.0 -0.5 0.25 0.5 0.5 -0.5 -0.05 0.5))))
 
 	(fpoly 0xff0ff0ff 0 (transform (* angle -1.0)
-		(path-stroke-polylines (list) (* canvas_width 0.05) eps join_bevel cap_square cap_square
+		(path-stroke-polylines (list) (* canvas_width 0.05) +eps+ join_bevel cap_square cap_square
 			(list (path-gen-quadratic
 				(* canvas_width -0.4) (* canvas_height 0.4)
 				(* canvas_width -0.2) (* canvas_height -1.1)
 				(* canvas_width 0.4) (* canvas_height 0.2)
-				eps (path))))))
+				+eps+ (path))))))
 
 	(fpoly 0xc000ff00 0 (transform angle
-		(path-stroke-polylines (list) (* canvas_width 0x0.1) eps join_round cap_round cap_round
+		(path-stroke-polylines (list) (* canvas_width 0x0.1) +eps+ join_round cap_round cap_round
 			(list (path (* canvas_width -0.4) (* canvas_height -0.4)
 				(* canvas_width 0.3) (* canvas_height -0.3)
 				(* canvas_width 0.4) (* canvas_height 0.4))))))
 
 	(fpoly argb_yellow 0 (defq p (transform (* angle (i2f -2))
-		(path-stroke-polygons (list) (* canvas_width 0.011) eps join_miter
-			(path-stroke-polylines (list) (* canvas_width 0.033) eps join_bevel cap_round cap_arrow
+		(path-stroke-polygons (list) (* canvas_width 0.011) +eps+ join_miter
+			(path-stroke-polylines (list) (* canvas_width 0.033) +eps+ join_bevel cap_round cap_arrow
 				(list (path-gen-cubic
 					(* canvas_width -0.45) (* canvas_height 0.3)
 					(* canvas_width -0.3) (* canvas_height -0.3)
 					(* canvas_width 0.45) (* canvas_height 0.6)
 					(* canvas_width 0.4) (* canvas_height -0.4)
-					eps (path))))))))
+					+eps+ (path))))))))
 	(fpoly 0x80000000 0 (slice 1 2 p))
 
 	(fpoly 0xd0ff00ff 0 (defq p (transform angle
-		(path-stroke-polygons (list) (* canvas_width 0.02) eps join_miter
+		(path-stroke-polygons (list) (* canvas_width 0.02) +eps+ join_miter
 			(list (path-gen-arc
 				(* canvas_width 0.2) (* canvas_height 0.3) 0.0 (const +fp_2pi+)
-				(* canvas_width 0.125) eps (path)))))))
+				(* canvas_width 0.125) +eps+ (path)))))))
 	(fpoly 0x60000000 0 (slice 0 1 p))
 
 	(fpoly 0xc00000ff 0 (defq polygons (transform angle
-		(path-stroke-polygons (list) (* canvas_width 0.025) eps join_miter
-			(path-stroke-polylines (list) (* canvas_width 0.05) eps join_bevel cap_square cap_tri (list
+		(path-stroke-polygons (list) (* canvas_width 0.025) +eps+ join_miter
+			(path-stroke-polylines (list) (* canvas_width 0.05) +eps+ join_bevel cap_square cap_tri (list
 				(path-gen-arc
 					(* canvas_width -0.1) (* canvas_height -0.2) 0.9 1.5
-					(* canvas_width 0.2) eps (path))
+					(* canvas_width 0.2) +eps+ (path))
 				(path-gen-arc
 					(* canvas_width -0.2) (* canvas_height -0.2) 4.0 2.0
-					(* canvas_width 0o0.1) eps (path))))))))
+					(* canvas_width 0o0.1) +eps+ (path))))))))
 	(fpoly 0xa0ffffff 0 (list (elem 1 polygons) (elem 3 polygons)))
 
 	(fpoly 0xff000000 0 (transform-copy angle fp1))
