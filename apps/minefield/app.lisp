9a0d2f5c3d6a0696d8a6200ef59bb8c5d0de667f
Nagasaki Nightmare <nuclearfall@protonmail.com>
2020-10-04 11:01:26 -0500

    added anaphoric aeach and replaced each-in usage

--- apps/minefield/app.lisp
+++ apps/minefield/app.lisp
@@ -132,8 +132,7 @@
 
 (defun-bind main ()
 	(defq started nil game (list) game_board (list) game_adj (list) game_map (list) +cell_wh+ 32 
-		first_click t difficulty (list) game_grid nil click_offset 4 game_over nil buttons_left 0
-		mouse_down 0)
+		first_click t difficulty (list) game_grid nil click_offset 4 game_over nil mouse_down 0)
 	(bind '(w h) (view-pref-size window))
 	(bind '(x y _ _) (view-get-bounds window))
 	(gui-add (view-change window x y w h))
