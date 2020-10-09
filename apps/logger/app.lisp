fa064a7c72e301ce9f15141bd28170718dc0f906
Frank V. Castellucci <frankiecast@gmail.com>
2020-10-09 05:32:15 -0400

    Update app.lisp

--- apps/logger/app.lisp
+++ apps/logger/app.lisp
@@ -19,11 +19,16 @@
 
   ; Setup general purpose information
   (defq
+<<<<<<< HEAD
     fs  (file-stream "./logs/logservice.log" file_open_write_append)
+=======
+    fs  (file-stream "logmsg.log" file_open_write)
+>>>>>>> parent of b4694fe0... log-msg writer
     reg (hmap)
     lup (hmap)
     active t)
 
+<<<<<<< HEAD
   ; Populate lookups
   (hmap-insert lup +log_message_debug+ "DEBUG")
   (hmap-insert lup +log_message_info+ "INFO")
@@ -32,12 +37,16 @@
   (hmap-insert lup +log_message_critical+ "CRITICAL")
 
   (defun-bind log-write (&rest _)
+=======
+  (defun log-write (&rest _)
+>>>>>>> parent of b4694fe0... log-msg writer
     ; (log-write ....) -> stream
     ; Wrap timestamp and nl to '_' arguments
     (setq _ (insert (push _ +nl+) 0 (list (encode-date (date)))))
     (write fs (apply str _))
     (stream-flush fs))
 
+<<<<<<< HEAD
   (defun-bind log-msg-writer (msg)
     ; (log-msg-writer mail-message) -> stream
     (defq
@@ -46,11 +55,21 @@
     (log-write (str
                  " [" (hmap-find lup (getp msgd :msg-type))"] "
                  (getp cnfg :name)": ") (getp msgd :message)))
+=======
+  (defun-bind deser-inbound (msg)
+    (yaml-xdeser (write (string-stream (cat "")) (slice mail_msg_data -1 msg))))
+
+  ; (log-write "apps/logger/logsrvc.yaml " (age "apps/logger/logsrvc.yaml"))
+>>>>>>> parent of b4694fe0... log-msg writer
 
   (defun-bind register-logger (config)
     ; (register-logger properties) -> ?
     (defq hsh (hash config))
+<<<<<<< HEAD
     (log-write " Registering " (getp config :name))
+=======
+    (log-write "Registering" (getp config :name))
+>>>>>>> parent of b4694fe0... log-msg writer
     (hmap-insert reg hsh config)
     (setp! config :token hsh t)
     (mail-send
@@ -69,11 +88,15 @@
       ; Information request about registrations (admin)
       ; Registration (client)
       ((= id +log_event_register+)
-       (register-logger (deser-inbound msg)))
+       (defq msgd (deser-inbound msg))
+       (register-logger msgd))
       ; Reconfiguration (client)
       ; Log Message (client)
       ((= id +log_event_logmsg+)
-        (log-msg-writer msg))
+        (defq
+          msgd  (deser-inbound msg)
+          mname (getp (hmap-find reg (getp msgd :module)) :name))
+        (log-write mname (getp msgd :message) (slice mail_msg_data -1 msg)))
       ; Should throw exception
       (t
         (log-write " Unknown " msg))))
