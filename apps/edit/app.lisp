e5ea285ebbca081971cd1a0d9adae669405eb627
Nagasaki Nightmare <nuclearfall@protonmail.com>
2020-10-12 18:52:04 -0500

    edit bug fixes

--- apps/edit/app.lisp
+++ apps/edit/app.lisp
@@ -6,7 +6,6 @@
 (import "apps/edit/find.inc")
 (import "lib/substr/substr.inc")
 
-
 ;alt,ctl,cmd, and shift key codes.
 (defq left_shift 0x400000E1 left_ctrl_key 0x400000E0 left_alt_key 0x400000E2 left_cmd_key 0x400000E3
 	right_shift 0x400000E5 right_alt_key 0x400000E6  right_cmd_key 0x400000E7)
@@ -21,22 +20,21 @@
 ;text structure
 (structure '+text 0
 	(byte 'index+ 'fpath+ 'title+ 'buffer+ 'position+))
- 
+;select is a an array using the +mbox structure: task+ file+ modal+
 (defq vdu_min_width 40 vdu_min_height 24 vdu_width 60 vdu_height 40 text_store (list) tmp_num 0
-	current_text (list) empty_buffer '("") home_dir (cat "apps/login/" *env_user*)
+	current_text (list) empty_buffer '("") home_dir (cat "apps/login/" *env_user* "/")
 	picker_mbox nil picker_mode nil select (array (task-mailbox) (mail-alloc-mbox) (mail-alloc-mbox))
 	find_list (list) find_index 0 sb_line_col_message "")
-;set views for buffer left margin.
+
+
 (ui-window window (:border 4)
-	(ui-flow _ (:flow_flags flow_right_fill) (ui-label _ (:text "" :min_width 4))
-		(ui-title-bar window_title "Edit" (0xea19 0xea1b 0xea1a) +event_close+))
+		(ui-title-bar window_title "Edit" (0xea19 0xea1b 0xea1a) +event_close+)
 		(ui-flow window_flow (:flow_flags flow_down_fill)
 			(ui-flow toolbar (:flow_flags flow_right_fill)
 				(ui-grid _ (:flow_flags flow_flag_align_hleft :grid_width 6 :grid_height 1 :font *env_toolbar_font*)
 					(each (lambda (c e)
-							(component-connect (ui-button _ (:font *env_medium_toolbar_font* :text (num-to-utf8 c)
-								 :min_height 32 :min_width 32 :border 0)) e))
-						'(0xe999 0xea07 0xe94c 0xe94e 0xe91d 0xe91e)
+						(component-connect (ui-button _ (:font *env_medium_toolbar_font* :text (num-to-utf8 c)
+							:min_height 32 :min_width 32 :border 0)) e)) '(0xe999 0xea07 0xe94c 0xe94e 0xe91d 0xe91e)
 							(list +event_open+ +event_save+ +event_closeb+ +event_new+ +event_prev+ +event_next+)))
 				(ui-flow _ (:flow_flags flow_left_fill)
 					(ui-grid _ (:flow_flag flow_flag_align_hright :grid_width 3 :grid_height 1)
@@ -48,17 +46,14 @@
 							:text (num-to-utf8 0xe988) :border 0)) +event_clear_text+)
 					(ui-textfield textfield (:border 0 :text ""))))
 				(ui-flow status_bar (:flow_flags flow_right_fill)
-					(ui-label sb_left_pad (:min_width 18))
 					(ui-label sb_line_col (:flow_flags flow_flag_align_hleft :font *env_body_font*  :text "Line xx, Column xx. "))
-					(ui-label _ (:min_width 48))
 					(ui-label sb_buf_disp (:flow_flags flow_flag_align_hright :text "Buffer 0/0" :font *env_body_font*)))
 			(ui-flow _ (:flow_flags flow_right_fill)
 				(ui-label _ (:min_width 36))
 				(ui-flow _ (:border 0 :flow_flags flow_left_fill)
 					(component-connect (ui-slider slider (:flow_flags flow_down_fill :color 0x22cccccc :border 0)) +event_scroll+)
-					(ui-vdu vdu (:vdu_width vdu_width :vdu_height vdu_height :min_width vdu_width :min_height vdu_height
-						:font *env_terminal_font*))))))		
-
+					(ui-vdu vdu (:ink_color 0x33000000 :vdu_width vdu_width :vdu_height vdu_height :min_width vdu_width :min_height vdu_height
+						:font *env_terminal_font*))))))
 
 (defun-bind window-resize (w h)
 	(bind '(_ fpath title buffer position) current_text)
@@ -75,6 +70,7 @@
 	(bind '(index fpath title buffer position) current_text)
 	(bind '(ox oy cx cy sx) position)
 	;for display purposes, index starts at 1.
+	(set window_title :text fpath)
 	(set sb_line_col :text (cat "Line " (str cy) ", Column " (str cx) sb_line_col_message))
 	(set sb_buf_disp :text (cat "Buffer " (str (inc index)) "/" (str (length text_store))))
 	(setq vdu_width w vdu_height h)
@@ -82,12 +78,11 @@
 	(bind '(x y) (view-get-pos vdu))
 	(bind '(w h) (view-pref-size vdu))
 	(set vdu :min_width vdu_min_width :min_height vdu_min_height)
-	(view-layout textfield)
 	(view-change vdu x y w h)
-	(set window_title :text title)
-	(view-layout window_title)
 	;set slider and textfield values
 	(def slider :maximum (max 0 (- (length buffer) vdu_height)) :portion vdu_height :value oy)
+	(view-layout slider)
+	;a single view-layout for the window
 	(view-layout window)
 	(view-dirty-all window)
 	(vdu-load vdu buffer ox oy cx cy))
@@ -105,14 +100,14 @@
 	(elem-set +text_buffer+ current_text buffer)
 	(elem-set +text_position+ current_text (list ox oy cx cy sx)))
 ;issue commands or search on textfield substr
-(defmacro-bind select-action-on-enter (f)
-	`(let ((tf_text (get :text ,f)))
+(defun-bind select-action-on-enter (f)
+	(let ((tf_text (get :text f)))
 		(cond
 			((and (starts-with "(save:" tf_text) (ends-with ")" tf_text)) 
 				(save-check (slice 6 -2 tf_text)) (set textfield :text ""))
 			((eql tf_text "(save)") (save-buffer (elem +text_fpath+ current_text))
 				(set sb_line_col :text "Saving...") (view-dirty sb_line_col)
-				(task-sleep 250000))
+				(task-sleep 150000))
 			((eql tf_text "(new)") (setq current_text (new-buffer)) (set textfield :text ""))
 			((eql tf_text "(close)") (close-buffer (elem +text_index+ current_text))
 				(set textfield :text ""))
@@ -127,7 +122,7 @@
 				(set textfield :text (slice 6 -2 tf_text)) (find-next))
 			(t (find-next)))
 		(window-layout vdu_width vdu_height)))
-;returns position just below the toolbar
+;returns position just below the statusbars
 (defun-bind notification-position ()
 	(bind '(tw th) (view-get-size window_title))
 	(bind '(sbw sbh) (view-get-size status_bar))
@@ -135,57 +130,68 @@
 	(bind '(x y w h) (view-get-bounds window))
 	(defq brdr (get :border window) x (+ x brdr) y (+ y brdr th tbh sbh))
 	(list x y tbw th))
+
+(defun-bind confirm (m b &optional c)
+	(bind '(x y w h) (notification-position))
+	(mail-send (list (elem +mbox_modal+ select)
+		m b (if c c *env_toolbar2_col*) 0 x y w h)
+	(defq modal (open-child "apps/messagebar/child.lisp" kn_call_open)))
+	(defq reply (mail-read (elem +mbox_modal+ select)))
+	(mail-send "" modal) (if (str? reply) reply nil))
+
+(defun-bind notify (m c &optional s)
+	(bind '(x y w h) (notification-position))
+	(mail-send (list (elem +mbox_modal+ select)
+		m "" (if c c *env_toolbar2_col*) (if s s 1500000) x y w h)
+	(defq modal (open-child "apps/messagebar/child.lisp" kn_call_open)))
+	(mail-send "" modal))
+
 ;create a new, unsaved buffer or open buffer created at optional path
-(defun-bind new-buffer (&optional path)
+(defun-bind new-buffer (&optional nfpath)
 	(defq index (length text_store) pos (list 0 0 0 0 0) 
-			title (cat "Untitled-" (str (setq tmp_num (inc tmp_num))))
-			buffer (list (list (join " " (ascii-char 10)))) fpath (if path path (cat home_dir title)))
+		title (cat "Untitled-" (str (setq tmp_num (inc tmp_num))))
+		buffer (list " ") fpath (if nfpath nfpath (cat home_dir title)))
+	(if nfpath (save-buffer nfpath))
 	(push text_store (list index fpath title buffer pos))
-	(elem index text_store))
+	(setq current_text (elem index text_store)))
 
 (defun-bind open-buffer (fpath)
 	(defq i 0 index (length text_store) pos (list 0 0 0 0 0))
 	(defq title fpath buffer (list))
-	(each-line (lambda (_) (push buffer _)) (file-stream fpath))
+	;ensure something to read on empty file.
+	(if (eql nil (read-line (file-stream fpath))) (setq buffer (list ""))
+	(each-line (lambda (_) (push buffer _)) (file-stream fpath)))
 	(push text_store (list index fpath title buffer pos))
 	(setq current_text (elem index text_store)))
 ;create and open with confirmation, move to open buffer, or open existing buffer.
 (defun-bind open-check (fpath)
 	(cond
-		((not (file-stream fpath))
-			(bind '(x y w h) (notification-position))
-			(mail-send (list (elem +mbox_modal+ select)
-				(cat "Create a new file at " fpath "?") "Yes, No" 0xffd7b61d 0 x y w h)
-			(defq modal (open-child "apps/messagebar/child.lisp" kn_call_open)))
-			(defq reply (mail-read (elem +mbox_modal+ select)))
-			(mail-send "" modal)
-			(when (eql reply "Yes") (save (join " " (ascii-char 10)) fpath) (new-buffer fpath)))
-		((defq index (some (lambda (_) (if (eql fpath (elem +text_fpath+ _)) _ nil)) text_store))
+		((defq index (some (lambda (_) (if (eql fpath (elem +text_fpath+ _)) (elem +text_index+ _) nil)) text_store))
 			(when index (move-to index)))
-		(t (setq current_text (open-buffer fpath)))))
+		((not (file-stream fpath))
+			(when (eql "Yes" (confirm (cat "Create and opene new file " fpath "?") "Yes,No" argb_yellow))
+				(new-buffer fpath)))
+		(t (open-buffer fpath))))
 ;save or save as, with confirmation
 (defun-bind save-check (fpath)
 	(cond
-		((eql fpath (elem +text_fpath+ current_text)) (save-buffer fpath))
-		(t
-			(bind '(x y w h) (notification-position))
-			(mail-send (list (elem +mbox_modal+ select)
-				"This will overwrite the existing file. Are You Sure?" "Yes, No" argb_red 0 x y w h)
-			(defq modal (open-child "apps/messagebar/child.lisp" kn_call_open)))
-			(defq reply (mail-read (elem +mbox_modal+ select)))
-			(mail-send "" modal)
-			(when (eql reply "Yes")
-				(save-buffer fpath)))))
+		((and (not (eql fpath (elem +text_fpath+ current_text))) (file-stream fpath))
+			(when (eql "Yes" (confirm "Overwrite existing file?" "Yes, No" argb_red))
+				(save-buffer fpath)))
+		((not (file-stream fpath))
+			(notify (cat "File saved to new path: " fpath ".") argb_yellow)
+				(save-buffer fpath))
+		(t (save-buffer fpath))))
 
 (defun-bind save-buffer (fpath)
-	(unless (or (eql fpath "") (ends-with "/" fpath))		
-			(defq save_buffer (join (elem +text_buffer+ current_text) (const (ascii-char 10))))
-			(save save_buffer fpath)
-			(elem-set +text_title+ current_text fpath)
-			(elem-set +text_fpath+ current_text fpath)))
+	(unless (or (eql fpath "") (ends-with "/" fpath) (find ":" fpath))
+		(defq save_buffer (join (elem +text_buffer+ current_text) (const (char 10))))
+		(save save_buffer fpath)
+		(elem-set +text_title+ current_text fpath)
+		(elem-set +text_fpath+ current_text fpath)))
 
 (defun-bind move-to (index)
-	(unless (= (elem +text_index+ (elem (dec index) text_store)) (elem +text_index+ current_text))
+	(when (< -1 index (length text_store))
 		(setq current_text (elem index text_store))))
 
 (defun-bind close-buffer (index)
@@ -232,7 +238,6 @@
 	(view-dirty sb_line_col)
 	(view-dirty slider))
 
-
 (defun-bind main ()
 	(defq cx 0 cy 0 index 0 find_textfield nil)
 	;open buffers from pupa or open new buffer
@@ -259,14 +264,12 @@
 						;(set textfield :text msg)
 						(window-layout vdu_width vdu_height))
 				;load buffer
-				(t	(setq current_text (open-buffer msg))
-					;(set textfield :text (elem +text_fpath+ current_text))
+				(t	(open-check msg)
 					(window-layout vdu_width vdu_height))))
 		((= (setq id (get-long msg (const ev_msg_target_id))) +event_close+)
 			(setq id nil))
 		((= id +event_new+)
-			(setq current_text (new-buffer))
-			;(set textfield :text (cat "(save:" home_dir "/" (elem +text_fpath+ current_text) ")"))
+			(new-buffer)
 			(window-layout vdu_width vdu_height))
 		((= id +event_save+)
 			(if picker_mbox (mail-send "" picker_mbox))
@@ -319,12 +322,13 @@
 						mouse_xy (list rx ry))
 					(mouse-cursor mouse_xy)))
 			(window-layout vdu_width vdu_height))
+		((and (= id (component-get-id textfield))
+			(= (get-long msg ev_msg_type) ev_type_key)
+			(> (get-int msg ev_msg_key_keycode) 0)
+			(or (= (get-int msg ev_msg_key_key) 13) (= (get-int msg ev_msg_key_key) 10)))
+			(select-action-on-enter textfield))
 		(t	
 			;capture the enter key for commands and search from the textfield.
-			(and (= (get-long msg ev_msg_type) ev_type_key)
-				(> (get-int msg ev_msg_key_keycode) 0)
-				(= (get-int msg ev_msg_key_key) 13) 
-				(select-action-on-enter textfield))
 			(view-event window msg))))
 	(if picker_mbox (mail-send "" picker_mbox))
 	(view-hide window))
