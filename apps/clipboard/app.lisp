507e701224dfdfbc85da2c671bb1eab6953dd517
Nagasaki Nightmare <nuclearfall@protonmail.com>
2020-11-17 12:52:19 -0600

    cut cursor placement and clean-up

--- apps/clipboard/app.lisp
+++ apps/clipboard/app.lisp
@@ -12,7 +12,6 @@
 	(defq id t select (array (task-mailbox) (mail-alloc-mbox)) clipboard (list) +req_get+ 0 +req_put+ 1)
 	;Start a new clipboard service, only if one isn't currently running.
 	(defq clip_service (mail-declare "CLIPBOARD_SERVICE" (elem 1 select) "Clipboard Service"))
-	(options-print "Clipboard Service Opened")
 	(while id
 		(defq idx (mail-select select) msg (mail-read (elem idx select)))
 		(cond
@@ -21,10 +20,8 @@
 					((eql (defq req_type (first msg)) "GET")
 						(unless (empty? clipboard)
 							(defq reply (last clipboard))
-							(options-print "GET: " reply)
 							(mail-send reply (second msg))))
 					((eql req_type "PUT")
-						(options-print "PUT: " (second msg))
 						(push clipboard (second msg)))))
 			((= (setq id (get-long msg ev_msg_target_id)) +event_close+)
 				;close
