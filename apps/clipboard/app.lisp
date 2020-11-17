3e88115bbf818c6db1e844ee0f2377218eefc58d
nuclearfall <41907342+nuclearfall@users.noreply.github.com>
2020-11-17 13:47:08 -0600

    Basic cut/copy/paste functionality and clipboard service (#201)
    * Adding clipboard service.
    
    * adding clipboard service
    
    * cleaned up select-action code. Added cut text.
    
    * cut cursor placement and clean-up
    
    * remove stray print.
    
    * remove unused notification file

---
+++ apps/clipboard/app.lisp
@@ -0,0 +1,31 @@
+;imports
+(import "sys/lisp.inc")
+(import "class/lisp.inc")
+(import "gui/lisp.inc")
+(import "lib/options/options.inc")
+
+(structure '+event 0
+	(byte 'close+)
+	(byte 'deliver+ 'paste+))
+
+(defun main ()
+	(defq id t select (array (task-mailbox) (mail-alloc-mbox)) clipboard (list) +req_get+ 0 +req_put+ 1)
+	;Start a new clipboard service, only if one isn't currently running.
+	(defq clip_service (mail-declare "CLIPBOARD_SERVICE" (elem 1 select) "Clipboard Service"))
+	(while id
+		(defq idx (mail-select select) msg (mail-read (elem idx select)))
+		(cond
+			((/= idx 0)
+				(cond
+					((eql (defq req_type (first msg)) "GET")
+						(unless (empty? clipboard)
+							(defq reply (last clipboard))
+							(mail-send reply (second msg))))
+					((eql req_type "PUT")
+						(push clipboard (second msg)))))
+			((= (setq id (get-long msg ev_msg_target_id)) +event_close+)
+				;close
+				(setq id nil))))
+	;shutdown, if requested.
+	(mail-free-mbox (pop select))
+	(mail-forget clip_service))
