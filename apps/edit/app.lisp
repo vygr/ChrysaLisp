9b784d87f13614a0cfa726bc59fb6309901f911a
Nagasaki Nightmare <nuclearfall@protonmail.com>
2020-11-17 11:48:48 -0600

    cleaned up select-action code. Added cut text.

--- apps/edit/app.lisp
+++ apps/edit/app.lisp
@@ -18,7 +18,7 @@
 (structure '+event 0
 	(byte 'close+ 'max+ 'min+ 'resize+ 'layout+ 'scroll+)
 	(byte 'new+ 'save+ 'open+ 'run+ 'closeb+ 'prev+ 'next+ 'find+ 'colorise+)
-	(byte 'copy+ 'paste+)
+	(byte 'cut+ 'copy+ 'paste+)
 	(byte 'action+ 'find+ 'find_prev+ 'find_next+ 'clear_text+)
 	(byte 'menu+ 'menu_click+)
 	(byte 'close_tab+ 'tabbar+))
@@ -37,8 +37,8 @@
 	current_text (list) home_dir (cat "apps/login/" *env_user* "/") picker_mbox nil  picker_mode nil 
 	mbox_array (array (task-mailbox) (mail-alloc-mbox) (mail-alloc-mbox) (mail-alloc-mbox)) 
 	find_list (list) find_index 0 tabbar (Flow) unsaved_buffers (list)  burger_open nil
-	status_bar_msg "" tb_font (create-font "fonts/Entypo.ctf" 20) cmd_menu (Window)
-	cmd_menu_grid (Grid) cmd_menu_up nil syn (Syntax) colorise t dirty_vdu t display_buffer (list))
+	status_bar_msg "" tb_font (create-font "fonts/Entypo.ctf" 20) cmd_return ""
+	cmd_menu_up nil syn (Syntax) colorise t dirty_vdu t display_buffer (list))
 
 (ui-window window (:color +argb_grey2+)
 	(ui-title-bar window_title "Edit" (0xea19 0xea1b 0xea1a) +event_close+)
@@ -53,12 +53,10 @@
 				(component-connect (ui-button srch (:border 0 :text (num-to-utf8 0xe9cd) 
 					:font *env_small_toolbar_font* :color +argb_white+)) +event_find+)
 				(ui-flow _ (:flow_flags +flow_left_fill+)
-					(component-connect (ui-button burger (:text (num-to-utf8 0xe9d4) 
-						:min_width 32 :border 0 :font *env_small_toolbar_font*)) +event_menu+)
-					(component-connect (ui-button paste_button (:font *env_medium_toolbar_font*
-						:text (num-to-utf8 0xe952) :border 0 :min_width 32)) +event_paste+)
-					(component-connect (ui-button copy_button (:font *env_medium_toolbar_font*
-						:text (num-to-utf8 0xe962) :border 0 :min_width 32)) +event_copy+)
+					(each (lambda (c e) 
+						(component-connect (ui-button _ (:font *env_medium_toolbar_font*
+							:text (num-to-utf8 c) :min_height 32 :min_width 32 :border 0)) e))
+						'(0xe9d4 0xe952 0xe962 0xea08) (list +event_menu+ +event_paste+ +event_copy+ +event_cut+))
 					(each (lambda (c e)
 						(component-connect (ui-button clrtxt (:border 0 :text (num-to-utf8 c) 
 							:font *env_small_toolbar_font* :color +argb_white+)) e))
@@ -71,7 +69,7 @@
 			(ui-flow tabbar_flow (:flow_flags +flow_right_fill+ :font *env_window_font*)
 				(component-connect (ui-button _ (:border 0 :text (num-to-utf8 0xe94e) :font tb_font)) +event_new+)))
 		(ui-flow status_bar (:color *env_toolbar_col* :flow_flags +flow_right_fill+)
-			(ui-label sb_label (:font *env_body_font*  :text "Line XX, Column XX")))
+			(ui-label sb_label (:font *env_body_font* :min_height 24 :text "Line XX, Column XX")))
 		(ui-flow vdu_flow (:border 0 :flow_flags +flow_left_fill+)
 			(component-connect (ui-slider slider 
 				(:flow_flags +flow_down_fill+ :border 0)) +event_scroll+)
@@ -164,20 +162,20 @@
 	(if (and (starts-with "(" cd) (ends-with ")" cd) (not (starts-with "(find:" cd)))
 		(split (slice 1 -2 cd) ":") (list (cat "" cd))))
 
-(defun select-action-on-enter (a)
+(defun select-action (a)
 	(defq cmd (first (split-cd a)) data (second (split-cd a)) is_find nil is_dirty nil)
 	(cond
-		((eql cmd "save") (when (not data) (defq data (elem +text_fpath+ current_text)))
+		((eql cmd "save") (if (not data) (defq data (elem +text_fpath+ current_text)))
 			(save-check data))
+		((eql cmd "save-as") (if (not data) (on-save-file) (save-check data)))
 		((eql cmd "save-all")
 			(each (lambda (_) (save-check (elem +text_fpath+ _))) text_store))
 		((eql cmd "new") (new-buffer))
 		((eql cmd "close") (close-buffer (elem +text_index+ current_text)))
-		((eql cmd "close-all") 
+		((eql cmd "exit") 
 			(each (lambda (_) (close-check (elem +text_index+ _))) text_store))
-		((eql cmd "open") (when (not data) (defq data (elem +text_fpath+ current_text)))
-			(set mytextfield :text "")
-			(open-check data))
+		((eql cmd "open") (if (not data) (on-open-file)
+			(open-check data)))
 		((eql cmd "prev") (prev-buffer (elem +text_index+ current_text))
 			(setq status_bar_msg ""))
 		((eql cmd "next") (next-buffer (elem +text_index+ current_text))
@@ -201,60 +199,16 @@
 	(bind '(x y w h) (. window :get_bounds))
 	(defq brdr (get :border window) x (+ x brdr) y (+ y brdr th tbh))
 	(list x y tbw th))
-; ;bar with buttons
-(defun confirm (m b &optional c)
-	(bind '(x y w h) (notification-position))
-	(mail-send (list (elem +mbox_dialog+ mbox_array)
-		m b (if c c *env_toolbar2_col*) 0 x y w h)
-		(defq modal (open-child "apps/edit/notification.inc" kn_call_open)))
-	(defq reply (mail-read (elem +mbox_dialog+ mbox_array)))
-	(mail-send "" modal) (if (str? reply) reply nil))
-; ;bar with timer and without buttons. Default is 1.5 seconds.
-; (defun notify (m &optional c s)
-; 	(bind '(x y w h) (notification-position))
-; 	(mail-send (list (elem +mbox_dialog+ mbox_array)
-; 		m "" (if c c *env_toolbar2_col*) (if s s 1500000) x y w h)
-; 		(defq modal (open-child "apps/edit/notification.lisp" kn_call_open)))
-; 	(defq reply (mail-read (elem +mbox_dialog+ mbox_array)))
-; 	(mail-send "" modal))
 
-;notify, confirm, on open, save, and close.
 (defun open-check (fpath)
 	(open-buffer fpath))
-	; (cond
-	; 	((defq index (some (lambda (_) 
-	; 		(if (eql fpath (elem +text_fpath+ _)) (elem +text_index+ _) nil)) text_store))
-	; 		(when index (move-to-buffer index)))
-	; 	((not (file-stream fpath))
-	; 		(when (eql "Yes" (confirm "File does not exist. Create new file?" 
-	; 			"Yes,No" +argb_yellow+)) (new-buffer fpath)))
-	; 	(t 	(open-buffer fpath))))
 
 (defun save-check (fpath)
 	(save-buffer fpath))
-	; (cond
-	; 	((and (not (eql fpath (elem +text_fpath+ current_text))) (file-stream fpath))
-	; 		(when (eql "Yes" (confirm "Overwrite existing file?" "Yes, No" +argb_red+))
-	; 			(save-buffer fpath)))
-	; 	((not (file-stream fpath))
-	; 		(notify (cat "Saving file to new path: " fpath ".") +argb_yellow+)
-	; 		(save-buffer fpath))
-	; 	(t 	(save-buffer fpath))))
 
 (defun close-check (index)
-	; (close-buffer index))
-	(defq reply nil)
-	(cond 
-		((unsaved-buffer index)
-			(defq reply (confirm "Buffer not saved since last edit." 
-				"Save,Close,Cancel" +argb_yellow+))
-			(cond 
-				((eql reply "Save") 
-					(save-check (elem +text_fpath+ current_text))
-					(close-buffer (remove-from-unsaved-buffers index)))
-				((eql reply "Close")
-					(close-buffer (remove-from-unsaved-buffers index)))))
-		(t (close-buffer index))))
+	(close-buffer index))
+	
 			
 ;sets the tab bar title to the last two parts of the path.
 (defmacro title-set (fp) 
@@ -279,15 +233,12 @@
 	(move-to-buffer index))
 
 (defun save-buffer (fpath)
-	(cond 
-		((or (eql fpath "") (ends-with "/" fpath) (find ":" fpath))
-			(notify "Invalid filename. Buffer not saved" +argb_yellow+))
-		(t 	(defq save_buffer (join (elem +text_buffer+ current_text) (const (char 10))))
-			(save save_buffer fpath)
-			(remove-from-unsaved-buffers (elem +text_index+ current_text))
-			(when (not (eql (elem +text_fpath+ current_text) fpath))
-				(defq fp (elem-set +text_fpath+ current_text fpath))
-				(elem-set +text_fpath+ current_text (title-set fp))))))
+	(defq save_buffer (join (elem +text_buffer+ current_text) (const (char 10))))
+	(save save_buffer fpath)
+	(remove-from-unsaved-buffers (elem +text_index+ current_text))
+	(when (not (eql (elem +text_fpath+ current_text) fpath))
+		(defq fp (elem-set +text_fpath+ current_text fpath))
+		(elem-set +text_fpath+ current_text (title-set fp))))
 
 (defun close-buffer (index)
 	(defq i 0)
@@ -339,7 +290,7 @@
 		((or (= c 10) (= c 13))		(return) (setq cx 0) (add-to-unsaved-buffers index))
 		((= c 8)					(backspace) (setq sx cx) (add-to-unsaved-buffers index))
 		((or (= c 9) (<= 32 c 127))	(printable c) (setq sx cx) (add-to-unsaved-buffers index))
-		((= c 0x40000050)			(left) (setq sx cx))
+		((= c 0x40000050)			(left) (setq sx cx) (setq))
 		((= c 0x4000004f)			(right) (setq sx cx))
 		((= c 0x40000052)			(up))
 		((= c 0x40000051)			(down))
@@ -347,14 +298,28 @@
 	;only reload if a key actually alters vdu.
 	(cursor-visible)
 	(set-sticky)
-	(when dirty_vdu (setq dirty_vdu nil)
-		(set slider :value oy)
-		(elem-set +text_buffer+ current_text buffer)
-		(elem-set +text_position+ current_text (list ox (setq oy (get :value slider)) cx cy sx))
-		(set sb_label :text (cat "Line " (str cy) ", Column " (str cx) status_bar_msg))
-		(. (. sb_label :layout) :dirty)
-		(. slider :dirty)
-		(vdu-colorise)))
+	(when (. text_select :find :selection_mode)
+		(defq fxy_lxy (. text_select :find :selection))
+		(setq buffer (cut-text buffer fxy_lxy)))
+	(set slider :value oy)
+	(elem-set +text_buffer+ current_text buffer)
+	(elem-set +text_position+ current_text (list ox (setq oy (get :value slider)) cx cy sx))
+	(set sb_label :text (cat "Line " (str cy) ", Column " (str cx) status_bar_msg))
+	(. (. sb_label :layout) :dirty)
+	(. slider :dirty)
+	(vdu-colorise))
+
+(defun on-save-file ()
+	(if picker_mbox (mail-send "" picker_mbox))
+	(mail-send (list (elem +mbox_file+ mbox_array) "Save Buffer..." "."  "")
+			(setq picker_mode t picker_mbox 
+					(open-child "apps/files/child.lisp" kn_call_open))))
+
+(defun on-open-file ()
+	(if picker_mbox (mail-send "" picker_mbox))
+	(mail-send (list (elem +mbox_file+ mbox_array) "Load Buffer..." "." "")
+				(setq picker_mode nil picker_mbox 
+					(open-child "apps/files/child.lisp" kn_call_open))))
 
 (defun main ()
 	(defq id t find_textfield nil mouse_down nil selection (list) 
@@ -372,36 +337,31 @@
 	(while id
 		(defq msg (mail-read (elem (defq idx (mail-select mbox_array)) mbox_array)))
 		(cond
-		((= idx +mbox_dialog+)
+		((= idx +mbox_file+)
 			(mail-send "" picker_mbox)
 			(setq picker_mbox nil)
 			(cond
 				((eql msg ""))
 				(picker_mode
-					(save-check msg)
-					(elem-set +text_fpath+ current_text msg)
+					;remove leading "./"
+					(save-check (slice 2 -1 msg))
 					(window-layout vdu_width vdu_height))
-				(t	(open-check msg)
+				(t	(open-check (slice 2 -1 msg))
 					(window-layout vdu_width vdu_height))))
 		((= idx +mbox_clip+)
 			;clipboard makes no reply if it is empty.
-			(options-print msg)
 			(on-paste msg))
 		((= (setq id (get-long msg (const ev_msg_target_id))) +event_close+) 
 			(setq id nil))
 		((= id +event_new+) (new-buffer) (window-layout vdu_width vdu_height))
-		((= id +event_save+) (if picker_mbox (mail-send "" picker_mbox))
-			(mail-send (list (elem +mbox_dialog+ mbox_array) "Save Buffer..." "."  "")
-				(setq picker_mode t picker_mbox 
-					(open-child "apps/files/child.lisp" kn_call_open))))
-		((= id +event_open+) (if picker_mbox (mail-send "" picker_mbox))
-			(mail-send (list (elem +mbox_dialog+ mbox_array) "Load Buffer..." "." "")
-				(setq picker_mode nil picker_mbox 
-					(open-child "apps/files/child.lisp" kn_call_open))))
+		((= id +event_save+) 
+			(on-save-file))
+		((= id +event_open+) 
+			(on-open-file))
 		((= id +event_run+) (open-child (elem +text_fpath+ current_text) kn_call_open))
 		((= id +event_closeb+) (close-check (elem +text_index+ current_text))
 			(window-layout vdu_width vdu_height))
-		((= id +event_action+) (select-action-on-enter (get :text mytextfield)))
+		((= id +event_action+) (select-action (get :text mytextfield)))
 		((= id +event_find+) 
 			(if (> (length find_list) 0)
 				(progn (setq find_list (list) find_index 0) (open-find)) (open-find)))
@@ -412,6 +372,13 @@
 			(defq fxy_lxy (. text_select :find :selection))
 			(. text_select :insert :copied (copy-text (get-buffer) fxy_lxy))
 			(mail-send (list "PUT" (copy-text (get-buffer) fxy_lxy)) clipboard_mbox))
+		((= id +event_cut+)
+			(defq fxy_lxy (. text_select :find :selection))
+			(. text_select :insert :copied (copy-text (get-buffer) fxy_lxy))
+			(mail-send (list "PUT" (copy-text (get-buffer) fxy_lxy)) clipboard_mbox)
+			(defq buffer (cut-text (get-buffer) fxy_lxy))
+			(bind '(fx fy) (first fxy_lxy))
+			(set-buffer buffer) (vdu-colorise))
 		((= id +event_paste+)
 			(mail-send (list "GET" (elem +mbox_clip+ mbox_array)) clipboard_mbox))
 		((= id +event_colorise+) (if colorise (setq colorise nil) (setq colorise t)) 
@@ -435,29 +402,16 @@
 			(move-to-buffer (- id +event_tabbar+))
 			(window-layout vdu_width vdu_height))
 		((= id +event_menu+)
-			(defq cmd_list '("new" "open" "save" "run" "close" "exit"))
+			(defq cmd_list '("new" "open" "save-as" "run" "close" "exit"))
 			(bind '(x y w h) (notification-position))
 			(bind '(wx wy ww wh) (apply view-locate (. window :pref_size)))
 			;pos is the rightmost, uppermost position of the menu.
 			(defq pos (list (+ x w) y))
 			(mail-send (list (elem +mbox_task+ mbox_array) cmd_list pos :top_right)
-				(defq menu_mbox (open-child "apps/edit/menu.lisp" kn_call_open)))
+					(defq menu_mbox (open-child "apps/edit/menu.lisp" kn_call_open)))
 			(defq reply (mail-read (elem +mbox_task+ mbox_array)))
-			(cond 
-				((eql "new" reply) (new-buffer) (window-layout vdu_width vdu_height))
-				((eql "open" reply) (if picker_mbox (mail-send "" picker_mbox))
-					(mail-send (list (elem +mbox_dialog+ mbox_array) "Load Buffer..." "." "")
-					(setq picker_mode nil picker_mbox 
-						(open-child "apps/files/child.lisp" kn_call_open))))
-				((eql "save" reply) (if picker_mbox (mail-send "" picker_mbox))
-					(mail-send (list (elem +mbox_dialog+ mbox_array) "Save Buffer..." "."  "")
-					(setq picker_mode t picker_mbox 
-						(open-child "apps/files/child.lisp" kn_call_open))))
-				((eql "run" reply) (open-child (elem +text_fpath+ current_text) kn_call_open))
-				((eql "close" reply) (close-check (elem +text_index+ current_text)))
-				((eql "exit" reply) (each (lambda (_) (close-check (elem +text_index+ current_text))) text_store)
-					(setq id nil)))
-			(mail-send "" menu_mbox))
+			(mail-send "" menu_mbox)
+			(select-action (cat "(" reply ")")))
 		((= id (component-get-id vdu))
 			(vdu-keyboard msg)
 			(vdu-mouse-down msg)
