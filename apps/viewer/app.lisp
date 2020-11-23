d2af6e7282536024545e0d8bf84ab4d413af6430
Chris Hinsley <chris.hinsley@gmail.com>
2020-11-23 13:02:39 +0000

    first stage of Tree widget

--- apps/viewer/app.lisp
+++ apps/viewer/app.lisp
@@ -11,7 +11,7 @@
 
 (defq vdu_min_width 16 vdu_min_height 16
 	vdu_max_width 120 vdu_max_height 50
-	vdu_width 60 vdu_height 50
+	vdu_width 80 vdu_height 50
 	text_buf nil syntax (Syntax) scroll_positions (xmap 101)
 	current_file nil current_button nil)
 
@@ -19,7 +19,7 @@
 	(ui-title-bar _ "Viewer" (0xea19 0xea1b 0xea1a) +event_close+)
 	(ui-flow _ (:flow_flags +flow_right_fill+ :font *env_terminal_font*)
 		(ui-scroll tree_scroll +scroll_flag_vertical+ nil
-			(ui-flow tree (:flow_flags (logior +flow_flag_down+ +flow_flag_fillw+))))
+			(ui-tree tree (:action_event +event_file_button+ :min_width 0 :color +argb_white+)))
 		(ui-flow _ (:flow_flags +flow_left_fill+)
 			(component-connect (ui-slider slider) +event_scroll+)
 			(ui-vdu vdu (:min_width vdu_width :min_height vdu_height
@@ -54,7 +54,7 @@
 				(apply cat (map (# (if (eql %0 (ascii-char 9)) "    " %0)) line))
 				line))))
 		(file-stream file))
-	(. vdu :load text_buf 0 (set-slider file) 0 10000))
+	(. vdu :load text_buf 0 (set-slider file) 0 -1))
 
 (defun populate-tree ()
 	;load up the file tree and the first file
@@ -63,14 +63,11 @@
 			(ends-with ".vp" %0)
 			(ends-with ".inc" %0)
 			(ends-with ".lisp" %0))) (all-files ".")))))
-	(each (#
-		(def (defq entry (Button)) :text %0 :border 0 :color +argb_white+)
-		(component-connect entry +event_file_button+)
-		(. tree :add_child entry)) all_src_files)
+	(each (# (. tree :add_route %0)) all_src_files)
 	(each (# (. scroll_positions :insert %0 0)) all_src_files)
 	(populate-vdu (elem 0 all_src_files)))
 
-(defun window-layout (w h)
+(defun window-resize (w h)
 	;layout the window and size the vdu to fit
 	(setq vdu_width w vdu_height h)
 	(set vdu :vdu_width w :vdu_height h :min_width w :min_height h)
@@ -78,9 +75,9 @@
 	(bind '(w h) (. vdu :pref_size))
 	(set vdu :min_width vdu_min_width :min_height vdu_min_height)
 	(view-change vdu x y w h)
-	(. vdu :load text_buf 0 (set-slider current_file) 0 10000))
+	(. vdu :load text_buf 0 (set-slider current_file) 0 -1))
 
-(defun window-resize (w h)
+(defun vdu-resize (w h)
 	;size the vdu and layout the window to fit
 	(setq vdu_width w vdu_height h)
 	(set vdu :vdu_width w :vdu_height h :min_width w :min_height h)
@@ -88,7 +85,7 @@
 		(cat (. mywindow :get_pos) (. mywindow :pref_size))))
 	(set vdu :min_width vdu_min_width :min_height vdu_min_height)
 	(. mywindow :change_dirty x y w h)
-	(. vdu :load text_buf 0 (set-slider current_file) 0 10000))
+	(. vdu :load text_buf 0 (set-slider current_file) 0 -1))
 
 (defun main ()
 	(populate-tree)
@@ -96,30 +93,30 @@
 	(view-change tree 0 0 (def tree_scroll :min_width w) h)
 	(bind '(x y w h) (apply view-locate (. (component-connect mywindow +event_layout+) :pref_size)))
 	(gui-add (view-change mywindow x y w h))
-	(. vdu :load text_buf 0 0 0 10000)
+	(. vdu :load text_buf 0 0 0 -1)
 	(while (cond
 		((= (defq id (get-long (defq msg (mail-read (task-mailbox))) ev_msg_target_id)) +event_close+)
 			nil)
 		((= id +event_layout+)
 			;user window resize
-			(apply window-layout (. vdu :max_size)))
+			(apply window-resize (. vdu :max_size)))
 		((= id +event_min+)
 			;min button
-			(window-resize vdu_min_width vdu_min_height))
+			(vdu-resize vdu_min_width vdu_min_height))
 		((= id +event_max+)
 			;max button
-			(window-resize vdu_max_width vdu_max_height))
+			(vdu-resize vdu_max_width vdu_max_height))
 		((= id +event_scroll+)
 			;user scroll bar
 			(defq scroll_position (get :value slider))
 			(. scroll_positions :insert current_file scroll_position)
-			(. vdu :load text_buf 0 scroll_position 0 10000))
+			(. vdu :load text_buf 0 scroll_position 0 -1))
 		((= id +event_file_button+)
 			;load up the file selected
 			(if current_button
-				(set (. current_button :dirty) :color +argb_white+))
+				(def (. current_button :dirty) :color +argb_white+))
 			(setq current_button (. mywindow :find_id (get-long msg ev_msg_action_source_id)))
-			(set (. current_button :dirty) :color +argb_grey12+)
-			(populate-vdu (get :text current_button)))
+			(def (. current_button :dirty) :color +argb_grey12+)
+			(populate-vdu (. tree :get_route current_button)))
 		(t (. mywindow :event msg))))
 	(. mywindow :hide))
