0ed685223da399ce9f7ea420c9788f408a104494
Chris Hinsley <chris.hinsley@gmail.com>
2020-12-22 11:47:34 +0000

    one clipboard max

--- apps/clipboard/app.lisp
+++ apps/clipboard/app.lisp
@@ -8,10 +8,13 @@
 	(byte 'close+)
 	(byte 'deliver+ 'paste+))
 
+;single instance only
+(when (= (length (mail-enquire "CLIPBOARD_SERVICE")) 0)
+	(defq select (list (task-mailbox) (mail-alloc-mbox))
+		clip_service (mail-declare (elem -2 select) "CLIPBOARD_SERVICE" "Clipboard Service 0.1"))
+
 (defun main ()
-	(defq id t select (list (task-mailbox) (mail-alloc-mbox)) clipboard (list) +req_get+ 0 +req_put+ 1)
-	;Start a new clipboard service, only if one isn't currently running.
-	(defq clip_service (mail-declare (elem 1 select) "CLIPBOARD_SERVICE" "Clipboard Service 0.1"))
+	(defq id t clipboard (list) +req_get+ 0 +req_put+ 1)
 	(while id
 		(defq idx (mail-select select) msg (mail-read (elem idx select)))
 		(cond
@@ -28,4 +31,4 @@
 				(setq id nil))))
 	;shutdown, if requested.
 	(mail-free-mbox (pop select))
-	(mail-forget clip_service))
+	(mail-forget clip_service)))
