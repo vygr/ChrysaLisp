5e97407a3a6fd40dde375e3ec6f9f5c44af7b237
Chris Hinsley <chris.hinsley@gmail.com>
2020-12-30 11:41:46 +0000

    check node can host a GUI

--- gui/gui/gui.lisp
+++ gui/gui/gui.lisp
@@ -2,23 +2,26 @@
 (import "sys/lisp.inc")
 (import "gui/lisp.inc")
 
-;profiling callbacks on the GUI thread from :draw method !!!
-(defq *profile* (env -1) *profile_ret* (list))
+;can this node host a GUI ?
+(when (gui-info)
+	;profiling callbacks on the GUI thread from :draw method !!!
+	(defq *profile* (env -1) *profile_ret* (list))
 
-;single instance only on this node
-(defq gui_services (mail-enquire "GUI_SERVICE")
-	node (slice (const long_size) -1 (task-mailbox)))
-(when (notany (# (eql node (slice (const long_size) -1
-		(to-net-id (elem 1 (split %0 ",")))))) gui_services)
-	(mail-declare (task-mailbox) "GUI_SERVICE" "GUI Service 0.2")
+	;single instance only on this node
+	(defq gui_services (mail-enquire "GUI_SERVICE")
+		node (slice (const long_size) -1 (task-mailbox)))
+	(when (notany (# (eql node (slice (const long_size) -1
+			(to-net-id (elem 1 (split %0 ",")))))) gui_services)
+		(mail-declare (task-mailbox) "GUI_SERVICE" "GUI Service 0.2")
 
-	;screen widget
-	(def (defq screen (Backdrop)) :style 0 :color +argb_grey2+ :ink_color +argb_grey1+)
-	(.-> screen (:change 0 0 1280 960) :dirty_all)
+		;screen widget
+		(def (defq screen (Backdrop)) :style 0 :color +argb_grey2+ :ink_color +argb_grey1+)
+		(.-> screen (:change 0 0 1280 960) :dirty_all)
 
-	;fire up the login app
-	(open-child "apps/login/app.lisp" kn_call_open)
-	(open-child "apps/clipboard/app.lisp" kn_call_open)
+		;fire up the login app
+		(open-child "apps/login/app.lisp" kn_call_open)
+		(open-child "apps/clipboard/app.lisp" kn_call_open)
 
-	;jump to gui compositor
-	((ffi _ "gui/gui/gui" 0) screen))
+		;jump to gui compositor
+		((ffi _ "gui/gui/gui" 0) screen))
+)
