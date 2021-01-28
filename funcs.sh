ea46015cb2594fbde4d1ebc2493652eb7bc0b4e5
Frank V. Castellucci <frankiecast@gmail.com>
2021-01-28 04:24:34 -0500

    Delay for enquiring node information

--- funcs.sh
+++ funcs.sh
@@ -58,7 +58,7 @@ function boot_cpu_tui
 {
 	if [ $1 -lt 1 ]
 	then
-		./obj/$CPU/$ABI/$OS/main_tui obj/$CPU/$ABI/sys/boot_image $2 $3 -run apps/terminal/tui.lisp
+		./obj/$CPU/$ABI/$OS/main_tui obj/$CPU/$ABI/sys/boot_image $2 $3 -run apps/terminal/tui2.lisp
 	else
 		./obj/$CPU/$ABI/$OS/main_tui obj/$CPU/$ABI/sys/boot_image $2 $3 &
 	fi
