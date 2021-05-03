9f8c09d9010a2051437c5b833152f237936f9d8f
Chris Hinsley <chris.hinsley@gmail.com>
2021-05-03 14:19:39 +0100

    single instance check in an import

--- apps/clipboard/app.lisp
+++ apps/clipboard/app.lisp
@@ -1,34 +1,6 @@
 ;imports
 (import "sys/lisp.inc")
-(import "class/lisp.inc")
-(import "gui/lisp.inc")
-(import "lib/options/options.inc")
-
-(enums +event 0
-	(enum close)
-	(enum deliver paste))
 
 ;single instance only
-(when (= (length (mail-enquire "CLIPBOARD_SERVICE")) 0)
-	(defq select (list (task-mailbox) (mail-alloc-mbox))
-		clip_service (mail-declare (elem -2 select) "CLIPBOARD_SERVICE" "Clipboard Service 0.1"))
-
-(defun main ()
-	(defq id t clipboard (list) +req_get 0 +req_put 1)
-	(while id
-		(defq idx (mail-select select) msg (mail-read (elem idx select)))
-		(cond
-			((/= idx 0)
-				(cond
-					((eql (defq req_type (first msg)) "GET")
-						(unless (empty? clipboard)
-							(defq reply (last clipboard))
-							(mail-send (second msg) reply)))
-					((eql req_type "PUT")
-						(push clipboard (second msg)))))
-			((= (setq id (getf msg +ev_msg_target_id)) +event_close)
-				;close
-				(setq id nil))))
-	;shutdown, if requested.
-	(mail-free-mbox (pop select))
-	(mail-forget clip_service)))
+(if (= (length (mail-enquire "CLIPBOARD_SERVICE")) 0)
+	(import "apps/clipboard/app_impl.lisp"))
