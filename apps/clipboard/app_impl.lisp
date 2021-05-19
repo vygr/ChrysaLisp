cec3825f545da5ccc291dc206f116fa5bfe102f9
Chris Hinsley <chris.hinsley@gmail.com>
2021-05-19 19:29:13 +0100

    no que or stack for now.

--- apps/clipboard/app_impl.lisp
+++ apps/clipboard/app_impl.lisp
@@ -2,16 +2,15 @@
 
 (defun main ()
 	(defq clip_service (mail-declare (task-mailbox) "CLIPBOARD_SERVICE" "Clipboard Service 0.2")
-		clipboard (list))
+		clipboard nil)
 	(while t
 		(defq msg (mail-read (task-mailbox)))
 		(cond
 			((= (defq type (getf msg +clipboard_event_type)) +clip_type_put)
 				;put string on clipboard
-				(setq clipboard (insert clipboard 0
-					(list (slice +clipboard_put_data -1 msg)))))
+				(setq clipboard (slice +clipboard_put_data -1 msg)))
 			((= type +clip_type_get)
 				;get string from clipboard
 				(mail-send (getf msg +clipboard_get_reply)
-					(if (defq data (pop clipboard)) data "")))))
+					(if clipboard clipboard "")))))
 	(mail-forget clip_service))
