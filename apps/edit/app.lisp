e592a42f13b040d335b391c0d58356d7b1391ede
Chris Hinsley <chris.hinsley@gmail.com>
2021-05-26 14:41:39 +0100

    prep ui for search/replace

--- apps/edit/app.lisp
+++ apps/edit/app.lisp
@@ -12,9 +12,10 @@
 	(enum file_folder_action file_leaf_action)
 	(enum open_folder_action open_leaf_action)
 	(enum undo redo rewind cut copy paste reflow tab_left tab_right)
-	(enum prev next save new))
+	(enum prev next save new)
+	(enum search replace))
 
-(defq vdu_min_width 32 vdu_min_height 16 vdu_max_width 100 vdu_max_height 48
+(defq vdu_min_width 32 vdu_min_height 16 vdu_max_width 100 vdu_max_height 46
 	vdu_width 80 vdu_height 40 mouse_state :u meta_map (xmap) underlay (list)
 	current_file nil selected_file_node nil selected_open_node nil
 	+selected (apply nums (map (lambda (_)
@@ -31,6 +32,17 @@
 				(:color (const *env_toolbar2_col*))))
 		(. (ui-textfield name_text (:hint_text "new filename" :clear_text "" :color +argb_white))
 			:connect +event_new))
+	(ui-grid _ (:grid_width 2 :grid_height 1)
+		(ui-flow _ (:flow_flags +flow_right_fill)
+			(ui-tool-bar _ ()
+				(ui-buttons (0xe9cd) +event_search))
+			(. (ui-textfield save_text (:hint_text "search" :clear_text "" :color +argb_white))
+				:connect +event_search))
+		(ui-flow _ (:flow_flags +flow_right_fill)
+			(ui-tool-bar _ ()
+				(ui-buttons (0xe962) +event_replace))
+			(. (ui-textfield replace_text (:hint_text "replace" :clear_text "" :color +argb_white))
+				:connect +event_replace)))
 	(ui-flow _ (:flow_flags +flow_right_fill)
 		(ui-flow _ (:flow_flags +flow_stack_fill)
 			(ui-grid tree_grid (:grid_width 1 :grid_height 2 :color +argb_grey14)
