33c42f4deb43c3ed004428157035c4526513992c
Chris Hinsley <chris.hinsley@gmail.com>
2021-05-30 13:10:20 +0100

    only one per node

--- apps/clipboard/app.lisp
+++ apps/clipboard/app.lisp
@@ -1,5 +1,7 @@
 (import "sys/lisp.inc")
 
-;single instance only
-(if (= (length (mail-enquire "CLIPBOARD_SERVICE")) 0)
+;single instance per node only
+(if (= 0 (length
+		(filter (# (eql (slice +long_size -1 (task-mailbox)) (slice +long_size -1 %0)))
+		(map (# (to-net-id (elem 1 (split %0 ",")))) (mail-enquire "CLIPBOARD_SERVICE")))))
 	(import "apps/clipboard/app_impl.lisp"))
