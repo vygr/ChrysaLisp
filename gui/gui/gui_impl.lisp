520ba0933899e6af606bec5846a7067c8082b476
Chris Hinsley <chris.hinsley@gmail.com>
2021-06-03 13:39:53 +0100

    non polling GUI_SERVICE

--- gui/gui/gui_impl.lisp
+++ gui/gui/gui_impl.lisp
@@ -15,45 +15,6 @@
 	select (list (task-mailbox) (mail-alloc-mbox))
 	rate (/ 1000000 60) id t)
 
-;; (defun main ()
-;; 	;declare service
-;; 	(defq service (mail-declare (task-mailbox) "GUI_SERVICE" "GUI Service 0.2"))
-;; 	;init screen widget
-;; 	(def (defq screen (Backdrop)) :style :grid :color +argb_grey2 :ink_color +argb_grey1)
-;; 	(.-> screen (:change 0 0 1280 960) :dirty_all)
-;; 	(gui-init screen)
-;; 	;fire up the login app and clipboard service
-;; 	(open-child "apps/login/app.lisp" +kn_call_open)
-;; 	(open-child "apps/clipboard/app.lisp" +kn_call_open)
-;; 	(mail-timeout (elem +select_timer select) rate)
-;; 	(while id
-;; 		(defq msg (mail-read (elem (defq idx (mail-select select)) select)))
-;; 		(cond
-;; 			((= idx +select_main)
-;; 				;main mailbox
-;; 				(bind '(cmd view owner reply) msg)
-;; 				(cond
-;; 					((= cmd 0)
-;; 						;hide and sub view
-;; 						(.-> view :hide :sub))
-;; 					((= cmd 1)
-;; 						;add view at front
-;; 						(setf view +view_owner_id owner 0)
-;; 						(. screen :add_front view)
-;; 						(. view :set_flags +view_flag_dirty_all +view_flag_dirty_all))
-;; 					((= cmd 2)
-;; 						;add view at back
-;; 						(setf view +view_owner_id owner 0)
-;; 						(. screen :add_back view)
-;; 						(. view :set_flags +view_flag_dirty_all +view_flag_dirty_all)))
-;; 				(mail-send reply msg))
-;; 			((= idx +select_timer)
-;; 				;timer event
-;; 				(mail-timeout (elem +select_timer select) rate)
-;; 				(gui-update screen))))
-;; 	(each mail-free-mbox (slice 1 -1select))
-;; 	(mail-forget service))
-
 (defun main ()
 	;declare service
 	(defq service (mail-declare (task-mailbox) "GUI_SERVICE" "GUI Service 0.2"))
@@ -64,26 +25,31 @@
 	;fire up the login app and clipboard service
 	(open-child "apps/login/app.lisp" +kn_call_open)
 	(open-child "apps/clipboard/app.lisp" +kn_call_open)
-	(while t
-		(when (defq idx (mail-poll select))
-			(defq msg (mail-read (elem idx select)))
-			(bind '(cmd view owner reply) msg)
-			(cond
-				((= cmd 0)
-					;hide and sub view
-					(.-> view :hide :sub))
-				((= cmd 1)
-					;add view at front
-					(setf view +view_owner_id owner 0)
-					(. screen :add_front view)
-					(. view :set_flags +view_flag_dirty_all +view_flag_dirty_all))
-				((= cmd 2)
-					;add view at back
-					(setf view +view_owner_id owner 0)
-					(. screen :add_back view)
-					(. view :set_flags +view_flag_dirty_all +view_flag_dirty_all)))
-			(mail-send reply msg))
-		(task-sleep rate)
-		(gui-update screen))
+	(mail-timeout (elem +select_timer select) rate)
+	(while id
+		(defq msg (mail-read (elem (defq idx (mail-select select)) select)))
+		(cond
+			((= idx +select_main)
+				;main mailbox
+				(bind '(cmd view owner reply) msg)
+				(cond
+					((= cmd 0)
+						;hide and sub view
+						(.-> view :hide :sub))
+					((= cmd 1)
+						;add view at front
+						(setf view +view_owner_id owner 0)
+						(. screen :add_front view)
+						(. view :set_flags +view_flag_dirty_all +view_flag_dirty_all))
+					((= cmd 2)
+						;add view at back
+						(setf view +view_owner_id owner 0)
+						(. screen :add_back view)
+						(. view :set_flags +view_flag_dirty_all +view_flag_dirty_all)))
+				(mail-send reply msg))
+			((= idx +select_timer)
+				;timer event
+				(mail-timeout (elem +select_timer select) rate)
+				(gui-update screen))))
 	(each mail-free-mbox (slice 1 -1select))
 	(mail-forget service))
