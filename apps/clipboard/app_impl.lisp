a0884edd314d1b4dec10604b38b1b893adf2fa7a
Chris Hinsley <chris.hinsley@gmail.com>
2021-07-20 13:56:51 +0100

    use (let) where it makes sense
    I must get over my aversion to (let)...

--- apps/clipboard/app_impl.lisp
+++ apps/clipboard/app_impl.lisp
@@ -4,14 +4,12 @@
 	(defq clip_service (mail-declare (task-mailbox) "CLIPBOARD_SERVICE" "Clipboard Service 0.2")
 		clipboard "")
 	(while t
-		(env-push)
-		(defq msg (mail-read (task-mailbox)))
-		(cond
-			((= (defq type (elem 0 msg)) +clip_type_put)
-				;put string on clipboard
-				(setq clipboard (elem 1 msg)))
-			((= type +clip_type_get)
-				;get string from clipboard
-				(mail-send (elem 1 msg) clipboard)))
-		(env-pop))
+		(let ((msg (mail-read (task-mailbox))))
+			(cond
+				((= (defq type (elem 0 msg)) +clip_type_put)
+					;put string on clipboard
+					(setq clipboard (elem 1 msg)))
+				((= type +clip_type_get)
+					;get string from clipboard
+					(mail-send (elem 1 msg) clipboard)))))
 	(mail-forget clip_service))
