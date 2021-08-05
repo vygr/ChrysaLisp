281efa39aeb48350dc8a5d804a5024d551271c26
Chris Hinsley <chris.hinsley@gmail.com>
2021-08-05 17:57:00 +0100

    some example .sdf files
    Still needs some work on the importer...

--- apps/molecule/app.lisp
+++ apps/molecule/app.lisp
@@ -8,7 +8,7 @@
 
 (enums +event 0
 	(enum close max min)
-	(enum prev next)
+	(enum prev next auto)
 	(enum xrot yrot zrot))
 
 (enums +select 0
@@ -20,26 +20,35 @@
 (enums +col 0
 	(enum red green blue))
 
+(defun all-mol-files (p)
+	(defq out (list))
+	(each! 0 -1 (lambda (f m)
+		(and (eql m "8") (ends-with ".sdf" f) (push out (cat p f))))
+		(unzip (split (pii-dirlist p) ",") (list (list) (list))))
+	(sort cmp out))
+
 (defq anti_alias nil timer_rate (/ 1000000 30)
 	canvas_width 600 canvas_height 600 canvas_scale (if anti_alias 1 2)
 	*rotx* (f2r 0.0) *roty* (f2r 0.0) *rotz* (f2r 0.0) +focal_dist (f2r 4.0)
 	+near +focal_dist +far (+ +near +real_4) balls (list)
 	+canvas_mode (if anti_alias +canvas_flag_antialias 0)
+	*mol_index* 0 *auto_mode* nil *dirty* t
+	mol_files (all-mol-files "apps/molecule/")
 	palette (map (lambda (_) (fixeds
 			(i2f (/ (logand (>> _ 16) 0xff) 0xff))
 			(i2f (/ (logand (>> _ 8) 0xff) 0xff))
 			(i2f (/ (logand _ 0xff) 0xff))))
-		(list +argb_white +argb_cyan +argb_yellow +argb_magenta
-			+argb_red +argb_green +argb_blue)))
+		(list +argb_black +argb_white +argb_red +argb_green
+			+argb_blue +argb_cyan +argb_yellow +argb_magenta)))
 
 (ui-window *window* ()
 	(ui-title-bar *title* "Molecule" (0xea19 0xea1b 0xea1a) +event_close)
 	(ui-flow _ (:flow_flags +flow_right_fill)
 		(ui-tool-bar main_toolbar ()
-			(ui-buttons (0xe91d 0xe91e) +event_prev))
+			(ui-buttons (0xe91d 0xe91e 0xea43) +event_prev))
 		(ui-backdrop _ (:color (const *env_toolbar_col*))))
 	(ui-flow _ (:flow_flags +flow_right_fill)
-		(ui-grid _ (:grid_width 1 :grid_height 3)
+		(ui-grid _ (:grid_width 1 :grid_height 3 :font *env_body_font*)
 			(ui-label _ (:text "X rot:"))
 			(ui-label _ (:text "Y rot:"))
 			(ui-label _ (:text "Z rot:")))
@@ -70,15 +79,15 @@
 	(.-> canvas (:set_color col) (:fpoly (r2f x) (r2f y) +winding_odd_even _)))
 
 (defun lighting (col s)
-	;very basic attenuation
+	;very basic attenuation and diffuse
 	(defq at (r2f s)
 		r (* (elem +col_red col) at)
 		g (* (elem +col_green col) at)
 		b (* (elem +col_blue col) at))
 	(+ 0xff000000
-		(<< (f2i (* r (const (i2f 0xff)))) 16)
-		(<< (f2i (* g (const (i2f 0xff)))) 8)
-			(f2i (* b (const (i2f 0xff))))))
+		(<< (f2i (min (* (+ r 0.2) 256.0) 255.0)) 16)
+		(<< (f2i (min (* (+ g 0.2) 256.0) 255.0)) 8)
+			(f2i (min (* (+ b 0.2) 256.0) 255.0))))
 
 (defun render-balls (canvas balls)
 	(defq sw (const (* +real_1/2 (i2r (dec (* canvas_width canvas_scale)))))
@@ -89,11 +98,11 @@
 			s (recip (+ z +real_2))
 			r (* r s) r4 (* r +real_1/4) r16 (* r +real_1/16)
 			sx (* (+ x +real_1) sw) sy (* (+ y +real_1) sh))
-		(fpoly canvas (lighting (vec-scale c 0.75 (const (fixeds 0.0 0.0 0.0))) s)
+		(fpoly canvas (lighting (vec-scale c 0.5 (const (fixeds 0.0 0.0 0.0))) s)
 			sx sy (circle r))
 		(fpoly canvas (lighting c s)
 			(- sx r16) (- sy r16) (circle (- r r16)))
-		(fpoly canvas (lighting (const (fixeds 1.0 1.0 1.0)) s)
+		(fpoly canvas +argb_white
 			(- sx r4) (- sy r4) (circle r4))) balls))
 
 (defun print-verts (balls)
@@ -134,8 +143,44 @@
 			(i2r (+ (* 30 canvas_scale) (random 10)))
 			(elem (random (length palette)) palette)))))
 
+(defun ball-file (index)
+	(when (defq stream (file-stream (elem index mol_files)))
+		(set (.-> *title* :layout :dirty) :text
+			(cat "Molecule -> " (elem -2 (split (elem index mol_files) "/"))))
+		(clear balls)
+		(times 3 (read-line stream))
+		(defq num_atoms (str-to-num (elem 0 (split (read-line stream) " "))))
+		(times num_atoms
+			(defq line (split (read-line stream) " "))
+			(bind '(x y z) (map
+					(# (/ (i2r (str-to-num %0)) (const (i2r 65536))))
+				(slice 0 3 line)))
+			(bind '(radius col) (case (elem 3 line)
+				("C" (list (const (i2r (* 60 canvas_scale))) (elem 0 palette)))
+				("H" (list (const (i2r (* 60 canvas_scale))) (elem 1 palette)))
+				("O" (list (const (i2r (* 60 canvas_scale))) (elem 2 palette)))
+				("N" (list (const (i2r (* 60 canvas_scale))) (elem 3 palette)))
+				(t (list (const (i2r (* 60 canvas_scale))) (const (fixeds 0.0 0.0 0.0))))))
+			(push balls (list (vertex-r x y z) radius col)))
+		(defq min_x (i2r 1000) max_x (i2r -1000)
+			min_y (i2r 1000) max_y (i2r -1000)
+			min_z (i2r 1000) max_z (i2r -1000))
+		(each (lambda (((x y z w) _ _))
+			(setq min_x (min min_x x) max_x (max max_x x)
+				min_y (min min_y y) max_y (max max_y y)
+				min_z (min min_z z) max_z (max max_z z))) balls)
+		(defq width_x (- max_x min_x) width_y (- max_y min_y) width_z (- max_z min_z)
+			scale (/ (const (f2r 2.3)) (max width_x width_y width_z)))
+		(each (lambda (ball)
+			(bind '((x y z w) _ _) ball)
+			(elem-set +ball_vertex ball (reals
+				(* scale (- x min_x (/ width_x (i2r 2))))
+				(* scale (- y min_y (/ width_y (i2r 2))))
+				(* scale (- z min_z (/ width_z (i2r 2))))
+				w))) balls)))
+
 (defun reset ()
-	(ball-cloud 1000)
+	(ball-file 0)
 	(setq *dirty* t))
 
 ;import actions and bindings
@@ -145,7 +190,7 @@
 	(catch (eval action) (progn (print _)(print) t)))
 
 (defun main ()
-	(defq select (alloc-select +select_size) *running* t mouse_state :u *dirty* t)
+	(defq select (alloc-select +select_size) *running* t mouse_state :u)
 	(bind '(x y w h) (apply view-locate (. *window* :pref_size)))
 	(.-> main_widget (:set_canvas_flags +canvas_mode) (:fill +argb_black) :swap)
 	(gui-add-front (. *window* :change x y w h))
@@ -162,6 +207,11 @@
 			((= idx +select_timer)
 				;timer event
 				(mail-timeout (elem +select_timer select) timer_rate 0)
+				(when *auto_mode*
+					(setq *rotx* (% (+ *rotx* (f2r 0.01)) +real_2pi)
+						*roty* (% (+ *roty* (f2r 0.02)) +real_2pi)
+						*rotz* (% (+ *rotz* (f2r 0.03)) +real_2pi))
+					(setq *dirty* t))
 				(when *dirty*
 					(setq *dirty* nil)
 					(render)))
