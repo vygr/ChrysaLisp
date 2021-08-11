6419a641aafb123732006fa2c550162ee0d35f86
Chris Hinsley <chris.hinsley@gmail.com>
2021-08-11 13:25:44 +0100

    start of Cubes demo and mesh lib

--- apps/cubes/app.lisp
+++ apps/cubes/app.lisp
@@ -4,11 +4,11 @@
 (import "sys/lisp.inc")
 (import "class/lisp.inc")
 (import "gui/lisp.inc")
-(import "lib/math/matrix.inc")
+(import "lib/math/mesh.inc")
 
 (enums +event 0
 	(enum close max min)
-	(enum prev next auto)
+	(enum auto)
 	(enum xrot yrot zrot)
 	(enum layout)
 	(enum plain grid axis))
@@ -16,25 +16,15 @@
 (enums +select 0
 	(enum main tip timer))
 
-(enums +ball 0
-	(enum vertex radius col))
-
-(defun all-mol-files (p)
-	(defq out (list))
-	(each! 0 -1 (lambda (f m)
-		(and (eql m "8") (ends-with ".sdf" f) (push out (cat p f))))
-		(unzip (split (pii-dirlist p) ",") (list (list) (list))))
-	(sort cmp out))
-
-(defq anti_alias t timer_rate (/ 1000000 30) +min_size 450 +max_size 800
-	canvas_size +min_size canvas_scale (if anti_alias 1 2)
-	*rotx* +real_0 *roty* +real_0 *rotz* +real_0 +focal_dist +real_4
-	+near +focal_dist +far (+ +near +real_4)
-	+top (* +focal_dist +real_1/3) +bottom (* +focal_dist +real_-1/3)
-	+left (* +focal_dist +real_-1/3) +right (* +focal_dist +real_1/3)
+(defq anti_alias nil timer_rate (/ 1000000 30) +min_size 450 +max_size 800
+	canvas_size +min_size canvas_scale (if anti_alias 1 2) +radius +real_1
+	*rotx* +real_0 *roty* +real_0 *rotz* +real_0
+	+focal_dist (* +radius +real_2)
+	+near +focal_dist +far (+ +near (* +radius +real_2))
+	+top +radius +bottom (* +radius +real_-1)
+	+left (* +radius +real_-1) +right +radius
 	+canvas_mode (if anti_alias +canvas_flag_antialias 0)
-	*mol_index* 0 *auto_mode* nil *dirty* t
-	balls (list) mol_files (all-mol-files "apps/molecule/data/")
+	*mol_index* 0 *auto_mode* nil *dirty* t object (list (list) (list) (list) (list))
 	palette (map (lambda (_) (fixeds
 			(i2f (/ (logand (>> _ 16) 0xff) 0xff))
 			(i2f (/ (logand (>> _ 8) 0xff) 0xff))
@@ -43,10 +33,10 @@
 			+argb_cyan +argb_blue +argb_yellow +argb_magenta)))
 
 (ui-window *window* ()
-	(ui-title-bar *title* "" (0xea19 0xea1b 0xea1a) +event_close)
+	(ui-title-bar *title* "Cubes" (0xea19 0xea1b 0xea1a) +event_close)
 	(ui-flow _ (:flow_flags +flow_right_fill)
 		(ui-tool-bar main_toolbar ()
-			(ui-buttons (0xe91d 0xe91e 0xea43) +event_prev))
+			(ui-buttons (0xea43) +event_auto))
 		(ui-tool-bar style_toolbar ()
 			(ui-buttons (0xe976 0xe9a3 0xe9f0) +event_plain))
 		(ui-backdrop _ (:color (const *env_toolbar_col*))))
@@ -98,6 +88,10 @@
 	;draw a polygon on a canvas
 	(.-> canvas (:set_color col) (:fpoly (r2f x) (r2f y) +winding_odd_even _)))
 
+(defun fpoly_zero (canvas col _)
+	;draw a polygon on a canvas
+	(.-> canvas (:set_color col) (:fpoly 0.0 0.0 +winding_odd_even _)))
+
 (defun lighting (col at)
 	;very basic attenuation and diffuse
 	(bind '(r g b) (vec-min (vec-add (vec-scale col (* (r2f at) 255.0) +fixeds_tmp3)
@@ -105,69 +99,78 @@
 		(const (fixeds 255.0 255.0 255.0)) +fixeds_tmp3))
 	(+ 0xff000000 (<< (f2i r) 16) (<< (f2i g) 8) (f2i b)))
 
-(defun render-balls (canvas balls)
-	(defq sp (* +real_1/2 (i2r (dec (* canvas_size canvas_scale)))))
-	(each (lambda (((x y z w) r c))
-		(task-sleep 0)
-		(defq w (recip w) x (* x w) y (* y w) z (* z w) at (recip (+ z +real_2))
-			r (* r sp w) r4 (* r +real_1/4) r8 (* r +real_1/8) r16 (* r +real_1/16)
-			sx (* (+ x +real_1) sp) sy (* (+ y +real_1) sp))
-		(fpoly canvas (lighting c (* at +real_1/2)) sx sy (circle r))
-		(fpoly canvas (lighting c at) (- sx r16) (- sy r16) (circle (- r r16)))
-		(fpoly canvas (lighting (const (fixeds 1.5 1.5 1.5)) at) (- sx r4) (- sy r4) (circle r8))) balls))
+(defun lighting-at3 (col at)
+	;very basic attenuation and diffuse
+	(bind '(r g b) (vec-min (vec-add (vec-scale col (* (r2f at) (const (/ 255.0 3.0))) +fixeds_tmp3)
+		(const (fixeds 32.0 32.0 32.0)) +fixeds_tmp3)
+		(const (fixeds 255.0 255.0 255.0)) +fixeds_tmp3))
+	(+ 0xc0000000 (<< (f2i r) 16) (<< (f2i g) 8) (f2i b)))
 
-(defun sort-balls (balls)
-	(sort (lambda ((v1 _ _) (v2 _ _))
-		(if (<= (elem +vec4_w v1) (elem +vec4_w v2)) 1 -1)) balls))
+(enums +object 0
+	(enum mesh color))
 
-(defun clip-balls (balls)
-	(filter (lambda (((_ _ _ w) _ _)) (<= +near w +far)) balls))
+(defun print-vec (v)
+	(each! 0 -2 (# (prin (pad (r2f %0) 8) " ")) (list v))
+	(prin (pad (r2f (elem -2 v)) 8))
+	(print))
+
+(defun print-mat (m)
+	(each (# (print-vec %0)) m))
+
+(defun sort-verts (verts)
+	(sort (lambda (v1 v2)
+		(if (<= (elem +vec4_w v1) (elem +vec4_w v2)) 1 -1)) verts))
+
+(defun render-object-verts (canvas mat4x4_obj mat4x4_proj object)
+	(defq sp (* +real_1/2 (i2r (dec (* canvas_size canvas_scale))))
+		prog_verts (map (# (mat4x4-vec4-mul mat4x4_proj %0))
+			(elem +mesh_verts (elem +object_mesh object))))
+	(each (lambda ((x y z w))
+			(defq w (recip w) x (* x w) y (* y w) z (* z w) at (recip (+ z +real_2))
+				r (* (const (f2r 0.0125)) sp w) sx (* (+ x +real_1) sp) sy (* (+ y +real_1) sp))
+			(fpoly canvas (lighting (const (fixeds 1.0 1.0 1.0)) at) sx sy (circle r)))
+		(sort-verts prog_verts)))
+
+(defun render-object-tris (canvas mat4x4_obj mat4x4_proj object)
+	(defq sp (* +real_1/2 (i2r (dec (* canvas_size canvas_scale))))
+		obj_verts (map (# (mat4x4-vec4-mul mat4x4_obj %0))
+			(elem +mesh_verts (elem +object_mesh object)))
+		obj_norms (map (# (mat4x4-vec3-mul mat4x4_obj %0))
+			(elem +mesh_norms (elem +object_mesh object)))
+		prog_verts (map (# (mat4x4-vec4-mul mat4x4_proj %0))
+			(elem +mesh_verts (elem +object_mesh object)))
+		screen_verts (cap (length obj_verts) (list))
+		ats (cap (length obj_verts) (list)))
+	(each (lambda ((x y z w))
+			(defq w (recip w) x (* x w) y (* y w) z (* z w) at (recip (+ z +real_2))
+				sx (* (+ x +real_1) sp) sy (* (+ y +real_1) sp))
+			(push screen_verts (path (r2f sx) (r2f sy)))
+			(push ats at))
+		prog_verts)
+	(each (lambda ((i0 i1 i2 in))
+			(defq v0 (elem i0 obj_verts) v1 (elem i1 obj_verts)
+				v2 (elem i2 obj_verts) n (elem in obj_norms)
+				at (+ (elem i0 ats) (elem i1 ats) (elem i2 ats)))
+			(when (> (vec-dot n v0) +real_0)
+				(fpoly_zero canvas (lighting-at3 (elem +object_color object) at)
+					(list (cat (elem i0 screen_verts) (elem i1 screen_verts) (elem i2 screen_verts))))))
+		(elem +mesh_tris (elem +object_mesh object))))
 
 (defun render ()
-	(defq mrx (mat4x4-rotx *rotx*) mry (mat4x4-roty *roty*) mrz (mat4x4-rotz *rotz*)
-		mrot (mat4x4-mul (mat4x4-mul mrx mry) mrz)
-		mtrans (mat4x4-translate +real_0 +real_0 (const (- +real_0 +focal_dist +real_2)))
-		mfrust (mat4x4-frustum +left +right +top +bottom +near +far)
-		matrix (mat4x4-mul mfrust (mat4x4-mul mtrans mrot))
-		balls (sort-balls (clip-balls (map (lambda ((v r c))
-			(list (mat4x4-vec4-mul matrix v) r c)) balls))))
+	(defq mat4x4_rot (mat4x4-mul (mat4x4-mul
+			(mat4x4-rotx *rotx*) (mat4x4-roty *roty*)) (mat4x4-rotz *rotz*))
+		mat4x4_trans (mat4x4-translate +real_0 +real_0 (const (- +real_0 +focal_dist +radius)))
+		mat4x4_frust (mat4x4-frustum +left +right +top +bottom +near +far)
+		mat4x4_obj (mat4x4-mul mat4x4_trans mat4x4_rot)
+		mat4x4_proj (mat4x4-mul mat4x4_frust mat4x4_obj))
 	(. main_widget :fill 0)
-	(render-balls main_widget balls)
+	(render-object-verts main_widget mat4x4_obj mat4x4_proj object)
+	(render-object-tris main_widget mat4x4_obj mat4x4_proj object)
 	(. main_widget :swap))
 
-(defun ball-file (index)
-	(when (defq stream (file-stream (defq file (elem index mol_files))))
-		(def (.-> *title* :layout :dirty) :text
-			(cat "Molecule -> " (slice (inc (find-rev "/" file)) -1 file)))
-		(clear balls)
-		(times 3 (read-line stream))
-		(defq num_atoms (str-to-num (elem 0 (split (read-line stream) " "))))
-		(times num_atoms
-			(defq line (split (read-line stream) " "))
-			(bind '(x y z) (map
-					(# (/ (i2r (str-to-num %0)) (const (i2r 65536))))
-				(slice 0 3 line)))
-			(bind '(radius col) (case (elem 3 line)
-				("C" (list (const (i2r (* 70 canvas_scale))) (elem 0 palette)))
-				("H" (list (const (i2r (* 25 canvas_scale))) (elem 1 palette)))
-				("O" (list (const (i2r (* 60 canvas_scale))) (elem 2 palette)))
-				("N" (list (const (i2r (* 65 canvas_scale))) (elem 3 palette)))
-				("F" (list (const (i2r (* 50 canvas_scale))) (elem 4 palette)))
-				("S" (list (const (i2r (* 88 canvas_scale))) (elem 6 palette)))
-				("Si" (list (const (i2r (* 111 canvas_scale))) (elem 6 palette)))
-				("P" (list (const (i2r (* 98 canvas_scale))) (elem 7 palette)))
-				(t (list (const (i2r (* 100 canvas_scale))) (const (fixeds 1.0 1.0 0.0))))))
-			(push balls (list (vec3-r x y z) radius col)))
-		(bind '(center radius) (bounding-sphere balls (# (elem +ball_vertex %0))))
-		(defq scale_p (/ (const (f2r 2.0)) radius) scale_r (/ (const (f2r 0.0625)) radius))
-		(each (lambda (ball)
-			(bind '(v r _) ball)
-			(push (vec-scale (vec-sub v center v) scale_p v) +real_1)
-			(elem-set +ball_radius ball (* scale_r r))) balls)))
-
 (defun reset ()
-	(ball-file 0)
-	(setq *dirty* t))
+	(setq object (list (gen-sphere +radius 20) (fixeds 1.0 0.0 0.0))
+		*dirty* t))
 
 ;import actions and bindings
 (import "./actions.inc")
