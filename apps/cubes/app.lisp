1ccbef9305d30ebedf67a0dc0b1f01d1715a665b
Chris Hinsley <chris.hinsley@gmail.com>
2021-08-14 11:53:07 +0100

    selectable render mode button

--- apps/cubes/app.lisp
+++ apps/cubes/app.lisp
@@ -1,4 +1,4 @@
-(import "lib/debug/frames.inc")
+;(import "lib/debug/frames.inc")
 ;(import "lib/debug/profile.inc")
 
 (import "sys/lisp.inc")
@@ -8,7 +8,7 @@
 
 (enums +event 0
 	(enum close max min)
-	(enum auto)
+	(enum mode auto)
 	(enum xrot yrot zrot)
 	(enum layout)
 	(enum plain grid axis))
@@ -23,7 +23,7 @@
 	+near +focal_dist +far (+ +near (* +radius +real_2))
 	+top +radius +bottom (* +radius +real_-1)
 	+left (* +radius +real_-1) +right +radius
-	*mol_index* 0 *auto_mode* nil *dirty* t scene nil
+	*mol_index* 0 *auto_mode* nil *render_mode* nil *dirty* t scene nil
 	palette (map (lambda (_) (fixeds
 			(i2f (/ (logand (>> _ 16) 0xff) 0xff))
 			(i2f (/ (logand (>> _ 8) 0xff) 0xff))
@@ -35,7 +35,7 @@
 	(ui-title-bar *title* "Cubes" (0xea19 0xea1b 0xea1a) +event_close)
 	(ui-flow _ (:flow_flags +flow_right_fill)
 		(ui-tool-bar main_toolbar ()
-			(ui-buttons (0xea43) +event_auto))
+			(ui-buttons (0xe962 0xea43) +event_mode))
 		(ui-tool-bar style_toolbar ()
 			(ui-buttons (0xe976 0xe9a3 0xe9f0) +event_plain))
 		(ui-backdrop _ (:color (const *env_toolbar_col*))))
@@ -58,7 +58,7 @@
 (defun tooltips ()
 	(def *window* :tip_mbox (elem +select_tip select))
 	(each (# (def %0 :tip_text %1)) (. main_toolbar :children)
-		'("prev" "next" "auto"))
+		'("mode" "auto"))
 	(each (# (def %0 :tip_text %1)) (. style_toolbar :children)
 		'("plain" "grid" "axis")))
 
@@ -75,96 +75,8 @@
 (defun get-rot (slider)
 	(/ (* (i2r (get :value slider)) +real_2pi) (const (i2r 1000))))
 
-(defun circle (r)
-	;cached circle generation, quantised to 1/4 pixel
-	(defq r (* (floor (* (r2f r) 4.0)) 0.25) i (% (logior r) 13)
-		k (elem i '(()()()()()()()()()()()()())) p (elem i '(()()()()()()()()()()()()())))
-	(cond ((defq i (some (lambda (i) (if (= i r) _)) k)) (elem i p))
-		(t (push k r) (elem -2 (push p (list
-			(path-gen-arc 0.0 0.0 0.0 +fp_2pi r 0.25 (path))))))))
-
-(defun fpoly (canvas col x y _)
-	;draw a polygon on a canvas
-	(.-> canvas (:set_color col) (:fpoly (r2f x) (r2f y) +winding_odd_even _)))
-
-(defun fpoly-zero (canvas col _)
-	;draw a polygon on a canvas
-	(.-> canvas (:set_color col) (:fpoly 0.0 0.0 +winding_odd_even _)))
-
-(defun lighting (alpha col at)
-	;very basic attenuation and diffuse
-	(bind '(r g b) (vec-min (vec-add (vec-scale col (* (r2f at) 255.0) +fixeds_tmp3)
-		(const (fixeds 32.0 32.0 32.0)) +fixeds_tmp3)
-		(const (fixeds 255.0 255.0 255.0)) +fixeds_tmp3))
-	(+ (<< (f2i (* alpha 255.0)) 24) (<< (f2i r) 16) (<< (f2i g) 8) (f2i b)))
-
-(defun lighting-at3 (alpha col at)
-	;very basic attenuation and diffuse
-	(bind '(r g b) (vec-min (vec-add (vec-scale col (* (r2f at) (const (/ 255.0 3.0))) +fixeds_tmp3)
-		(const (fixeds 32.0 32.0 32.0)) +fixeds_tmp3)
-		(const (fixeds 255.0 255.0 255.0)) +fixeds_tmp3))
-	(+ (<< (f2i (* alpha 255.0)) 24) (<< (f2i r) 16) (<< (f2i g) 8) (f2i b)))
-
-(defun sort-verts (verts)
-	(sort (lambda (v1 v2)
-		(if (<= (elem +vec4_w v1) (elem +vec4_w v2)) 1 -1)) verts))
-
-(defun sorted-tris (tris prog_verts)
-	(sorted (lambda (t1 t2)
-		(if (<= (elem +vec4_w (elem (elem +tri_i_v0 t1) prog_verts))
-				(elem +vec4_w (elem (elem +tri_i_v0 t2) prog_verts)))
-			1 -1)) tris))
-
-(defun render-object-verts (canvas mat4x4_obj mat4x4_proj object)
-	(defq sp (* +real_1/2 (i2r (dec (* canvas_size canvas_scale))))
-		prog_verts (map (# (mat4x4-vec4-mul mat4x4_proj %0)) (. object :get_verts)))
-	(each (lambda ((x y z w))
-			(defq w (recip w) x (* x w) y (* y w) z (* z w) at (recip (+ z +real_2))
-				r (* (const (f2r 0.02)) sp w) sx (* (+ x +real_1) sp) sy (* (+ y +real_1) sp))
-			(fpoly canvas (lighting 1.0 (const (fixeds 1.0 1.0 1.0)) at) sx sy (circle r)))
-		(sort-verts prog_verts)))
-
-(defun render-object-tris (canvas mat4x4_obj mat4x4_proj object)
-	(defq sp (* +real_1/2 (i2r (dec (* canvas_size canvas_scale))))
-		obj_verts (map (# (mat4x4-vec4-mul mat4x4_obj %0)) (. object :get_verts))
-		obj_norms (map (# (mat4x4-vec3-mul mat4x4_obj %0)) (. object :get_norms))
-		prog_verts (map (# (mat4x4-vec4-mul mat4x4_proj %0)) (. object :get_verts))
-		screen_verts (cap (length obj_verts) (list))
-		ats (cap (length obj_verts) (list)))
-	(each (lambda ((x y z w))
-			(defq w (recip w) x (* x w) y (* y w) z (* z w) at (recip (+ z +real_2))
-				sx (* (+ x +real_1) sp) sy (* (+ y +real_1) sp))
-			(push screen_verts (path (r2f sx) (r2f sy)))
-			(push ats at))
-		prog_verts)
-	(each (lambda ((i0 i1 i2 in))
-			(defq v0 (elem i0 obj_verts) v1 (elem i1 obj_verts)
-				v2 (elem i2 obj_verts) n (elem in obj_norms)
-				at (+ (elem i0 ats) (elem i1 ats) (elem i2 ats)))
-			(if (> (vec-dot n v0) +real_0)
-				(fpoly-zero canvas (lighting-at3 0.9 (. object :get_color) at)
-					(list (cat (elem i0 screen_verts) (elem i1 screen_verts) (elem i2 screen_verts))))))
-		(sorted-tris (elem +mesh_tris (. object :get_mesh)) prog_verts)))
-
-(defun render ()
-	(. main_widget :fill 0)
-	(defq mat4x4_frust (Mat4x4-frustum +left +right +top +bottom +near +far)
-		matrix_stack (list (Mat4x4-unity)))
-	(. scene :walk_nodes
-		(lambda (object children)
-			(push matrix_stack (defq mat4x4_obj (mat4x4-mul
-				(elem -2 matrix_stack) (. object :get_matrix))))
-			(when (def? :mesh object)
-				(defq mat4x4_proj (mat4x4-mul mat4x4_frust mat4x4_obj))
-;				(render-object-verts main_widget mat4x4_obj mat4x4_proj object)
-				(render-object-tris main_widget mat4x4_obj mat4x4_proj object))
-			nil)
-		(lambda (object children)
-			(pop matrix_stack)))
-	(. main_widget :swap))
-
 (defun reset ()
-	(setq scene (Scene-node "root") *dirty* t)
+	(setq scene (Scene "root") *dirty* t)
 	(.-> scene
 		(:add_node (defq sphere (Scene-object (gen-sphere +radius 10) (fixeds 1.0 0.0 0.0) "sphere1")))
 		(:add_node (defq torus (Scene-object (gen-torus (- +radius +real_1/3) +real_1/3 15) (fixeds 0.0 1.0 0.0) "torus1"))))
@@ -214,7 +126,8 @@
 					(setq *dirty* nil)
 					(. scene :set_rotation +real_0 +real_0 *rotz*)
 					(each (# (. %0 :set_rotation *rotx* *roty* +real_0)) (. scene :children))
-					(render)))
+					(. scene :render main_widget (* canvas_size canvas_scale)
+						+left +right +top +bottom +near +far *render_mode*)))
 			((defq id (getf *msg* +ev_msg_target_id) action (. event_map :find id))
 				;call bound event action
 				(dispatch-action action))
