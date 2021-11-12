337a4b568c36ea1fdc48a52e32de4b6fbfa29dc9
Chris Hinsley <chris.hinsley@gmail.com>
2021-11-12 21:36:36 +0000

    playing with the heuristics

--- apps/pcb/child.lisp
+++ apps/pcb/child.lisp
@@ -13,28 +13,12 @@
 (enums +select 0
 	(enum main timeout))
 
-(defun gen-vectors (vec_range x_range y_range)
-	;generate range of routing vectors
-	(defq out (list) vec_range (n2f vec_range) y (neg y_range))
-	(while (<= y y_range)
-		(defq x (neg x_range))
-		(while (<= x x_range)
-			(defq l (vec-length (Vec2-f (n2f x) (n2f y))))
-			(and (> l 0.1) (<= l vec_range)
-				(push out (Node x y 0)))
-			(setq x (inc x)))
-		(setq y (inc y)))
-	out)
-
 (defun route (mbox data)
 	(defq pcb_list (pcb-read data))
 	(bind '(width height depth) (elem-get 0 pcb_list))
 	(bind '(res verb quant viascost) '(1 1 1 0))
-	(bind '(flood_range flood_range_even_layer flood_range_odd_layer) '(2 1 1))
-	(defq rfvs (list
-			(gen-vectors flood_range flood_range_even_layer flood_range)
-			(gen-vectors flood_range flood_range flood_range_odd_layer))
-		pcb (Pcb width height depth rfvs res verb quant viascost))
+	(bind '(fr fr_even fr_odd) '(2 1 1))
+	(defq pcb (Pcb width height depth res verb quant viascost fr fr_even fr_odd))
 	(each! 1 -1 (lambda ((id track_radius via_radius track_gap pads wires &optional paths))
 		(task-slice)
 		(setq wires '() pads (map (lambda ((radius gap pos shape))
@@ -57,7 +41,7 @@
 			((= idx +select_main)
 				;main mailbox, reset timeout and reply with result
 				(mail-timeout (elem-get +select_timeout select) 0 0)
-				(mail-timeout (elem-get +select_timeout select) 1000000000 0)
+				(mail-timeout (elem-get +select_timeout select) 10000000000 0)
 				(route (getf msg +job_reply) (slice +job_data -1 msg)))))
 	(free-select select)
 	(if (get 'profile-report)
