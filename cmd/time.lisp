b52954e6781c1097c376e69f4a7e3924fe6fe417
Chris Hinsley <chris.hinsley@gmail.com>
2023-03-03 12:36:48 +0000

    Update time.lisp
    more comments for folks

--- cmd/time.lisp
+++ cmd/time.lisp
@@ -15,13 +15,20 @@
 	(when (and
 			(defq stdio (create-stdio))
 			(defq args (options stdio usage)))
+		;get start time
 		(defq start (pii-time))
+		;pass it all through
 		(while (defq c (read-char (io-stream 'stdin)))
 			(write-char (io-stream 'stdout) c))
+		;get duration
 		(defq duration (- (pii-time) start))
 		(stream-flush (io-stream 'stdout))
+		;wait for the stdout data to flow along...
+		(task-sleep 100000)
 		(write (io-stream 'stderr)
 			(cat "Time:" (char 10) (time-in-seconds duration) " seconds" (char 10)))
-		(task-sleep 100000)
 		(stream-flush (io-stream 'stderr))
-		(task-sleep 100000)))
+		;wait for the stderr data to flow along...
+		(task-sleep 100000)
+		;and now exit and send EOF along the pipe.
+		))
