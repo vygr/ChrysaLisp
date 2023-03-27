5e224a7070953801fb9208e8a482cea70a4c2716
Chris Hinsley <chris.hinsley@gmail.com>
2023-03-27 11:57:05 +0100

    prep for multi speed categories

--- apps/netspeed/app.lisp
+++ apps/netspeed/app.lisp
@@ -10,28 +10,47 @@
 (enums +select 0
 	(enum main task reply nodes))
 
-(defq +vops_scale_size 4 +vops_align (* 1024 4096) max_vops (* 1024 16384)
+(defq +scale_size 10 +max_align +scale_size
+	max_regs 10 max_memory 10 max_reals 10
 	id :t +poll_rate (/ 1000000 4)
 	+retry_timeout (if (starts-with "obj/vp64" (load-path)) 20000000 2000000))
 
 (ui-window *window* ()
 	(ui-title-bar _ "Network Speed" (0xea19 0xea1b 0xea1a) +event_close)
 	(ui-grid _ (:grid_height 1 :flow_flags +flow_down_fill :maximum 100 :value 0)
-		(ui-flow _ (:color +argb_red)
-			(ui-label _ (:text "VPSpeed (vops)" :color +argb_white))
-			(ui-grid vops_scale_grid (:grid_width +vops_scale_size :grid_height 1 :color +argb_white
+		(ui-flow _ (:color +argb_green)
+			(ui-label _ (:text "Regs" :color +argb_white))
+			(ui-grid regs_scale_grid (:grid_width +scale_size :grid_height 1 :color +argb_white
 					:font *env_medium_terminal_font*)
-				(times +vops_scale_size (ui-label _
+				(times +scale_size (ui-label _
 					(:text "|" :flow_flags (logior +flow_flag_align_vcenter +flow_flag_align_hright)))))
-			(ui-grid vops_grid (:grid_width 1)))))
+			(ui-grid regs_grid (:grid_width 1)))
+		(ui-flow _ (:color +argb_yellow)
+			(ui-label _ (:text "Memory" :color +argb_white))
+			(ui-grid memory_scale_grid (:grid_width +scale_size :grid_height 1 :color +argb_white
+					:font *env_medium_terminal_font*)
+				(times +scale_size (ui-label _
+					(:text "|" :flow_flags (logior +flow_flag_align_vcenter +flow_flag_align_hright)))))
+			(ui-grid memory_grid (:grid_width 1)))
+		(ui-flow _ (:color +argb_red)
+			(ui-label _ (:text "Reals" :color +argb_white))
+			(ui-grid reals_scale_grid (:grid_width +scale_size :grid_height 1 :color +argb_white
+					:font *env_medium_terminal_font*)
+				(times +scale_size (ui-label _
+					(:text "|" :flow_flags (logior +flow_flag_align_vcenter +flow_flag_align_hright)))))
+			(ui-grid reals_grid (:grid_width 1)))))
 
 (defun create (key now)
 	; (create key now) -> val
 	;function called when entry is created
 	(.-> (defq ub (Progress) ab (Progress) tb (Progress) val (emap))
 		(:insert :timestamp now)
-		(:insert :vops_bar ab))
-	(. vops_grid :add_child ab)
+		(:insert :reals_bar ub)
+		(:insert :memory_bar ab)
+		(:insert :regs_bar tb))
+	(. reals_grid :add_child ub)
+	(. memory_grid :add_child ab)
+	(. regs_grid :add_child tb)
 	(open-task "apps/netspeed/child.lisp" key +kn_call_open 0 (elem-get +select_task select))
 	val)
 
@@ -39,7 +58,15 @@
 	; (destroy key val)
 	;function called when entry is destroyed
 	(when (defq child (. val :find :child)) (mail-send child ""))
-	(.-> val (:find :vops_bar) :sub))
+	(.-> val (:find :reals_bar) :sub)
+	(.-> val (:find :memory_bar) :sub)
+	(.-> val (:find :regs_bar) :sub))
+
+(defun update-scale (scale max_scale)
+	(each (lambda (mark)
+		(defq val (* (inc _) (/ max_scale (length scale))))
+		(def mark :text (str val "|"))
+		(. mark :layout)) scale))
 
 (defun main ()
 	(defq select (alloc-select +select_size))
@@ -80,28 +107,41 @@
 			(+select_reply
 				;child poll responce
 				(when (defq val (. global_tasks :find (getf msg +reply_node)))
-					(defq vops_val (getf msg +reply_vp_speed)
-						vops_bar (. val :find :vops_bar))
-					(setq max_vops (align (max max_vops vops_val) +vops_align))
-					(def vops_bar :maximum max_vops :value vops_val)
-					(. vops_bar :dirty)
+					(defq vops_regs (getf msg +reply_vops_regs)
+						vops_memory (getf msg +reply_vops_memory)
+						vops_reals (getf msg +reply_vops_reals)
+						regs_bar (. val :find :regs_bar)
+						memory_bar (. val :find :memory_bar)
+						reals_bar (. val :find :reals_bar))
+					(setq max_reals (align (max max_reals vops_reals) +max_align)
+						max_memory (align (max max_memory vops_memory) +max_align)
+						max_regs (align (max max_regs vops_regs) +max_align))
+					(def regs_bar :maximum max_regs :value vops_regs)
+					(def memory_bar :maximum max_memory :value vops_memory)
+					(def reals_bar :maximum max_reals :value vops_reals)
+					(. regs_bar :dirty) (. memory_bar :dirty) (. reals_bar :dirty)
 					(. val :insert :timestamp (pii-time))
 					(push poll_que (. val :find :child))))
 			(:t	;polling timer event
 				(mail-timeout (elem-get +select_nodes select) +poll_rate 0)
 				(when (. global_tasks :refresh +retry_timeout)
 					;nodes have mutated
-					(. vops_grid :layout)
+					(. reals_grid :layout)
+					(. memory_grid :layout)
+					(. regs_grid :layout)
 					(bind '(x y w h) (apply view-fit
 						(cat (. *window* :get_pos) (. *window* :pref_size))))
 					(. *window* :change_dirty x y w h))
 				;set scales
-				(defq vops_scale (. vops_scale_grid :children))
-				(each (lambda (mark)
-					(defq val (* (inc _) (/ (* max_vops 100) (length vops_scale))))
-					(def mark :text (str (/ val 102400) "|"))
-					(. mark :layout)) vops_scale)
-				(. vops_scale_grid :dirty_all)
+				(defq regs_scale (. regs_scale_grid :children)
+					memory_scale (. memory_scale_grid :children)
+					reals_scale (. reals_scale_grid :children))
+				(update-scale regs_scale max_regs)
+				(update-scale memory_scale max_memory)
+				(update-scale reals_scale max_reals)
+				(. regs_scale_grid :dirty_all)
+				(. memory_scale_grid :dirty_all)
+				(. reals_scale_grid :dirty_all)
 				;poll any ready children
 				(each (# (mail-send %0 (elem-get +select_reply select))) poll_que)
 				(clear poll_que))))
--- apps/netspeed/child.lisp
+++ apps/netspeed/child.lisp
@@ -22,7 +22,10 @@
 			((= idx +select_main)
 				;main mailbox, reset timeout and reply with info
 				(mail-timeout (elem-get +select_timeout select) 0 0)
+				(bind '(vops_regs vops_memory vops_reals) (vops))
 				(mail-send msg (setf-> (str-alloc +reply_size)
 					(+reply_node (slice +long_size -1 (task-mailbox)))
-					(+reply_vp_speed (vops)))))))
+					(+reply_vops_regs vops_regs)
+					(+reply_vops_memory vops_memory)
+					(+reply_vops_reals vops_reals))))))
 	(free-select select))
