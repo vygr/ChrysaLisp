58b719007d03cb940823e7556fed5b57591c55d0
Chris Hinsley <chris.hinsley@gmail.com>
2023-04-07 15:53:10 +0100

    first pass at `ui-hchart` widget

--- apps/netmon/app.lisp
+++ apps/netmon/app.lisp
@@ -11,45 +11,24 @@
 	(enum main task reply nodes))
 
 (defq +task_scale_size 10 +mem_scale_size 4 +task_align 10
-	+mem_align (* 1024 4096) +poll_rate (/ 1000000 4)
+	+mem_align (* 1024 16) +poll_rate (/ 1000000 4)
 	+retry_timeout (if (starts-with "obj/vp64" (load-path)) 20000000 2000000))
 
 (ui-window *window* ()
 	(ui-title-bar _ "Network Monitor" (0xea19 0xea1b 0xea1a) +event_close)
-	(ui-grid _ (:grid_height 1 :flow_flags +flow_down_fill :maximum 100 :value 0)
-		(ui-flow _ (:color +argb_green)
-			(ui-label _ (:text "Tasks" :color +argb_white))
-			(ui-grid task_scale_grid (:grid_width +task_scale_size :grid_height 1 :color +argb_white
-					:font *env_medium_terminal_font*)
-				(times +task_scale_size (ui-label _
-					(:text "|" :flow_flags (logior +flow_flag_align_vcenter +flow_flag_align_hright)))))
-			(ui-grid task_grid (:grid_width 1)))
-		(ui-flow _ (:color +argb_yellow)
-			(ui-label _ (:text "Alloc (kb)" :color +argb_white))
-			(ui-grid alloc_scale_grid (:grid_width +mem_scale_size :grid_height 1 :color +argb_white
-					:font *env_medium_terminal_font*)
-				(times +mem_scale_size (ui-label _
-					(:text "|" :flow_flags (logior +flow_flag_align_vcenter +flow_flag_align_hright)))))
-			(ui-grid alloc_grid (:grid_width 1)))
-		(ui-flow _ (:color +argb_red)
-			(ui-label _ (:text "Used (kb)" :color +argb_white))
-			(ui-grid used_scale_grid (:grid_width +mem_scale_size :grid_height 1 :color +argb_white
-					:font *env_medium_terminal_font*)
-				(times +mem_scale_size (ui-label _
-					(:text "|" :flow_flags (logior +flow_flag_align_vcenter +flow_flag_align_hright)))))
-			(ui-grid used_grid (:grid_width 1)))))
+	(ui-grid _ (:grid_height 1)
+		(ui-hchart task_chart "Tasks" +task_scale_size 1 (:color +argb_green))
+		(ui-hchart alloc_chart "Alloc (kb)" +mem_scale_size 1024 (:color +argb_yellow))
+		(ui-hchart used_chart "Used (kb)" +mem_scale_size 1024 (:color +argb_red))))
 
 (defun create (key now)
 	; (create key now) -> val
 	;function called when entry is created
-	(.-> (defq ub (Progress) ab (Progress) tb (Progress) node (emap))
+	(.-> (defq node (emap))
 		(:insert :timestamp now)
-		(:insert :used_bar ub)
-		(:insert :alloc_bar ab)
-		(:insert :task_bar tb))
-	(. used_grid :add_child ub)
-	(. alloc_grid :add_child ab)
-	(. task_grid :add_child tb)
+		(:insert :used_bar (. used_chart :add_bar))
+		(:insert :alloc_bar (. alloc_chart :add_bar))
+		(:insert :task_bar (. task_chart :add_bar)))
 	(open-task "apps/netmon/child.lisp" key +kn_call_open 0 (elem-get +select_task select))
 	node)
 
@@ -61,24 +40,20 @@
 	(.-> node (:find :alloc_bar) :sub)
 	(.-> node (:find :task_bar) :sub))
 
-(defun update-scale (scale max_scale inc_scale)
-	(setq scale (.-> scale :dirty_all :children))
-	(each (lambda (mark)
-		(defq val (* (inc _) (/ (* max_scale 100) (length scale))))
-		(def mark :text (str (/ val inc_scale) "|"))
-		(. mark :layout)) scale))
-
-(defun update-result (node val max_val max_align bsym)
-	(def (. (. node :find bsym) :dirty)
-		:value val :maximum (align (max max_val val) max_align)))
+(defun update-result (node tasks alloc used)
+	(def task_chart :maximum (align (max tasks (get :maximum task_chart)) +task_align))
+	(def alloc_chart :maximum (align (max alloc (get :maximum alloc_chart)) +mem_align))
+	(def used_chart :maximum (align (max used (get :maximum used_chart)) +mem_align))
+	(def (.-> node (:find :task_bar) :dirty) :value tasks)
+	(def (.-> node (:find :alloc_bar) :dirty) :value alloc)
+	(def (.-> node (:find :used_bar) :dirty) :value used))
 
 (defun main ()
+	(defq id :t select (alloc-select +select_size)
+		global_tasks (Global create destroy) poll_que (list))
 	(bind '(x y w h) (apply view-locate (. *window* :pref_size)))
 	(. *window* :set_flags +view_flag_at_front +view_flag_at_front)
-	(gui-add-front (. *window* :change x y w h))
-	(defq id :t select (alloc-select +select_size)
-		max_tasks +task_scale_size max_used (* 1024 16384) max_alloc (* 1024 16384)
-		global_tasks (Global create destroy) poll_que (list))
+	(gui-add-front (. *window* :change_dirty x y w h))
 	(mail-timeout (elem-get +select_nodes select) 1 0)
 	(while id
 		(defq msg (mail-read (elem-get (defq idx (mail-select select)) select)))
@@ -113,23 +88,23 @@
 			(+select_reply
 				;child poll responce
 				(when (defq node (. global_tasks :find (getf msg +reply_node)))
-					(setq max_tasks (update-result node (getf msg +reply_task_count) max_tasks +task_align :task_bar)
-						max_alloc (update-result node (getf msg +reply_mem_alloc) max_alloc +mem_align :alloc_bar)
-						max_used (update-result node (getf msg +reply_mem_used) max_used +mem_align :used_bar))
+					(update-result node
+						(getf msg +reply_task_count)
+						(getf msg +reply_mem_alloc)
+						(getf msg +reply_mem_used))
 					(. node :insert :timestamp (pii-time))
 					(push poll_que (. node :find :child))))
 			(:t	;polling timer event
 				(mail-timeout (elem-get +select_nodes select) +poll_rate 0)
 				(when (. global_tasks :refresh +retry_timeout)
 					;nodes have mutated
-					(. used_grid :layout) (. alloc_grid :layout) (. task_grid :layout)
 					(bind '(x y w h) (apply view-fit
 						(cat (. *window* :get_pos) (. *window* :pref_size))))
 					(. *window* :change_dirty x y w h))
 				;set scales
-				(update-scale task_scale_grid max_tasks 100)
-				(update-scale alloc_scale_grid max_alloc 102400)
-				(update-scale used_scale_grid max_used 102400)
+				(. task_chart :update_scale)
+				(. alloc_chart :update_scale)
+				(. used_chart :update_scale)
 				;poll any ready children
 				(each (# (mail-send %0 (elem-get +select_reply select))) poll_que)
 				(clear poll_que))))
--- apps/netspeed/app.lisp
+++ apps/netspeed/app.lisp
@@ -17,65 +17,26 @@
 
 (ui-window *window* ()
 	(ui-title-bar _ "Network Speed" (0xea19 0xea1b 0xea1a) +event_close)
-	(ui-grid _ (:grid_height 1 :flow_flags +flow_down_fill :maximum 100 :value 0)
-		(ui-flow _ (:color +argb_green)
-			(ui-label _ (:text "Net Regs (bops/s)" :color +argb_white))
-			(ui-grid net_regs_scale_grid (:grid_width +scale_size :grid_height 1 :color +argb_white
-					:font *env_medium_terminal_font*)
-				(times +scale_size (ui-label _
-					(:text "|" :flow_flags (logior +flow_flag_align_vcenter +flow_flag_align_hright)))))
-			(ui-progress net_regs_bar))
-		(ui-flow _ (:color +argb_yellow)
-			(ui-label _ (:text "Net Memory (bops/s)" :color +argb_white))
-			(ui-grid net_memory_scale_grid (:grid_width +scale_size :grid_height 1 :color +argb_white
-					:font *env_medium_terminal_font*)
-				(times +scale_size (ui-label _
-					(:text "|" :flow_flags (logior +flow_flag_align_vcenter +flow_flag_align_hright)))))
-			(ui-progress net_memory_bar))
-		(ui-flow _ (:color +argb_red)
-			(ui-label _ (:text "Net Reals (mops/s)" :color +argb_white))
-			(ui-grid net_reals_scale_grid (:grid_width +scale_size :grid_height 1 :color +argb_white
-					:font *env_medium_terminal_font*)
-				(times +scale_size (ui-label _
-					(:text "|" :flow_flags (logior +flow_flag_align_vcenter +flow_flag_align_hright)))))
-			(ui-progress net_reals_bar)))
-	(ui-grid _ (:grid_height 1 :flow_flags +flow_down_fill :maximum 100 :value 0)
-		(ui-flow _ (:color +argb_green)
-			(ui-label _ (:text "Regs (bops/s)" :color +argb_white))
-			(ui-grid regs_scale_grid (:grid_width +scale_size :grid_height 1 :color +argb_white
-					:font *env_medium_terminal_font*)
-				(times +scale_size (ui-label _
-					(:text "|" :flow_flags (logior +flow_flag_align_vcenter +flow_flag_align_hright)))))
-			(ui-grid regs_bar_grid (:grid_width 1)))
-		(ui-flow _ (:color +argb_yellow)
-			(ui-label _ (:text "Memory (bops/s)" :color +argb_white))
-			(ui-grid memory_scale_grid (:grid_width +scale_size :grid_height 1 :color +argb_white
-					:font *env_medium_terminal_font*)
-				(times +scale_size (ui-label _
-					(:text "|" :flow_flags (logior +flow_flag_align_vcenter +flow_flag_align_hright)))))
-			(ui-grid memory_bar_grid (:grid_width 1)))
-		(ui-flow _ (:color +argb_red)
-			(ui-label _ (:text "Reals (mops/s)" :color +argb_white))
-			(ui-grid reals_scale_grid (:grid_width +scale_size :grid_height 1 :color +argb_white
-					:font *env_medium_terminal_font*)
-				(times +scale_size (ui-label _
-					(:text "|" :flow_flags (logior +flow_flag_align_vcenter +flow_flag_align_hright)))))
-			(ui-grid reals_bar_grid (:grid_width 1)))))
+	(ui-grid _ (:grid_height 1)
+		(ui-hchart net_regs_chart "Net Regs (bops/s)" +scale_size +bops (:color +argb_green))
+		(ui-hchart net_memory_chart "Net Memory (bops/s)" +scale_size +bops (:color +argb_yellow))
+		(ui-hchart net_reals_chart "Net Reals (mops/s)" +scale_size +mops (:color +argb_red)))
+	(ui-grid _ (:grid_height 1)
+		(ui-hchart regs_chart "Regs (bops/s)" +scale_size +bops (:color +argb_green))
+		(ui-hchart memory_chart "Memory (bops/s)" +scale_size +bops (:color +argb_yellow))
+		(ui-hchart reals_chart "Reals (mops/s)" +scale_size +mops (:color +argb_red))))
 
 (defun create (key now)
 	; (create key now) -> val
 	;function called when entry is created
-	(.-> (defq ub (Progress) ab (Progress) tb (Progress) node (emap))
+	(.-> (defq node (emap))
 		(:insert :timestamp now)
-		(:insert :regs_bar tb)
-		(:insert :memory_bar ab)
-		(:insert :reals_bar ub)
+		(:insert :regs_bar (. regs_chart :add_bar))
+		(:insert :memory_bar (. memory_chart :add_bar))
+		(:insert :reals_bar (. reals_chart :add_bar))
 		(:insert :regs_results (list))
 		(:insert :memory_results (list))
 		(:insert :reals_results (list)))
-	(. reals_bar_grid :add_child ub)
-	(. memory_bar_grid :add_child ab)
-	(. regs_bar_grid :add_child tb)
 	(open-task "apps/netspeed/child.lisp" key +kn_call_open 0 (elem-get +select_task select))
 	node)
 
@@ -87,37 +48,49 @@
 	(.-> node (:find :memory_bar) :sub)
 	(.-> node (:find :regs_bar) :sub))
 
-(defun update-scale (scale max_scale units)
-	(defq scale (.-> scale :dirty_all :children) steps (/ max_scale (length scale)))
-	(each (lambda (mark)
-		(def mark :text (str (/ (* (inc _) steps) units) "|"))
-		(. mark :layout)) scale))
-
 (defun smooth-result (results val)
 	(if (> (length (push results val)) +smooth_steps)
 		(setq results (slice 1 -1 results)))
 	(list results (/ (reduce + results 0) (length results))))
 
-(defun update-result (node vops max_vops max_vops_align bsym rsym)
-	(bind '(results vops) (smooth-result (. node :find rsym) vops))
-	(def (. (. (. node :insert rsym results) :find bsym) :dirty)
-		:value vops :maximum (align (max max_vops vops) max_vops_align)))
+(defun update-result (node reg mem real)
+	(bind '(regs_results reg) (smooth-result (. node :find :regs_results) reg))
+	(bind '(memory_results mem) (smooth-result  (. node :find :memory_results) mem))
+	(bind '(reals_results real) (smooth-result (. node :find :reals_results) real))
+	(. node :insert :regs_results regs_results)
+	(. node :insert :memory_results memory_results)
+	(. node :insert :reals_results reals_results)
+	(def regs_chart :maximum (align (max reg (get :maximum regs_chart)) +max_bops_align))
+	(def memory_chart :maximum (align (max mem (get :maximum memory_chart)) +max_bops_align))
+	(def reals_chart :maximum (align (max real (get :maximum reals_chart)) +max_mops_align))
+	(def (.-> node (:find :regs_bar) :dirty) :value reg)
+	(def (.-> node (:find :memory_bar) :dirty) :value mem)
+	(def (.-> node (:find :reals_bar) :dirty) :value real))
 
-(defun update-net-result (grid bar max_vops max_vops_align rsym)
-	(bind '(results total_vops) (smooth-result (get rsym)
-		(reduce (# (+ %0 (get :value %1))) (. grid :children) 0)))
-	(set (penv) rsym results)
-	(def (. bar :dirty) :value total_vops
-		:maximum (align (max max_vops total_vops) max_vops_align)))
+(defun update-net-result ()
+	(bind '(regs_results total_regs) (smooth-result net_regs_results
+		(reduce (# (+ %0 (get :value %1))) (.-> regs_chart :get_bar_grid :children) 0)))
+	(bind '(mem_results total_mem) (smooth-result net_memory_results
+		(reduce (# (+ %0 (get :value %1))) (.-> memory_chart :get_bar_grid :children) 0)))
+	(bind '(real_results total_real) (smooth-result net_reals_results
+		(reduce (# (+ %0 (get :value %1))) (.-> reals_chart :get_bar_grid :children) 0)))
+	(setq net_regs_results regs_results net_memory_results mem_results net_reals_results real_results)
+	(def net_regs_chart :maximum (align (max total_regs (get :maximum net_regs_chart)) +max_bops_align))
+	(def net_memory_chart :maximum (align (max total_mem (get :maximum net_memory_chart)) +max_bops_align))
+	(def net_reals_chart :maximum (align (max total_real (get :maximum net_reals_chart)) +max_mops_align))
+	(def (. net_regs_bar :dirty) :value total_regs)
+	(def (. net_memory_bar :dirty) :value total_mem)
+	(def (. net_reals_bar :dirty) :value total_real))
 
 (defun main ()
-	(bind '(x y w h) (apply view-locate (. *window* :pref_size)))
-	(gui-add-front (. *window* :change x y w h))
 	(defq id :t select (alloc-select +select_size)
-		:net_regs_results (list) :net_memory_results (list) :net_reals_results (list)
-		net_max_regs +max_bops_align net_max_memory +max_bops_align net_max_reals +max_mops_align
-		max_regs +max_bops_align max_memory +max_bops_align max_reals +max_mops_align
+		net_regs_bar (. net_regs_chart :add_bar)
+		net_memory_bar (. net_memory_chart :add_bar)
+		net_reals_bar (. net_reals_chart :add_bar)
+		net_regs_results (list) net_memory_results (list) net_reals_results (list)
 		global_tasks (Global create destroy) poll_que (list))
+	(bind '(x y w h) (apply view-locate (. *window* :pref_size)))
+	(gui-add-front (. *window* :change_dirty x y w h))
 	(mail-timeout (elem-get +select_nodes select) 1 0)
 	(while id
 		(defq msg (mail-read (elem-get (defq idx (mail-select select)) select)))
@@ -152,29 +125,27 @@
 			(+select_reply
 				;child poll responce
 				(when (defq node (. global_tasks :find (getf msg +reply_node)))
-					(setq max_regs (update-result node (getf msg +reply_vops_regs) max_regs +max_bops_align :regs_bar :regs_results)
-						max_memory (update-result node (getf msg +reply_vops_memory) max_memory +max_bops_align :memory_bar :memory_results)
-						max_reals (update-result node (getf msg +reply_vops_reals) max_reals +max_mops_align :reals_bar :reals_results))
+					(update-result node
+						(getf msg +reply_vops_regs)
+						(getf msg +reply_vops_memory)
+						(getf msg +reply_vops_reals))
 					(. node :insert :timestamp (pii-time))
 					(push poll_que (. node :find :child))))
 			(:t	;polling timer event
 				(mail-timeout (elem-get +select_nodes select) +poll_rate 0)
 				(when (. global_tasks :refresh +retry_timeout)
 					;nodes have mutated
-					(. reals_bar_grid :layout) (. memory_bar_grid :layout) (. regs_bar_grid :layout)
 					(bind '(x y w h) (apply view-fit
 						(cat (. *window* :get_pos) (. *window* :pref_size))))
 					(. *window* :change_dirty x y w h))
 				;set scales
-				(setq net_max_regs (update-net-result regs_bar_grid net_regs_bar net_max_regs +max_bops_align :net_regs_results)
-					net_max_memory (update-net-result memory_bar_grid net_memory_bar net_max_memory +max_bops_align :net_memory_results)
-					net_max_reals (update-net-result reals_bar_grid net_reals_bar net_max_reals +max_mops_align :net_reals_results))
-				(update-scale regs_scale_grid max_regs +bops)
-				(update-scale memory_scale_grid max_memory +bops)
-				(update-scale reals_scale_grid max_reals +mops)
-				(update-scale net_regs_scale_grid net_max_regs +bops)
-				(update-scale net_memory_scale_grid net_max_memory +bops)
-				(update-scale net_reals_scale_grid net_max_reals +mops)
+				(update-net-result)
+				(. regs_chart :update_scale)
+				(. memory_chart :update_scale)
+				(. reals_chart :update_scale)
+				(. net_regs_chart :update_scale)
+				(. net_memory_chart :update_scale)
+				(. net_reals_chart :update_scale)
 				;poll any ready children
 				(each (# (mail-send %0 (elem-get +select_reply select))) poll_que)
 				(clear poll_que))))
