81d37ccc9564eae7517ddd2eb3006212875d3d5b
Chris Hinsley <chris.hinsley@gmail.com>
2023-07-05 11:59:15 +0100

    relative paths

--- gui/gui/gui.lisp
+++ gui/gui/gui.lisp
@@ -1,13 +1,13 @@
 ;can this node host a GUI ?
 (env-push)
 (import "sys/lisp.inc")
-(import "gui/gui/lisp.inc")
+(import "./lisp.inc")
 (if (gui-info)
 	;single instance per node only
 	(when (= 0 (length
 			(filter (# (eql (slice +long_size -1 (task-mailbox)) (slice +long_size -1 %0)))
 			(map (# (to-net-id (elem-get 1 (split %0 ",")))) (mail-enquire "GUI_SERVICE")))))
 		(env-pop)
-		(import "gui/gui/gui_impl.lisp")
+		(import "./gui_impl.lisp")
 		(env-push)))
 (env-pop)
