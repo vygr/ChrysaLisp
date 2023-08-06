04b678258d8ce5c879ccaea4cac9c2abc61c3315
Chris Hinsley <chris.hinsley@gmail.com>
2023-08-06 13:42:02 +0100

    Update app.lisp

--- apps/fonts/app.lisp
+++ apps/fonts/app.lisp
@@ -41,7 +41,7 @@
 	(bind '(x y w h) (apply view-fit (cat (. *window* :get_pos) (. *window* :pref_size))))
 	(. *window* :change_dirty x y w h))
 
-(defq index 1 id :t fonts (all-files "fonts/" '(".ctf")))
+(defq index 1 id :t fonts (sort cmp (all-files "fonts/" '(".ctf"))))
 
 (ui-window *window* ()
 	(ui-title-bar window_title "" (0xea19) +event_close)
