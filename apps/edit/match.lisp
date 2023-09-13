febe8cb35fbc44f3ab3299f7e09cbba73ab5b646
Chris Hinsley <chris.hinsley@gmail.com>
2023-09-13 21:51:49 +0100

    module for global `:match?`

--- apps/edit/match.lisp
+++ apps/edit/match.lisp
@@ -1,16 +1,16 @@
-;;;;;;;;;;;;;;
-; match? Child
-;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;
+; global match?
+;;;;;;;;;;;;;;;
 
 (import "sys/lisp.inc")
 (import "class/lisp.inc")
 
-(defq +_timeout 5000000)
+(defq +timeout 5000000)
 
-(enums +_select 0
+(enums +select 0
 	(enum main timeout))
 
-(structure +_job 0
+(structure +job 0
 	(long key)
 	(netid reply)
 	(int result)
@@ -24,29 +24,29 @@
 		result))
 
 (defun main ()
-	(defq *select* (alloc-select +_select_size) *working* :t *msg* :nil)
+	(defq *select* (alloc-select +select_size) *working* :t *msg* :nil)
 	(while *working*
-		(mail-timeout (elem-get +_select_timeout *select*) +_timeout 0)
+		(mail-timeout (elem-get +select_timeout *select*) +timeout 0)
 		(setq *msg* (mail-read (elem-get (defq *idx* (mail-select *select*)) *select*)))
 		(cond
 			;timeout or quit
-			((or (= *idx* +_select_timeout) (eql *msg* ""))
+			((or (= *idx* +select_timeout) (eql *msg* ""))
 				(setq *working* :nil))
 			;main mailbox
-			((= *idx* +_select_main)
+			((= *idx* +select_main)
 				;clear timeout
-				(mail-timeout (elem-get +_select_timeout *select*) 0 0)
+				(mail-timeout (elem-get +select_timeout *select*) 0 0)
 				;read job
-				(defq *reply_key* (getf *msg* +_job_key)
-					*reply_mbox* (getf *msg* +_job_reply)
+				(defq *reply_key* (getf *msg* +job_key)
+					*reply_mbox* (getf *msg* +job_reply)
 					result 0)
 				(bind '(file pattern whole_words regexp)
-					(elem-get 0 (read (string-stream (slice +_job_params -1 *msg*)) (ascii-code " "))))
+					(elem-get 0 (read (string-stream (slice +job_params -1 *msg*)) (ascii-code " "))))
 				(when (defq search (if regexp +*regexp* +*substr*)
 						pattern (map (const progn) pattern)
 						meta (. search :compile pattern))
 					(setq result (if (file-match? file search pattern meta) 1 0)))
 				;send reply
-				(mail-send *reply_mbox* (setf (slice 0 +_job_params *msg*)
-					+_job_result result)))))
+				(mail-send *reply_mbox* (setf (slice 0 +job_params *msg*)
+					+job_result result)))))
 	(free-select *select*))
