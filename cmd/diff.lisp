2cd0848940a099daf3f8e936e1b24a6f81673406
Chris Hinsley <chris.hinsley@gmail.com>
2023-10-25 09:54:36 +0100

    num vector paths

--- cmd/diff.lisp
+++ cmd/diff.lisp
@@ -16,9 +16,8 @@
 (defq +min_paths 4 +max_paths 16)
 
 (defun goal-distance (p)
-	(bind '(x y) p)
-	(defq x (- x lal) y (- y lbl))
-	(+ (* x x) (* y y)))
+	(defq _ (slice -3 -1 p))
+	(nums-sum (nums-mul (nums-sub goal _ _) _ _)))
 
 (defun main ()
 	;initialize pipe details and command args, abort on error
@@ -29,29 +28,30 @@
 		(defq a (file-stream (second args)) b (file-stream (third args)))
 		(when (and a b)
 			(defq al (list) _ (each-line (# (push al %0)) a) lal (length al)
-				bl (list) _ (each-line (# (push bl %0)) b) lbl (length bl))
+				bl (list) _ (each-line (# (push bl %0)) b) lbl (length bl)
+				goal (nums lal lbl))
 			(cond
 				((and (= lal 0) (= lbl 0)))
 				((= lal 0) (each (# (print "+ " _ " " %0)) bl))
 				((= lbl 0) (each (# (print "- " _ " " %0)) al))
-				(:t (defq ps (list (list (list 0 0))) nps (list) run :t)
+				(:t (defq ps (list (nums 0 0)) nps (list) run :t)
 					(while run
 						(each (lambda (p)
-							(bind '(a b) (last p))
+							(bind '(a b) (slice -3 -1 p))
 							(push nps (cond
 								((or (/= a lal) (/= b lbl))
 									(cond
-										((= a lal) (push p (list a (inc b))))
-										((= b lbl) (push p (list (inc a) b)))
+										((= a lal) (push p a (inc b)))
+										((= b lbl) (push p (inc a) b))
 										((eql (elem-get a al) (elem-get b bl))
-											(push p (list (inc a) (inc b))))
-										(:t (push nps (push (cat p) (list a (inc b))))
-											(push p (list (inc a) b)))))
+											(push p (inc a) (inc b)))
+										(:t (push nps (push (cat p) a (inc b)))
+											(push p (inc a) b))))
 								(:t (setq run :nil) p)))) ps)
 						(setq ps (cat nps)) (clear nps)
 						(when (> (length ps) +max_paths)
 							(setq ps (slice 0 +min_paths (sort (# (-
-								(goal-distance (last %0)) (goal-distance (last %1)))) ps)))))
+								(goal-distance %0) (goal-distance %1))) ps)))))
 					(defq a -1 b -1 oa 0)
 					(sort (# (- (length %0) (length %1))) ps)
 					(each (lambda ((pa pb))
@@ -62,4 +62,4 @@
 									(setq oa (inc oa)))
 								(:t (print "- " (+ a oa) " " (elem-get a al))))
 							(setq a pa b pb))
-						(first ps)))))))
+						(partition 2 (first ps))))))))
