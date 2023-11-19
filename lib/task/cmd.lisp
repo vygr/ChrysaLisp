a4ebcc5cc3d157a3cad69c6c2cc9d86746f664b7
Chris Hinsley <chris.hinsley@gmail.com>
2023-11-19 19:18:42 +0000

    tidy up

--- lib/task/cmd.lisp
+++ lib/task/cmd.lisp
@@ -4,33 +4,33 @@
 
 (import "lib/task/pipe.inc")
 
-(defq +_timeout 2000000)
+(defq +timeout 2000000)
 
-(enums +_select 0
+(enums +select 0
 	(enum main timeout))
 
-(structure +_job 0
+(structure +job 0
 	(long key)
 	(netid reply)
 	(offset params))
 
 (defun main ()
-	(defq *select* (alloc-select +_select_size) *working* :t *msg* :nil)
+	(defq *select* (alloc-select +select_size) *working* :t *msg* :nil)
 	(while *working*
-		(mail-timeout (elem-get +_select_timeout *select*) +_timeout 0)
+		(mail-timeout (elem-get +select_timeout *select*) +timeout 0)
 		(setq *msg* (mail-read (elem-get (defq *idx* (mail-select *select*)) *select*)))
 		(cond
 			;timeout or quit
-			((or (= *idx* +_select_timeout) (eql *msg* ""))
+			((or (= *idx* +select_timeout) (eql *msg* ""))
 				(setq *working* :nil))
 			;main mailbox
-			((= *idx* +_select_main)
+			((= *idx* +select_main)
 				;clear timeout
-				(mail-timeout (elem-get +_select_timeout *select*) 0 0)
+				(mail-timeout (elem-get +select_timeout *select*) 0 0)
 				;read job
-				(defq *reply_key* (getf *msg* +_job_key)
-					*reply_mbox* (getf *msg* +_job_reply)
-					*cmd* (slice +_job_params -1 *msg*))
+				(defq *reply_key* (getf *msg* +job_key)
+					*reply_mbox* (getf *msg* +job_reply)
+					*cmd* (slice +job_params -1 *msg*))
 				;run the command and catch output
 				(setq *msg* (string-stream (cat "")))
 				(write-line *msg* (str *reply_key*))
