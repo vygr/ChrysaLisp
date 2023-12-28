d8e4e67e83a5d62ea6cd13b6ba1bf93881d0f0e0
Chris Hinsley <chris.hinsley@gmail.com>
2023-12-28 12:13:59 +0000

    Update app.lisp

--- apps/docs/app.lisp
+++ apps/docs/app.lisp
@@ -79,7 +79,7 @@
 
 (defun main ()
 	(defq select (alloc-select +select_size) syntax (Syntax) handlers (Emap)
-		scroll_pos (Fmap) *running* :t *current_file* "docs/vm/vp_vm.md"
+		scroll_pos (Fmap) *running* :t *current_file* "docs/apps/docs.md"
 		*page_scale* 1.0 *regexp* :nil *whole_words* :nil
 		*last_key* "" *last_files* (list) *page_words* (Fset 101)
 		*last_widget* :nil *search_widgets* (list))
