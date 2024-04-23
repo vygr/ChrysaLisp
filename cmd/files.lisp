dba99d27d23394f0271c813c4d8e8c24596f5efe
Chris Hinsley <chris.hinsley@gmail.com>
2024-04-23 13:36:21 +0100

    add the observations

--- cmd/files.lisp
+++ cmd/files.lisp
@@ -32,14 +32,18 @@
 			(defq stdio (create-stdio))
 			(defq opt_d :nil opt_i :nil opt_a :nil
 				args (options stdio usage)))
+		;here we are good to go...
 		(defq postfix (if (< (length args) 2) "." (second args))
 			prefix (if (< (length args) 3) "" (third args)))
 		(if (ends-with "/" postfix) (setq postfix (most postfix)))
+		;got our ducks in order, so we use a lathe... a reduction ladder !
 		(each (const print) (usort
 			(reduce (lambda (files (option func)) (if option (func files) files))
+				;the tools we apply to the workpiece... sequentially
 				(list
 					(list opt_i (# (usort (flatten (map (const files-depends) %0)))))
 					(list opt_a (const files-all-depends))
 					(list opt_d (const files-dirs)))
+				;the initial list of files, from the command line filters.
 				(map (# (if (starts-with "./" %0) (slice %0 2 -1) %0))
 					(files-all postfix `(,prefix))))))))
