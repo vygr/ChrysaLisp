29e273f160ff91320279df6b5f99ba28fdbced71
Chris Hinsley <chris.hinsley@gmail.com>
2024-04-27 11:51:06 +0100

    `some!` mode `:t` is fraction faster

--- cmd/test.lisp
+++ cmd/test.lisp
@@ -10,22 +10,19 @@
 	Simple timing test framework.")
 ))
 
-(defun f0 (_)
-	(defq out (list))
-	(walk-list _
-		;element func
-		(lambda (_) (unless (list? _) (push out _)))
-		;enter list func
-		(lambda (_) :nil)
-		;exit list func
-		progn) out)
-
-(defun f1 (lst)
+(defun f0 (lst)
 	(defq out (list) stack (list lst 0))
 	(while (defq idx (pop stack) lst (pop stack))
 		(some! idx -1 :nil (# (cond
 			((list? %0) (push stack lst (inc _) %0 0))
 			(:t (push out %0) :nil))) (list lst))) out)
+
+(defun f1 (lst)
+	(defq out (list) stack (list lst 0))
+	(while (defq idx (pop stack) lst (pop stack))
+		(some! idx -1 :t (# (cond
+			((list? %0) (push stack lst (inc _) %0 0) :nil)
+			((push out %0)))) (list lst))) out)
 
 (defmacro time-it (name cnt &rest _)
 	`(progn
@@ -41,7 +38,7 @@
 	(when (and
 			(defq stdio (create-stdio))
 			(defq args (options stdio usage)))
-		(defq s '(1 2 3(4 5)((6 7)(8 9))) c 1000000)
+		(defq s '(1 2 3 (4 5) ((6 7) (8 9)) 10 11 12) c 1000000)
 		(time-it "f0" c (f0 s))
 		(time-it "f1" c (f1 s))
 		(time-it "f0" c (f0 s))
