2806cc8af5ff5939a0b566e1a7cde1e7e079fc51
Chris Hinsley <chris.hinsley@gmail.com>
2025-01-01 13:07:20 +0000

    Update app_impl.lisp

--- service/audio/app_impl.lisp
+++ service/audio/app_impl.lisp
@@ -1,17 +1,14 @@
 (import "./app.inc")
 
-;lisp bindings
-(ffi audio-init "service/audio/lisp_init" 0)
-(ffi audio-deinit "service/audio/lisp_deinit" 0)
-(ffi audio-add-sfx "service/audio/lisp_add_sfx" 0)
-(ffi audio-play-sfx "service/audio/lisp_play_sfx" 0)
-(ffi audio-change-sfx "service/audio/lisp_change_sfx" 0)
-(ffi audio-remove-sfx "service/audio/lisp_remove_sfx" 0)
+(defmacro audio-call (func &rest args)
+	(setq func (cat "service/audio/lisp_" (str func) "_sfx"))
+	`(mail-send reply_id (setf-> (str-alloc +audio_sfx_ret_size)
+		(+audio_sfx_ret_retval ((const (ffi _ ,func 0)) ~args)))))
 
 (defun main ()
 	; Declare the audio service
 	(defq audio_service (mail-declare (task-netid) "Audio" "Audio Service 0.1"))
-	(audio-init)
+	((ffi _ "service/audio/lisp_init" 0))
 	; Main loop
 	(while :t
 		(let ((msg (mail-read (task-netid))))
@@ -20,25 +17,16 @@
 			(case type
 				(+audio_type_add_sfx
 					; Add sound effect
-					(mail-send reply_id (setf-> (str-alloc +audio_sfx_ret_size)
-						(+audio_sfx_ret_retval (audio-add-sfx
-							(slice msg +audio_add_sfx_size -1))))))
+					(audio-call add (slice msg +audio_add_sfx_size -1)))
 				(+audio_type_play_sfx
 					; Play sound effect
-					(mail-send reply_id (setf-> (str-alloc +audio_sfx_ret_size)
-						(+audio_sfx_ret_retval (audio-play-sfx
-							(getf msg +audio_play_sfx_handle))))))
+					(audio-call play (getf msg +audio_play_sfx_handle)))
 				(+audio_type_change_sfx
 					; Change playing state
-					(mail-send reply_id (setf-> (str-alloc +audio_sfx_ret_size)
-						(+audio_sfx_ret_retval (audio-change-sfx
-							(getf msg +audio_change_sfx_handle)
-							(getf msg +audio_change_sfx_state))))))
+					(audio-call change (getf msg +audio_change_sfx_handle) (getf msg +audio_change_sfx_state)))
 				(+audio_type_remove_sfx
 					; Remove sound effect
-					(mail-send reply_id (setf-> (str-alloc +audio_sfx_ret_size)
-						(+audio_sfx_ret_retval (audio-remove-sfx
-							(getf msg +audio_remove_sfx_handle)))))))))
-	(audio-deinit)
+					(audio-call remove (getf msg +audio_remove_sfx_handle))))))
+	((ffi _ "service/audio/lisp_deinit" 0))
 	; Forget the audio service
 	(mail-forget audio_service))
