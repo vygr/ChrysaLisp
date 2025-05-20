28005aa0b800e9d4a47a2a0fdfa4b50138121799
Chris Hinsley <chris.hinsley@gmail.com>
2025-05-20 12:46:24 +0100

    consistant EOF

--- cmd/make.lisp
+++ cmd/make.lisp
@@ -240,9 +240,9 @@
 			(defq folder "host")
 			(if (defq i (find "/" file)) (setq folder (slice file 0 i)))
 			(. folders :update folder (# (if %0 (push %0 file) (list file)))))
-		(files-all "." '(".vp" ".inc" ".lisp" ".c" ".cpp" ".h" ".sh" ".ps1" ".bat") 2))
+		(files-all "." '("Makefile" ".vp" ".inc" ".lisp" ".c" ".cpp" ".h" ".sh" ".ps1" ".bat") 2))
 	(. folders :each
-		(# (push cmds (cat "cat -f " (apply cat (join %1 '(" "))) " | tee " %0 ".txt | null"))))
+		(# (push cmds (cat "cat -f " (apply cat (join %1 '(" "))) " | tee ai/" %0 ".txt | null"))))
 	(pipe-farm cmds))
 
 (defun main ()
