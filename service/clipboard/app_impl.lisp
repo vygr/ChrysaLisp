86811e4a78d858492b3a1743b87ec7b5ac679b89
Chris Hinsley <chris.hinsley@gmail.com>
2025-10-07 20:45:05 +0100

    Update app_impl.lisp

--- service/clipboard/app_impl.lisp
+++ service/clipboard/app_impl.lisp
@@ -14,8 +14,7 @@
 			(case (getf msg +clip_rpc_type)
 				(+clip_type_put
 					;put string on clipboard
-					(setq clipboard (slice msg +clip_put_size -1))
-					(clip-put clipboard)
+					(clip-put (setq clipboard (slice msg +clip_put_size -1)))
 					(mail-send reply_id ""))
 				(+clip_type_get
 					;get string from clipboard
