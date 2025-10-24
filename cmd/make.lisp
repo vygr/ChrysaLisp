90989fc2f285632235d2f0c252a9ce2b1370a8b5
Chris Hinsley <chris.hinsley@gmail.com>
2025-10-24 11:36:54 +0100

    Update make.lisp

--- cmd/make.lisp
+++ cmd/make.lisp
@@ -191,8 +191,7 @@
 			(if (defq i (find "/" file)) (setq folder (slice file 0 i)))
 			(. folders :update folder (# (if %0 (push %0 file) (list file)))))
 		(files-all "." '("Makefile" ".vp" ".inc" ".lisp" ".c" ".cpp" ".h" ".sh" ".ps1" ".bat") 2))
-	(. folders :each
-		(# (push cmds (cat "cat -f " (join %1 " ") " | tee ai/" %0 ".txt | null"))))
+	(. folders :each (# (push cmds (cat "cat -f " (join %1 " ") " | tee ai/" %0 ".txt | null"))))
 	(pipe-farm cmds))
 
 (defun main ()
