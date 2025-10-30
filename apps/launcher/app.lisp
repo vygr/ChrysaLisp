b7d7c768732b46ce0d0d16e2d3bd036256615f4f
Chris Hinsley <chris.hinsley@gmail.com>
2025-10-30 14:12:16 +0000

    Update app.lisp

--- apps/launcher/app.lisp
+++ apps/launcher/app.lisp
@@ -16,13 +16,20 @@
 		:order '("System" "Accessories" "Media" "Communication" "Games" "Science" "Generic")
 		:exclude '("launcher" "login" "wallpaper" "tui")
 		:categories (scatter (Fmap 11)
-			"System" (scatter (Lmap) :collapsed :nil :apps '("terminal" "services" "debug" "profile" "netmon" "netspeed" "files" "logout"))
-			"Accessories" (scatter (Lmap) :collapsed :nil :apps '("edit" "viewer" "docs" "fonts" "clock" "calculator"))
-			"Media" (scatter (Lmap) :collapsed :nil :apps '("images" "films" "canvas" "mesh" "raymarch"))
-			"Communication" (scatter (Lmap) :collapsed :nil :apps '("chat" "whiteboard"))
-			"Games" (scatter (Lmap) :collapsed :nil :apps '("boing" "freeball" "bubbles" "chess" "minefield"))
-			"Science" (scatter (Lmap) :collapsed :nil :apps '("molecule" "pcb" "mandelbrot"))
-			"Generic" (scatter (Lmap) :collapsed :nil :apps '("template")))))
+			"System" (scatter (Lmap) :collapsed :nil
+				:apps '("terminal" "services" "debug" "profile" "netmon" "netspeed" "files" "logout"))
+			"Accessories" (scatter (Lmap) :collapsed :nil
+				:apps '("edit" "viewer" "docs" "fonts" "clock" "calculator"))
+			"Media" (scatter (Lmap) :collapsed :nil
+				:apps '("images" "films" "canvas" "raymarch"))
+			"Communication" (scatter (Lmap) :collapsed :nil
+				:apps '("chat" "whiteboard"))
+			"Games" (scatter (Lmap) :collapsed :nil
+				:apps '("boing" "freeball" "bubbles" "chess" "minefield"))
+			"Science" (scatter (Lmap) :collapsed :nil
+				:apps '("molecule" "pcb" "mandelbrot" "mesh"))
+			"Generic" (scatter (Lmap) :collapsed :nil
+				:apps '()))))
 
 ; Configuration state
 (defq *config* :nil *config_file* (cat *env_home* "launcher.tre"))
@@ -37,24 +44,24 @@
 		(tree-save stream *config*)))
 
 (defun get-app-name (folder)
-	(if (defq i (rfind "/" folder))
-		(slice folder i -3)
+	(defq folder (slice folder 5 -3))
+	(if (defq i (find "/" folder))
+		(slice folder 0 i)
 		folder))
 
 (defun scan-apps ()
-	(defq disk_apps (map get-app-name (files-dirs (files-all "apps")))
-		exclude_list (. *config* :find :exclude)
-		categories (. *config* :find :categories))
+	(defq exclude_list (. *config* :find :exclude) categories (. *config* :find :categories)
+		disk_apps (filter (# (not (find %0 exclude_list)))
+			(usort (map get-app-name (files-dirs (files-all "apps" '("app.lisp")))))))
 	(each (lambda (app_name)
-		(unless (find app_name exclude_list)
-			(defq found :nil)
-			(. categories :each (lambda (name cat_data)
-				(if (find app_name (. cat_data :find :apps)) (setq found :t))))
-			(unless found
-				(. categories :update "Generic" (lambda (generic_cat)
-					(unless generic_cat
-						(setq generic_cat (scatter (Lmap) :collapsed :nil :apps '())))
-					(. generic_cat :insert :apps (sort (push (. generic_cat :find :apps) app_name))))))))
+		(defq found :nil)
+		(. categories :each (lambda (name cat_data)
+			(if (find app_name (. cat_data :find :apps)) (setq found :t))))
+		(unless found
+			(. categories :update "Generic" (lambda (generic_cat)
+				(unless generic_cat
+					(setq generic_cat (scatter (Lmap) :collapsed :nil :apps '())))
+				(. generic_cat :insert :apps (usort (push (. generic_cat :find :apps) app_name)))))))
 		disk_apps))
 
 (defun app-path (app_name)
@@ -65,12 +72,11 @@
 		collapsed (. cat_data :find :collapsed))
 	(when (nempty? apps)
 		(ui-root cat_flow (Flow) (:flow_flags +flow_down_fill :cat_name name)
-			(ui-flow header (:flow_flags +flow_right_fill :color *env_title_col*)
-				(. (ui-button toggle (:text (if collapsed ">" "v") :minimal :t
-						:color *env_title_col* :ink_color *env_ink_col*
-						:font *env_medium_terminal_font*))
+			(ui-flow header (:flow_flags +flow_right_fill)
+				(. (ui-button toggle (:text (if collapsed ">" "v")
+						:color *env_title_col* :font *env_medium_terminal_font*))
 					:connect +event_toggle)
-				(ui-label title (:text name :font *env_title_font* :color *env_title_col*)))
+				(ui-title title (:text name :color *env_title_col*)))
 			(ui-grid app_grid (:grid_width 2)
 				(each (lambda (app)
 					(. (ui-button _ (:text app)) :connect +event_launch))
@@ -97,16 +103,11 @@
 
 (defun main ()
 	; Initialization
-	(defq select (task-mboxes +select_size)
-		*running* :t)
+	(defq select (task-mboxes +select_size) *running* :t)
 	(setup-tooltips select)
 	(load-config)
 	(scan-apps)
 	(build-ui)
-
-	; Set the initial size of the main flow to fit its content
-	(bind '(w h) (. *main_flow* :pref_size))
-	(. *main_flow* :change 0 0 w h)
 
 	; Position and show window
 	(bind '(x y w h) (apply view-locate (push (. *window* :pref_size) *env_launcher_position*)))
@@ -146,8 +147,6 @@
 						(. grid :set_flags (if collapsed +view_flag_hidden 0) +view_flag_hidden)
 
 						; Recalculate size and update scrollbars after visibility change
-						(bind '(w h) (. *main_flow* :pref_size))
-						(. *main_flow* :change 0 0 w h)
 						(bind '(x y) (. *window* :get_pos))
 						(bind '(w h) (. *window* :pref_size))
 						(bind '(x y w h) (view-fit x y w h))
