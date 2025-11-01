97be9768a63c90f07e661782258d5b3b80a5e845
Chris Hinsley <chris.hinsley@gmail.com>
2025-11-01 10:40:29 +0000

    save position in config

--- apps/eyes/app.lisp
+++ apps/eyes/app.lisp
@@ -2,16 +2,18 @@
 (import "gui/lisp.inc")
 (import "lib/math/vector.inc")
 
-;;;
-;;; Configuration
-;;;
+;;;;;;;;;;;;;;;
+; configuration
+;;;;;;;;;;;;;;;
 
-(defq *config* :nil *config_version* 1
+(defq *config* :nil *config_version* 2
 	*config_file* (cat *env_home* "eyes.tre"))
 
 (defun get-default-config ()
 	(scatter (Emap)
 		:version *config_version*
+		:x 0
+		:y 0
 		:width min_width
 		:height min_height
 		:iris_color +argb_green
@@ -25,16 +27,22 @@
 		(setq *config* (get-default-config))))
 
 (defun save-config ()
+	(bind '(x y) (. *window* :get_pos))
+	(bind '(w h) (. *canvas* :get_size))
 	(scatter *config*
-		:iris_color *iris_color*
-		:iris_scale *iris_scale*
-		:pupil_scale *pupil_scale*)
+		:x x
+		:y y
+		:width w
+		:height h
+		:iris_color iris_color
+		:iris_scale iris_scale
+		:pupil_scale pupil_scale)
 	(when (defq stream (file-stream *config_file* +file_open_write))
 		(tree-save stream *config*)))
 
-;;;
-;;; UI and State
-;;;
+;;;;;;;;;;;;;;
+; UI and State
+;;;;;;;;;;;;;;
 
 (enums +event 0
 	(enum close max min))
@@ -46,14 +54,11 @@
 	(ui-title-bar _ "Eyes" (0xea19 0xea1b 0xea1a) +event_close)
 	(ui-backdrop *backdrop* (:style :plain :color +argb_black)))
 
-;;;
-;;; Drawing and Window Logic
-;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Drawing and Window Logic
+;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun resize-window (w h)
-	(. *config* :insert :width w)
-	(. *config* :insert :height h)
-
 	(def *backdrop* :min_width w :min_height h)
 	(if *canvas* (. *canvas* :sub))
 	(setq *canvas* (Canvas w h 1))
@@ -62,12 +67,12 @@
 
 	; Get current position and new preferred size
 	(bind '(x y) (. *window* :get_pos))
-	(bind '(pw ph) (. *window* :pref_size))
+	(bind '(w h) (. *window* :pref_size))
 
 	; Fit and apply change
-	(bind '(x y w h) (view-fit x y pw ph))
+	(bind '(x y w h) (view-fit x y w h))
 	(. *window* :change_dirty x y w h :t)
-	(setq *last_mx* -1 *last_my* -1))
+	(setq last_mx -1 last_my -1))
 
 (defun circle (r)
 	; Cached circle generation
@@ -84,8 +89,8 @@
 
 	; Calculate eye dimensions and positions using configured scales
 	(defq eye_radius (* h 0.48)
-		  iris_radius (* eye_radius *iris_scale*)
-		  pupil_radius (* iris_radius *pupil_scale*)
+		  iris_radius (* eye_radius iris_scale)
+		  pupil_radius (* iris_radius pupil_scale)
 		  highlight_radius (* pupil_radius 0.3)
 		  max_iris_dist (- eye_radius iris_radius)
 		  left_eye_cx (* w 0.25)
@@ -93,18 +98,18 @@
 		  eye_cy (* h 0.5))
 
 	; --- Left Eye ---
-	(defq vec_to_mouse (Vec2-f (- rel_mx left_eye_cx) (- rel_my eye_cy)))
-	(defq dist_to_mouse (vec-length vec_to_mouse))
-	(defq iris_offset (if (> dist_to_mouse 0.0)
-		(vec-scale (vec-norm vec_to_mouse) (min dist_to_mouse max_iris_dist))
-		(Vec2-f 0.0 0.0)))
+	(defq vec_to_mouse (Vec2-f (- rel_mx left_eye_cx) (- rel_my eye_cy))
+		dist_to_mouse (vec-length vec_to_mouse)
+		iris_offset (if (> dist_to_mouse 0.0)
+			(vec-scale (vec-norm vec_to_mouse) (min dist_to_mouse max_iris_dist))
+			(Vec2-f 0.0 0.0)))
 	(bind '(l_iris_px l_iris_py) (vec-add (Vec2-f left_eye_cx eye_cy) iris_offset))
 
 	; Draw left eye
 	(.-> *canvas*
 		(:set_color +argb_white)
 		(:fpoly left_eye_cx eye_cy +winding_odd_even (circle eye_radius))
-		(:set_color *iris_color*)
+		(:set_color iris_color)
 		(:fpoly l_iris_px l_iris_py +winding_odd_even (circle iris_radius))
 		(:set_color +argb_black)
 		(:fpoly l_iris_px l_iris_py +winding_odd_even (circle pupil_radius))
@@ -112,18 +117,18 @@
 		(:fpoly (+ l_iris_px (* pupil_radius -0.4)) (+ l_iris_py (* pupil_radius -0.4)) +winding_odd_even (circle highlight_radius)))
 
 	; --- Right Eye ---
-	(defq vec_to_mouse (Vec2-f (- rel_mx right_eye_cx) (- rel_my eye_cy)))
-	(defq dist_to_mouse (vec-length vec_to_mouse))
-	(defq iris_offset (if (> dist_to_mouse 0.0)
-		(vec-scale (vec-norm vec_to_mouse) (min dist_to_mouse max_iris_dist))
-		(Vec2-f 0.0 0.0)))
+	(defq vec_to_mouse (Vec2-f (- rel_mx right_eye_cx) (- rel_my eye_cy))
+		dist_to_mouse (vec-length vec_to_mouse)
+		iris_offset (if (> dist_to_mouse 0.0)
+			(vec-scale (vec-norm vec_to_mouse) (min dist_to_mouse max_iris_dist))
+			(Vec2-f 0.0 0.0)))
 	(bind '(r_iris_px r_iris_py) (vec-add (Vec2-f right_eye_cx eye_cy) iris_offset))
 
 	; Draw right eye
 	(.-> *canvas*
 		(:set_color +argb_white)
 		(:fpoly right_eye_cx eye_cy +winding_odd_even (circle eye_radius))
-		(:set_color *iris_color*)
+		(:set_color iris_color)
 		(:fpoly r_iris_px r_iris_py +winding_odd_even (circle iris_radius))
 		(:set_color +argb_black)
 		(:fpoly r_iris_px r_iris_py +winding_odd_even (circle pupil_radius))
@@ -132,50 +137,47 @@
 
 	(. *canvas* :swap 0))
 
-;;;
-;;; Main Loop
-;;;
+;;;;;;;;;;;
+; Main Loop
+;;;;;;;;;;;
 
 (defun main ()
 	(defq select (task-mboxes +select_size)
-		*last_mx* -1 *last_my* -1
-		+rate (/ 1000000 30) ; 30 FPS
+		last_mx -1 last_my -1
+		poll_rate (/ 1000000 30)
 		min_width 256 min_height 128
 		max_width 512 max_height 256
-		*canvas* :nil *running* :t)
+		*canvas* :nil running :t)
 	(load-config)
 
 	; Get initial dimensions and settings from config
-	(bind '(w h *iris_color* *iris_scale* *pupil_scale*)
-		(gather *config* :width :height :iris_color
+	(bind '(x y w h iris_color iris_scale pupil_scale)
+		(gather *config* :x :y :width :height :iris_color
 			:iris_scale :pupil_scale))
 
 	; Position and display the window for the first time
+	(. *window* :set_pos x y)
 	(resize-window w h)
-	(bind '(x y w h) (apply view-locate (. *window* :pref_size)))
-	(gui-add-front-rpc (. *window* :change x y w h))
+	(gui-add-front-rpc *window*)
 
-	(mail-timeout (elem-get select +select_timer) +rate 0)
-	(while *running*
+	(mail-timeout (elem-get select +select_timer) 1 0)
+	(while running
 		(defq msg (mail-read (elem-get select (defq idx (mail-select select)))))
 		(cond
 			((= idx +select_timer)
-				(mail-timeout (elem-get select +select_timer) +rate 0)
+				(mail-timeout (elem-get select +select_timer) poll_rate 0)
 				(bind '(mx my _ _) (gui-info))
-				(when (or (/= mx *last_mx*) (/= my *last_my*))
-					(setq *last_mx* mx *last_my* my)
+				(when (or (/= mx last_mx) (/= my last_my))
+					(setq last_mx mx last_my my)
 					(redraw mx my)))
-
+			;must be for +select_main !
 			((= (defq id (getf msg +ev_msg_target_id)) +event_close)
-				(setq *running* :nil))
-
+				(setq running :nil))
 			((= id +event_min)
 				(resize-window min_width min_height))
-
 			((= id +event_max)
 				(resize-window max_width max_height))
-
-			(:t (. *window* :event msg))))
+			((. *window* :event msg))))
 
 	(save-config)
 	(gui-sub-rpc *window*))
