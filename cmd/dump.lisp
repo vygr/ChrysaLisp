5931c09321147fb4b974baae41215b4bab3bb451
Chris Hinsley <chris.hinsley@gmail.com>
2025-11-06 11:57:23 +0000

    tidy up

--- cmd/dump.lisp
+++ cmd/dump.lisp
@@ -26,11 +26,12 @@
 	(when stream
 		(defq adr 0)
 		(while (defq c (read-chunk stream))
-			(prin (int-to-hex-str adr) " " (apply (const cat) (map (#
-				(cat (byte-to-hex-str %0) " ")) c)))
-			(times (- opt_c (length c)) (prin "   "))
-			(print (apply (const cat) (map (#
-				(if (<= 32 %0 126) (char %0) ".")) c)))
+			(print (int-to-hex-str adr) " "
+				(apply (const cat)
+					(map (# (cat (byte-to-hex-str %0) " ")) c))
+				(pad "" (* 3 (- opt_c (length c))) "                ")
+				(apply (const cat)
+					(map (# (if (<= 32 %0 126) (char %0) ".")) c)))
 			(setq adr (+ adr opt_c)))))
 
 (defun main ()
