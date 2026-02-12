3104ed866eb8fc0673fe5a7ac9e50aa541e097cd
Chris Hinsley <chris.hinsley@gmail.com>
2026-02-12 13:41:52 +0000

    Update edit.lisp
    gets first batch of tests correct. Can trim itself..
    
    `edit -c "(edit-trim)" cmd/edit.lisp`

--- cmd/edit.lisp
+++ cmd/edit.lisp
@@ -17,9 +17,9 @@
 	custom environment populated with editing primitives.
 
 	Available Commands:
-	
-	Search:     (edit-find pattern [:w] [:r]) 
-	Cursors:    (edit-cursors) (edit-add-cursors)
+
+	Search:	 (edit-find pattern [:w] [:r])
+	Cursors:	(edit-cursors) (edit-add-cursors)
 
 	Selection:  (edit-select-all) (edit-select-line)
 				(edit-select-word) (edit-select-block)
@@ -36,7 +36,7 @@
 
 	Mutation:   (edit-insert text) (edit-delete) (edit-backspace)
 				(edit-trim) (edit-sort) (edit-unique) (edit-upper)
-                (edit-lower) (edit-reflow) (edit-split) (edit-comment)
+				(edit-lower) (edit-reflow) (edit-split) (edit-comment)
 				(edit-indent) (edit-outdent)
 
 	Properties: (edit-get-text) (edit-get-filename)
@@ -44,50 +44,45 @@
 	Example - Numbering lines:
 
 	edit -c \q(edit-top) (defq i 0)
-        (while (edit-down)
-            (edit-home) (edit-insert (str (++ i) ': '))) \q
-        file.txt"
+		(while (edit-down)
+			(edit-home) (edit-insert (str (++ i) ': '))) \q
+		file.txt"
 	)
 (("-j" "--jobs") ,(opt-num 'opt_j))
 (("-c" "--cmd") ,(opt-str 'opt_c))
 (("-s" "--script") ,(opt-str 'opt_s))
 ))
 
-(defun setup-edit-env ()
-	; Programmatically define simple proxy commands
-	; These functions rely on *doc* being defined in the caller's scope
-	(each (lambda ((name method))
-			(eval (static-qq (defun ,name () (. *doc* ,method)))))
-		  '((edit-top :top) (edit-bottom :bottom) (edit-up :up) (edit-down :down)
-			(edit-left :left) (edit-right :right) (edit-home :home) (edit-end :end)
-			(edit-bracket-left :left_bracket) (edit-bracket-right :right_bracket)
-			(edit-ws-left :left_white_space) (edit-ws-right :right_white_space)
-			
-			(edit-select-all :select_all) (edit-select-line :select_line)
-			(edit-select-word :select_word) (edit-select-block :select_block)
-			(edit-select-form :select_form) (edit-select-para :select_paragraph)
-			(edit-select-left :left_select) (edit-select-right :right_select)
-			(edit-select-up :up_select) (edit-select-down :down_select)
-			(edit-select-home :home_select) (edit-select-end :end_select)
-			(edit-select-top :top_select) (edit-select-bottom :bottom_select)
+; define simple proxy commands
+(defmacro gen-edit (n m) `(defun ,(sym (str "edit-" n)) () (. *doc* ,m)))
+(gen-edit top :top) (gen-edit bottom :bottom) (gen-edit up :up) (gen-edit down :down)
+(gen-edit left :left) (gen-edit right :right) (gen-edit home :home) (gen-edit end :end)
+(gen-edit bracket-left :left_bracket) (gen-edit bracket-right :right_bracket)
+(gen-edit ws-left :left_white_space) (gen-edit ws-right :right_white_space)
+(gen-edit select-all :select_all) (gen-edit select-line :select_line)
+(gen-edit select-word :select_word) (gen-edit select-block :select_block)
+(gen-edit select-form :select_form) (gen-edit select-para :select_paragraph)
+(gen-edit select-left :left_select) (gen-edit select-right :right_select)
+(gen-edit select-up :up_select) (gen-edit select-down :down_select)
+(gen-edit select-home :home_select) (gen-edit select-end :end_select)
+(gen-edit select-top :top_select) (gen-edit select-bottom :bottom_select)
+(gen-edit delete :delete) (gen-edit backspace :backspace) (gen-edit trim :trim)
+(gen-edit sort :sort) (gen-edit unique :unique) (gen-edit upper :to_upper) (gen-edit lower :to_lower)
+(gen-edit reflow :reflow) (gen-edit split :split) (gen-edit comment :comment)
+(gen-edit indent :right_tab) (gen-edit outdent :left_tab)
 
-			(edit-delete :delete) (edit-backspace :backspace) (edit-trim :trim)
-			(edit-sort :sort) (edit-unique :unique) (edit-upper :to_upper) (edit-lower :to_lower)
-			(edit-reflow :reflow) (edit-split :split) (edit-comment :comment)
-			(edit-indent :right_tab) (edit-outdent :left_tab)))
-
-	; Define complex commands
-	(defun edit-insert (txt) (. *doc* :insert txt))
-	(defun edit-find (pattern &rest flags)
-		(. *doc* :find pattern (find :w flags) (find :r flags)))
-	(defun edit-cursors ()
-		(. *doc* :set_found_cursors (. *doc* :get_buffer_found)))
-	(defun edit-add-cursors ()
-		(. *doc* :add_found_cursors (. *doc* :get_buffer_found)))
-	(defun edit-get-text () (. *doc* :get_select))
-	(defun edit-get-filename () *file*)
-	(defun edit-print (&rest args)
-		(apply print (if args args (list (. *doc* :get_select))))))
+; more complex commands
+(defun edit-insert (txt) (. *doc* :insert txt))
+(defun edit-find (pattern &rest flags)
+	(. *doc* :find pattern (find :w flags) (find :r flags)))
+(defun edit-cursors ()
+	(. *doc* :set_found_cursors (. *doc* :get_buffer_found)))
+(defun edit-add-cursors ()
+	(. *doc* :add_found_cursors (. *doc* :get_buffer_found)))
+(defun edit-get-text () (. *doc* :get_select))
+(defun edit-get-filename () *file*)
+(defun edit-print (&rest args)
+	(apply print (if args args (list (. *doc* :get_select)))))
 
 (defun work (file)
 	; *doc* and *file* are bound here, visible to edit-script
@@ -95,11 +90,8 @@
 	(defq *doc* (Document) *file* file)
 	(catch
 		(progn
-			; Load
 			(. *doc* :stream_load (file-stream file))
-			; Run the compiled user script
 			(edit-script)
-			; Save
 			(. *doc* :stream_save (file-stream file +file_open_write))
 			(print "Edited: " file))
 		(print "Error editing " file ": " _)))
@@ -109,34 +101,26 @@
 	(when (and
 			(defq stdio (create-stdio))
 			(defq opt_j 1 opt_c :nil opt_s :nil args (options stdio usage)))
-		; Get file list (args or stdin)
+		; file list (args or stdin)
 		(if (empty? (defq jobs (rest args)))
 			(lines! (# (push jobs %0)) (io-stream 'stdin)))
-		; Prepare the script stream
+		; prepare the script stream
 		(defq script_stream (memory-stream))
 		(if opt_c (write-blk script_stream opt_c))
 		(if opt_s (write-blk script_stream (load opt_s)))
-		(when (and (> (stream-seek script_stream 0 2) 0) (nempty? jobs))
+		(when (and (>= (stream-seek script_stream 0 0) 0) (nempty? jobs))
 			(if (<= (length jobs) opt_j)
 				(progn
-					; Parse script into a list of forms
-					(stream-seek script_stream 0 0)
-					(defq forms (list) next " " form :t)
+					; parse user script(s)
+					(defq forms (list) next (ascii-code " ") form :t)
 					(while form
 						(bind '(form next) (read script_stream next))
 						(push forms form))
-					(setq forms (reverse forms))
-					; Create the editing environment
-					(env-push)
-					(setup-edit-env)
-					; Compile user script into a function 'edit-script'
-					; (defun edit-script () ...forms...)
-					(exec `(defun edit-script '() ~forms))
-					; Run locally
-					(each (const work) jobs)
-					; Clean up environment
-					(env-pop))
-				; Distribute to farm
+					; compile user script into a function 'edit-script'
+					(defq edit-script (macrobind `(lambda () ~forms)))
+					; run locally
+					(each (const work) jobs))
+				; distribute to farm
 				(each (lambda ((job result)) (prin result))
 					(pipe-farm (map (# (str (first args)
 							" -j " opt_j
