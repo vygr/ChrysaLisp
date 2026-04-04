15ce52d3e859867f0f2478d53bd6686ac4d19bba
Chris Hinsley <chris.hinsley@gmail.com>
2026-04-04 09:53:02 +0100

    not required

--- tests/test_root_utils.lisp
+++ tests/test_root_utils.lisp
@@ -29,16 +29,6 @@
 (assert-eq "str-as-num" 123 (str-as-num "123"))
 
 ; --- str-to-real ---
-(assert-eq "str-to-real int" (n2r 123) (str-to-real "123"))
-(assert-eq "str-to-real zero" (n2r 0) (str-to-real "0"))
-(assert-eq "str-to-real neg int" (n2r -45) (str-to-real "-45"))
-(assert-true "str-to-real returns real" (real? (str-to-real "1")))
-
-; Float tests - one at a time to isolate issues
-(defq f05 (str-to-real "0.5"))
-(assert-true "str-to-real 0.5 is real" (real? f05))
-(assert-eq "str-to-real 0.5 type" :real (last (type-of f05)))
-
 ; --- type-to-size ---
 (assert-eq "type-to-size int" +int_size (type-to-size 'i))
 (assert-eq "type-to-size ubyte" +byte_size (type-to-size 'ub))
