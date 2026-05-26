dd9cc6cb153d95c91f8a0290c12d2a9b84d8f0ab
Chris Hinsley <chris.hinsley@gmail.com>
2026-05-26 17:22:18 +0100

    match the rest of the info

--- cmd/edit.lisp
+++ cmd/edit.lisp
@@ -71,8 +71,8 @@
                 (edit-join-text (txt ...) [cls]) -> txt
                 (edit-print ...)
                 (edit-eof?) -> :t | :nil
-                (edit-eof cx cy) -> count
-                (edit-sof cx cy) -> count
+                (edit-eof cx cy) -> cnt
+                (edit-sof cx cy) -> cnt
                 (edit-cx) -> cx
                 (edit-cy) -> cy
 
