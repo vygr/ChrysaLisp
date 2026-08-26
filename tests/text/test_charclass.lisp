7f9348579759a9eba7acdf103ff00af6ff869e7f
Chris Hinsley <chris.hinsley@gmail.com>
2026-08-26 09:39:37 +0100

    extra escape/unescape tests

--- tests/text/test_charclass.lisp
+++ tests/text/test_charclass.lisp
@@ -8,10 +8,54 @@
 (assert-true "bfind match hex"   (bfind "C" cls))
 (assert-true "bfind miss"		(not (bfind "G" cls)))
 
-; --- Escaping ---
-(assert-eq "escape" "Hello\\nWorld" (escape "Hello\nWorld"))
+; --- Escaping: All standard character types ---
+(assert-eq "escape empty" "" (escape ""))
+(assert-eq "escape plain text" "abc XYZ 123" (escape "abc XYZ 123"))
+(assert-eq "escape newline" "a\\nb" (escape "a\nb"))
+(assert-eq "escape tab" "a\\tb" (escape "a\tb"))
+(assert-eq "escape carriage return" "a\\rb" (escape "a\rb"))
+(assert-eq "escape form feed" "a\\fb" (escape "a\fb"))
+(assert-eq "escape vertical tab" "a\\vb" (escape "a\vb"))
+(assert-eq "escape double quote" "a\\qb" (escape "a\qb"))
+(assert-eq "escape backslash" "a\\\\b" (escape "a\\b"))
+(assert-eq "escape all control chars" "\\r\\f\\v\\n\\t\\q\\\\" (escape "\r\f\v\n\t\q\\"))
+(assert-eq "escape consecutive" "\\n\\n\\t\\t" (escape "\n\n\t\t"))
+(assert-eq "escape interleaved" "a\\rb\\fc\\vd\\ne\\tf\\qg\\\\h" (escape "a\rb\fc\vd\ne\tf\qg\\h"))
 (assert-eq "escape-regexp" "Hello\\." (escape-regexp "Hello."))
-(assert-eq "unescape" "a\nb" (unescape "a\\nb"))
+
+; --- Unescaping: All standard character types ---
+(assert-eq "unescape empty" "" (unescape ""))
+(assert-eq "unescape plain text" "abc XYZ 123" (unescape "abc XYZ 123"))
+(assert-eq "unescape newline" "a\nb" (unescape "a\\nb"))
+(assert-eq "unescape tab" "a\tb" (unescape "a\\tb"))
+(assert-eq "unescape carriage return" "a\rb" (unescape "a\\rb"))
+(assert-eq "unescape form feed" "a\fb" (unescape "a\\fb"))
+(assert-eq "unescape vertical tab" "a\vb" (unescape "a\\vb"))
+(assert-eq "unescape double quote" "a\qb" (unescape "a\\qb"))
+(assert-eq "unescape backslash" "a\\b" (unescape "a\\\\b"))
+(assert-eq "unescape all control chars" "\r\f\v\n\t\q\\" (unescape "\\r\\f\\v\\n\\t\\q\\\\"))
+(assert-eq "unescape consecutive" "\n\n\t\t" (unescape "\\n\\n\\t\\t"))
+(assert-eq "unescape interleaved" "a\rb\fc\vd\ne\tf\qg\\h" (unescape "a\\rb\\fc\\vd\\ne\\tf\\qg\\\\h"))
+
+; --- Unescaping: Hex escapes (\xNN) ---
+(assert-eq "unescape hex space" " " (unescape "\\x20"))
+(assert-eq "unescape hex uppercase" "A" (unescape "\\x41"))
+(assert-eq "unescape hex lowercase" "z" (unescape "\\x7a"))
+(assert-eq "unescape hex upper digits" "Z" (unescape "\\x5A"))
+(assert-eq "unescape hex tilde" "~" (unescape "\\x7E"))
+(assert-eq "unescape hex null byte" (char 0) (unescape "\\x00"))
+(assert-eq "unescape hex 0xFF" (char 255) (unescape "\\xff"))
+(assert-eq "unescape hex mixed string" "Hello, World!" (unescape "Hello\\x2C\\x20World\\x21"))
+
+; --- Unescaping: Edge cases & Fallbacks ---
+(assert-eq "unescape unhandled escape char" "z" (unescape "\\z"))
+(assert-eq "unescape trailing backslash" "test\\" (unescape "test\\"))
+(assert-eq "unescape incomplete hex 1" "x" (unescape "\\x"))
+(assert-eq "unescape incomplete hex 2" "x4" (unescape "\\x4"))
+
+; --- Escape / Unescape Roundtrip ---
+(defq rt_sample "Line 1\t\qquoted\q\r\nLine 2\v\fEnd\\Path")
+(assert-eq "escape-unescape roundtrip" rt_sample (unescape (escape rt_sample)))
 
 ; --- bskip family tests ---
 
