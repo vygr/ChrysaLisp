70a88acabf00854899f46c78dc9756e8a8ca3cfc
Chris Hinsley <chris.hinsley@gmail.com>
2026-09-05 20:59:06 +0100

    async connect/dns

--- src/host/net.cpp
+++ src/host/net.cpp
@@ -6,6 +6,7 @@
 #ifdef _WIN64
 	#include <winsock2.h>
 	#include <ws2tcpip.h>
+	#include <windows.h>
 	typedef SOCKET socket_t;
 	#define SOCK_INVALID INVALID_SOCKET
 	#define SOCK_ERR SOCKET_ERROR
@@ -21,6 +22,7 @@
 	#include <poll.h>
 	#include <signal.h>
 	#include <errno.h>
+	#include <pthread.h>
 	typedef int socket_t;
 	#define SOCK_INVALID (-1)
 	#define SOCK_ERR (-1)
@@ -28,26 +30,21 @@
 
 #define MAX_NET_HANDLES 256
 
-static struct addrinfo hints;
-static char port_str[16];
-static struct sockaddr_in net_addr;
-static struct sockaddr_in client_addr;
-#ifdef _WIN64
-static WSAPOLLFD pfd;
-#else
-static struct pollfd pfd;
-#endif
-
 enum SockState {
 	SOCK_STATE_FREE = 0,
+	SOCK_STATE_RESOLVING,
 	SOCK_STATE_CONNECTING,
 	SOCK_STATE_CONNECTED,
-	SOCK_STATE_LISTENING
+	SOCK_STATE_LISTENING,
+	SOCK_STATE_FAILED
 };
 
 struct NetSlot {
 	socket_t fd;
-	int state;
+	volatile int state;
+	char host[256];
+	uint32_t port;
+	volatile int cancelled;
 };
 
 static NetSlot slots[MAX_NET_HANDLES];
@@ -75,12 +72,109 @@ static uint32_t alloc_slot(socket_t fd, int state) {
 		if (slots[i].state == SOCK_STATE_FREE) {
 			slots[i].fd = fd;
 			slots[i].state = state;
+			slots[i].cancelled = 0;
 			return i;
 		}
 	}
-	close_fd(fd);
+	if (fd != SOCK_INVALID) close_fd(fd);
 	return 0;
 }
+
+static void resolve_and_connect_worker(uint32_t handle) {
+	NetSlot *slot = &slots[handle];
+
+	char p_str[16];
+	snprintf(p_str, sizeof(p_str), "%u", slot->port);
+	struct addrinfo h_hints, *res = nullptr;
+
+	memset(&h_hints, 0, sizeof(h_hints));
+	h_hints.ai_family = AF_INET;
+	h_hints.ai_socktype = SOCK_STREAM;
+
+	int gai_err = getaddrinfo(slot->host, p_str, &h_hints, &res);
+
+	if (slot->cancelled) {
+		if (res) freeaddrinfo(res);
+		slot->state = SOCK_STATE_FREE;
+		return;
+	}
+
+	if (gai_err != 0 || !res) {
+		slot->state = SOCK_STATE_FAILED;
+		return;
+	}
+
+	socket_t fd = SOCK_INVALID;
+	int ret = SOCK_ERR;
+
+	for (struct addrinfo *p = res; p != nullptr; p = p->ai_next) {
+		fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+		if (fd == SOCK_INVALID) continue;
+
+		set_nonblocking(fd);
+		ret = connect(fd, p->ai_addr, (int)p->ai_addrlen);
+
+#ifdef _WIN64
+		if (ret == 0 || WSAGetLastError() == WSAEWOULDBLOCK) {
+			break;
+		}
+#else
+		if (ret == 0 || errno == EINPROGRESS) {
+			break;
+		}
+#endif
+		close_fd(fd);
+		fd = SOCK_INVALID;
+	}
+	freeaddrinfo(res);
+
+	if (slot->cancelled) {
+		if (fd != SOCK_INVALID) close_fd(fd);
+		slot->state = SOCK_STATE_FREE;
+		return;
+	}
+
+	if (fd == SOCK_INVALID) {
+		slot->state = SOCK_STATE_FAILED;
+		return;
+	}
+
+	slot->fd = fd;
+	__sync_synchronize();
+	slot->state = (ret == 0) ? SOCK_STATE_CONNECTED : SOCK_STATE_CONNECTING;
+}
+
+#ifdef _WIN64
+static DWORD WINAPI resolve_worker_win(LPVOID arg) {
+	resolve_and_connect_worker((uint32_t)(uintptr_t)arg);
+	return 0;
+}
+
+static void start_resolve_thread(uint32_t handle) {
+	HANDLE th = CreateThread(nullptr, 0, resolve_worker_win, (LPVOID)(uintptr_t)handle, 0, nullptr);
+	if (th) {
+		CloseHandle(th);
+	} else {
+		slots[handle].state = SOCK_STATE_FAILED;
+	}
+}
+#else
+static void* resolve_worker_posix(void* arg) {
+	resolve_and_connect_worker((uint32_t)(uintptr_t)arg);
+	return nullptr;
+}
+
+static void start_resolve_thread(uint32_t handle) {
+	pthread_t th;
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	if (pthread_create(&th, &attr, resolve_worker_posix, (void*)(uintptr_t)handle) != 0) {
+		slots[handle].state = SOCK_STATE_FAILED;
+	}
+	pthread_attr_destroy(&attr);
+}
+#endif
 
 int64_t host_net_init() {
 #ifdef _WIN64
@@ -92,6 +186,7 @@ int64_t host_net_init() {
 	for (int i = 0; i < MAX_NET_HANDLES; ++i) {
 		slots[i].fd = SOCK_INVALID;
 		slots[i].state = SOCK_STATE_FREE;
+		slots[i].cancelled = 0;
 	}
 	return 0;
 }
@@ -99,9 +194,13 @@ int64_t host_net_init() {
 int64_t host_net_deinit() {
 	for (int i = 1; i < MAX_NET_HANDLES; ++i) {
 		if (slots[i].state != SOCK_STATE_FREE) {
-			close_fd(slots[i].fd);
-			slots[i].state = SOCK_STATE_FREE;
-			slots[i].fd = SOCK_INVALID;
+			if (slots[i].state == SOCK_STATE_RESOLVING) {
+				slots[i].cancelled = 1;
+			} else {
+				close_fd(slots[i].fd);
+				slots[i].state = SOCK_STATE_FREE;
+				slots[i].fd = SOCK_INVALID;
+			}
 		}
 	}
 #ifdef _WIN64
@@ -111,40 +210,15 @@ int64_t host_net_deinit() {
 }
 
 uint32_t host_net_connect(const char *host, uint32_t port) {
-	snprintf(port_str, sizeof(port_str), "%u", port);
-	struct addrinfo *res = nullptr;
+	uint32_t h = alloc_slot(SOCK_INVALID, SOCK_STATE_RESOLVING);
+	if (h == 0) return 0;
 
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
+	slots[h].port = port;
+	slots[h].cancelled = 0;
+	strncpy(slots[h].host, host ? host : "", sizeof(slots[h].host) - 1);
+	slots[h].host[sizeof(slots[h].host) - 1] = '\0';
 
-	if (getaddrinfo(host, port_str, &hints, &res) != 0 || !res) return 0;
-
-	socket_t fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-	if (fd == SOCK_INVALID) {
-		freeaddrinfo(res);
-		return 0;
-	}
-
-	set_nonblocking(fd);
-
-	int ret = connect(fd, res->ai_addr, (int)res->ai_addrlen);
-	freeaddrinfo(res);
-
-#ifdef _WIN64
-	if (ret == SOCK_ERR && WSAGetLastError() != WSAEWOULDBLOCK) {
-		close_fd(fd);
-		return 0;
-	}
-#else
-	if (ret == SOCK_ERR && errno != EINPROGRESS) {
-		close_fd(fd);
-		return 0;
-	}
-#endif
-
-	int state = (ret == 0) ? SOCK_STATE_CONNECTED : SOCK_STATE_CONNECTING;
-	uint32_t h = alloc_slot(fd, state);
+	start_resolve_thread(h);
 	return h;
 }
 
@@ -235,6 +309,10 @@ int64_t host_net_close(uint32_t handle) {
 	if (handle == 0 || handle >= MAX_NET_HANDLES || slots[handle].state == SOCK_STATE_FREE) {
 		return -1;
 	}
+	if (slots[handle].state == SOCK_STATE_RESOLVING) {
+		slots[handle].cancelled = 1;
+		return 0;
+	}
 	close_fd(slots[handle].fd);
 	slots[handle].fd = SOCK_INVALID;
 	slots[handle].state = SOCK_STATE_FREE;
@@ -246,17 +324,26 @@ int64_t host_net_poll(uint32_t handle) {
 		return 4; // Error / disconnected
 	}
 
-	short events = POLLIN | POLLOUT;
+	if (slots[handle].state == SOCK_STATE_RESOLVING) {
+		return 0; // Still resolving in background
+	}
+
+	if (slots[handle].state == SOCK_STATE_FAILED) {
+		return 4; // Resolution failed or connect error
+	}
+
 #ifdef _WIN64
-	pfd.fd = slots[handle].fd;
-	pfd.events = (SHORT)events;
-	pfd.revents = 0;
-	int ret = WSAPoll(&pfd, 1, 0);
+	WSAPOLLFD local_pfd;
+	local_pfd.fd = slots[handle].fd;
+	local_pfd.events = POLLIN | POLLOUT;
+	local_pfd.revents = 0;
+	int ret = WSAPoll(&local_pfd, 1, 0);
 #else
-	pfd.fd = slots[handle].fd;
-	pfd.events = events;
-	pfd.revents = 0;
-	int ret = poll(&pfd, 1, 0);
+	struct pollfd local_pfd;
+	local_pfd.fd = slots[handle].fd;
+	local_pfd.events = POLLIN | POLLOUT;
+	local_pfd.revents = 0;
+	int ret = poll(&local_pfd, 1, 0);
 #endif
 	if (ret <= 0) return 0;
 
@@ -275,9 +362,9 @@ int64_t host_net_poll(uint32_t handle) {
 	}
 
 	int64_t flags = 0;
-	if (pfd.revents & (POLLERR | POLLHUP | POLLNVAL)) flags |= 4;
-	if (pfd.revents & POLLIN) flags |= 1;
-	if (pfd.revents & POLLOUT) {
+	if (local_pfd.revents & (POLLERR | POLLHUP | POLLNVAL)) flags |= 4;
+	if (local_pfd.revents & POLLIN) flags |= 1;
+	if (local_pfd.revents & POLLOUT) {
 		if (slots[handle].state == SOCK_STATE_CONNECTING) {
 			slots[handle].state = SOCK_STATE_CONNECTED;
 		}
