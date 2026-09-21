32c13f9758087c8a7694ecc5b13ba01a17775a2a
Chris Hinsley <chris.hinsley@gmail.com>
2026-09-21 10:03:11 +0100

    Update fmt.lisp

--- cmd/fmt.lisp
+++ cmd/fmt.lisp
@@ -1,6 +1,7 @@
 (import "lib/options/options.inc")
 (import "lib/task/cmd.inc")
 (import "lib/files/files.inc")
+(import "lib/text/syntax.inc")
 
 (defq usage `(
 (("-h" "--help")
@@ -25,65 +26,98 @@
 (defq +file_types ''(".lisp" ".inc" ".vp") +max_line_len 90 +tab_width 4)
 
 ;;;;;;;;;;;;;;;;;;;;;;;
-; Lossless Tokenizer
+; Fast Syntax Tokenizer
 ;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun tokenize-lisp (source)
-	(defq tokens (list) len (length source) idx 0 start 0 depth 0 ch "")
-	(while (< idx len)
-		(setq ch (elem-get source idx))
+	(defq tokens (list) stream (string-stream source) lines (list)
+		syntax (Syntax) line_idx 0)
+	(while (defq raw (read-line stream))
+		(push lines raw))
+	(defq num_lines (length lines))
+	(while (< line_idx num_lines)
+		(defq raw_line (trim-end (elem-get lines line_idx) "\r")
+			prev_state (. syntax :get_state))
 		(cond
-			((or (eql ch " ") (eql ch "\t"))
-				(setq start idx)
-				(while (and (< idx len)
-						(or (eql (defq c (elem-get source idx)) " ") (eql c "\t")))
-					(++ idx))
-				(push tokens (list :ws (slice source start idx))))
-			((or (eql ch "\n") (eql ch "\r"))
-				(if (and (eql ch "\r") (< (inc idx) len) (eql (elem-get source (inc idx)) "\n"))
-					(++ idx))
-				(++ idx)
-				(push tokens (list :nl "\n")))
-			((eql ch ";")
-				(setq start idx)
-				(while (and (< idx len)
-						(nql (defq c (elem-get source idx)) "\n") (nql c "\r"))
-					(++ idx))
-				(push tokens (list :comment (slice source start idx))))
-			((eql ch "\q")
-				(setq start idx)
-				(++ idx)
-				(while (and (< idx len) (nql (elem-get source idx) "\q"))
-					(if (and (eql (elem-get source idx) "\\") (< (inc idx) len))
-						(++ idx 2)
-						(++ idx)))
-				(if (< idx len) (++ idx))
-				(push tokens (list :string (slice source start idx))))
-			((eql ch "{")
-				(setq start idx depth 1)
-				(++ idx)
-				(while (and (< idx len) (> depth 0))
-					(defq c (elem-get source idx))
-					(cond
-						((eql c "{") (++ depth))
-						((eql c "}") (-- depth)))
-					(++ idx))
-				(push tokens (list :cscript (slice source start idx))))
-			((eql ch "(")
-				(push tokens (list :lparen "("))
-				(++ idx))
-			((eql ch ")")
-				(push tokens (list :rparen ")"))
-				(++ idx))
-			((find ch "'`~,")
-				(push tokens (list :quote (str ch)))
-				(++ idx))
+			((and (eql prev_state :text)
+				(starts-with "(defq usage" (trim-start raw_line)))
+				(defq u_lines (list) depth 0 in_str :nil done :nil)
+				(while (and (< line_idx num_lines) (not done))
+					(defq u_line (elem-get lines line_idx)
+						u_len (length u_line) ui 0)
+					(push u_lines u_line)
+					(while (< ui u_len)
+						(defq uc (elem-get u_line ui))
+						(cond
+							(in_str
+								(cond
+									((eql uc "\\") (++ ui))
+									((eql uc "\q") (setq in_str :nil))))
+							((eql uc ";")
+								(setq ui u_len))
+							((eql uc "\q")
+								(setq in_str :t))
+							((eql uc "(")
+								(++ depth))
+							((eql uc ")")
+								(-- depth)
+								(if (= depth 0) (setq done :t))))
+						(++ ui))
+					(++ line_idx))
+				(push tokens (list :raw (cat (join u_lines "\n") "\n"))))
 			(:t
-				(setq start idx)
-				(while (and (< idx len)
-						(not (find (elem-get source idx) " \t\r\n();{}\q'`~,")))
-					(++ idx))
-				(push tokens (list :atom (slice source start idx))))))
+				(bind '(toks states) (. syntax :tokenize raw_line))
+				(if (and (empty? toks) (find prev_state '(:string1 :string2)))
+					(elem-set (last tokens) 1 (cat (second (last tokens)) "\n"))
+					(each (lambda (val tok_state)
+						(cond
+							((find tok_state '(:string1 :string2))
+								(defq kind (if (eql tok_state :string1) :string :cscript))
+								(if (and (eql tok_state prev_state) (nempty? tokens) (eql (first (last tokens)) kind))
+									(elem-set (last tokens) 1 (cat (second (last tokens)) "\n" val))
+									(push tokens (list kind val))))
+							((eql tok_state :comment)
+								(push tokens (list :comment val)))
+							((find tok_state '(:number :keysym))
+								(push tokens (list :atom val)))
+							((eql tok_state :symbol)
+								(defq s val)
+								(while (and (nempty? s) (find (first s) "'`~,"))
+									(push tokens (list :quote (slice s 0 1)))
+									(setq s (slice s 1 -1)))
+								(if (nempty? s)
+									(push tokens (list :atom s))))
+							((eql tok_state :text)
+								(defq tlen (length val) ti 0)
+								(while (< ti tlen)
+									(defq ch (elem-get val ti))
+									(cond
+										((or (eql ch " ") (eql ch "\t"))
+											(defq ws_start ti)
+											(while (and (< ti tlen)
+												(or (eql (defq c (elem-get val ti)) " ") (eql c "\t")))
+												(++ ti))
+											(push tokens (list :ws (slice val ws_start ti))))
+										((eql ch "(")
+											(push tokens (list :lparen "("))
+											(++ ti))
+										((eql ch ")")
+											(push tokens (list :rparen ")"))
+											(++ ti))
+										((find ch "'`~,")
+											(push tokens (list :quote (str ch)))
+											(++ ti))
+										(:t
+											(defq atom_start ti)
+											(while (and (< ti tlen)
+												(not (find (elem-get val ti) " \t()'`~,")))
+												(++ ti))
+											(push tokens (list :atom (slice val atom_start ti)))))))))
+						toks states))
+				(when (< (inc line_idx) num_lines)
+					(unless (find (. syntax :get_state) '(:string1 :string2))
+						(push tokens (list :nl "\n"))))
+				(++ line_idx))))
 	tokens)
 
 ;;;;;;;;;;;;;;;;;;;;;;;
@@ -100,12 +134,12 @@
 	(defq len (length tokens) i (skip-ws-nl tokens idx))
 	(if (>= i len)
 		(list len 0)
-		(defq tok (elem-get tokens i) type (first tok))
+		(defq tok (elem-get tokens i) tok_type (first tok))
 		(cond
-			((eql type :quote)
+			((eql tok_type :quote)
 				(bind '(next_i next_len) (measure-form tokens (inc i)))
 				(list next_i (inc next_len)))
-			((eql type :lparen)
+			((eql tok_type :lparen)
 				(defq depth 1 cur_i (inc i) total_len 1)
 				(while (and (< cur_i len) (> depth 0))
 					(defq t_type (first (elem-get tokens cur_i))
@@ -125,14 +159,12 @@
 
 (defun form-kind (tokens idx)
 	(defq next_i (skip-ws-nl tokens idx))
-	(if (< next_i (length tokens))
-		(defq tok (elem-get tokens next_i))
-		(if (eql (first tok) :atom)
-			(case (second tok)
-				(("defq" "setq") :defq)
-				(("def" "set") :def)
-				(:t :normal))
-			:normal)
+	(if (and (< next_i (length tokens))
+		(eql (first (defq tok (elem-get tokens next_i))) :atom))
+		(case (second tok)
+			(("defq" "setq") :defq)
+			(("def" "set") :def)
+			(:t :normal))
 		:normal))
 
 (defun inc-arg-count (form_stack)
@@ -143,82 +175,103 @@
 (defun make-indent (level)
 	(pad "" level "\t"))
 
+(defun last-line-len (s)
+	(if (defq pos (rfind "\n" s))
+		(- (length s) (inc pos))
+		(length s)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;
 ; Code Formatter
 ;;;;;;;;;;;;;;;;;;;;;;;
 
+(defun current-target-indent (form_stack)
+	(if (nempty? form_stack)
+		(last (last form_stack))
+		0))
+
 (defun format-lisp (source)
 	(defq tokens (tokenize-lisp source) len (length tokens)
-		out (string-stream (str-alloc len)) indent_level 0 current_col 0
+		out (string-stream (str-alloc len))
+		cur_line_indent 0 current_col 0
 		at_line_start :t after_lparen :nil after_quote :nil
-		pending_nl :nil pending_comment :nil consec_nl 0
-		form_stack (list) idx 0 tok :nil type :nil val "")
+		pending_nl :nil consec_nl 0
+		form_stack (list) idx 0 tok :nil tok_type :nil val "")
 
 	(while (< idx len)
-		(setq tok (elem-get tokens idx) type (first tok) val (second tok))
+		(setq tok (elem-get tokens idx) tok_type (first tok) val (second tok))
 		(cond
-			((eql type :ws)
+			((eql tok_type :ws)
 				(++ idx))
-			((eql type :nl)
+			((eql tok_type :nl)
 				(setq pending_nl :t)
-				(if (< consec_nl 2) (++ consec_nl))
+				(if (< consec_nl 3) (++ consec_nl))
 				(++ idx))
-			((eql type :comment)
-				(if at_line_start
+			((eql tok_type :raw)
+				(when pending_nl
+					(times consec_nl (write-blk out "\n")))
+				(write-blk out val)
+				(setq at_line_start :t pending_nl :nil
+					consec_nl 0 current_col 0 after_lparen :nil after_quote :nil)
+				(++ idx))
+			((eql tok_type :comment)
+				(if (or at_line_start pending_nl)
 					(progn
-						(if pending_nl
-							(times (min consec_nl 2) (write-blk out "\n")))
-						(write-blk out (make-indent indent_level)))
+						(when pending_nl
+							(times consec_nl (write-blk out "\n")))
+						(defq ind (current-target-indent form_stack))
+						(write-blk out (make-indent ind))
+						(setq cur_line_indent ind current_col (* ind +tab_width)))
 					(write-blk out " "))
 				(write-blk out val)
-				(setq at_line_start :t pending_nl :t pending_comment :t
-					consec_nl 1 current_col 0 after_lparen :nil after_quote :nil)
+				(setq at_line_start :nil pending_nl :nil
+					consec_nl 0 current_col 0 after_lparen :nil after_quote :nil)
 				(++ idx))
-			((eql type :rparen)
-				(setq indent_level (max 0 (dec indent_level)))
-				(when at_line_start
-					(if pending_comment
-						(progn
-							(write-blk out "\n")
-							(write-blk out (make-indent indent_level)))
-						(write-blk out (make-indent indent_level))))
+			((eql tok_type :rparen)
+				(defq closed_frame (if (nempty? form_stack) (pop form_stack) :nil))
+				(when pending_nl
+					(times consec_nl (write-blk out "\n"))
+					(defq r_ind (if closed_frame (second closed_frame) (current-target-indent form_stack)))
+					(write-blk out (make-indent r_ind))
+					(setq at_line_start :t cur_line_indent r_ind
+						current_col (* r_ind +tab_width)))
 				(write-blk out ")")
 				(setq at_line_start :nil after_lparen :nil after_quote :nil
-					pending_nl :nil pending_comment :nil consec_nl 0
+					pending_nl :nil consec_nl 0
 					current_col (+ current_col 1))
-				(when (nempty? form_stack)
-					(pop form_stack)
-					(inc-arg-count form_stack))
+				(inc-arg-count form_stack)
 				(++ idx))
 			(:t
-				; Flush deferred newlines and indent before normal token content
+				; Flush deferred newlines and apply line-relative indentation
 				(when pending_nl
-					(times (min consec_nl 2) (write-blk out "\n"))
-					(write-blk out (make-indent indent_level))
-					(setq at_line_start :t pending_nl :nil pending_comment :nil
-						consec_nl 0 current_col (* indent_level +tab_width)))
+					(times consec_nl (write-blk out "\n"))
+					(defq ind (current-target-indent form_stack))
+					(write-blk out (make-indent ind))
+					(setq at_line_start :t pending_nl :nil
+						cur_line_indent ind
+						consec_nl 0 current_col (* ind +tab_width)))
 
 				(cond
-					((eql type :cscript)
+					((find tok_type '(:cscript :string))
 						(unless (or at_line_start after_lparen after_quote)
 							(write-blk out " ")
 							(++ current_col))
 						(write-blk out val)
 						(setq at_line_start :nil after_lparen :nil after_quote :nil
-							current_col (+ current_col (length val)))
+							current_col (if (find "\n" val) (last-line-len val) (+ current_col (length val))))
 						(inc-arg-count form_stack)
 						(++ idx))
-					((eql type :lparen)
+					((eql tok_type :lparen)
 						(unless (or at_line_start after_lparen after_quote)
 							(write-blk out " ")
 							(++ current_col))
 						(write-blk out "(")
-						(push form_stack (list (form-kind tokens (inc idx)) indent_level 0))
-						(++ indent_level)
+						(defq kind (form-kind tokens (inc idx))
+							child_ind (+ cur_line_indent 1))
+						(push form_stack (list kind cur_line_indent 0 child_ind))
 						(setq at_line_start :nil after_lparen :t after_quote :nil
 							current_col (+ current_col 1))
 						(++ idx))
-					((eql type :quote)
+					((eql tok_type :quote)
 						(unless (or at_line_start after_lparen after_quote)
 							(write-blk out " ")
 							(++ current_col))
@@ -229,7 +282,7 @@
 					(:t
 						; Handle pair wrapping for defq, setq, def, and set
 						(when (and (nempty? form_stack) (not at_line_start))
-							(bind '(kind base_indent arg_count) (last form_stack))
+							(bind '(kind base_indent arg_count &ignore) (last form_stack))
 							(defq is_pair_start (cond
 								((and (eql kind :defq) (>= arg_count 3) (odd? arg_count)) :t)
 								((and (eql kind :def) (>= arg_count 2) (even? arg_count)) :t)
@@ -238,9 +291,11 @@
 								(bind '(& pair_len) (measure-pair tokens idx))
 								(when (> (+ current_col 1 pair_len) +max_line_len)
 									(write-blk out "\n")
-									(write-blk out (make-indent (+ base_indent 1)))
+									(defq p_ind (+ base_indent 1))
+									(write-blk out (make-indent p_ind))
 									(setq at_line_start :t
-										current_col (* (+ base_indent 1) +tab_width)))))
+										cur_line_indent p_ind
+										current_col (* p_ind +tab_width)))))
 
 						(unless (or at_line_start after_lparen after_quote)
 							(write-blk out " ")
@@ -250,6 +305,8 @@
 							current_col (+ current_col (length val)))
 						(inc-arg-count form_stack)
 						(++ idx))))))
+	(when pending_nl
+		(times consec_nl (write-blk out "\n")))
 	(str out))
 
 ;;;;;;;;;;;;;;;;;;;;;;;
@@ -259,35 +316,32 @@
 (defun work (file opt_w opt_c)
 	(when (defq raw (load file))
 		(defq formatted (format-lisp raw))
-		(when (nql raw formatted)
-			(cond
-				(opt_w
+		(cond
+			(opt_w
+				(when (nql raw formatted)
 					(save formatted file)
-					(print "Formatted: " file))
-				(opt_c
-					(print "Needs formatting: " file))
-				(:t
-					(print "--- " file " ---")
-					(print formatted))))))
+					(print "Formatted: " file)))
+			(opt_c
+				(when (nql raw formatted)
+					(print "Needs formatting: " file)))
+			(:t
+				(prin formatted)))))
 
 (defun main ()
 	(when (and
-			(defq stdio (create-stdio))
-			(defq opt_j 8 opt_w :nil opt_c :nil args (options stdio usage)))
+		(defq stdio (create-stdio))
+		(defq opt_j 8 opt_w :nil opt_c :nil args (options stdio usage)))
 		(defq files (rest args))
 		(if (empty? files)
 			(lines! (# (push files %0) :nil) (io-stream 'stdin)))
 		(setq files (usort (filter (lambda (file)
 			(some (# (ends-with %0 file)) +file_types)) files)))
-		(cond
-			((empty? files))
-			((<= (length files) opt_j)
-				(each (# (work %0 opt_w opt_c)) files))
-			(:t
-				(each (lambda ((job result)) (prin result))
-					(pipe-farm (map (# (str (first args)
-							" -j " opt_j
-							(if opt_w " -w" "")
-							(if opt_c " -c" "")
-							" " (slice (str %0) 1 -2)))
-						(partition files opt_j))))))))
+		(if (<= (length files) opt_j)
+			(each (# (work %0 opt_w opt_c)) files)
+			(each (lambda ((job result)) (prin result))
+				(pipe-farm (map (# (str (first args)
+					" -j " opt_j
+					(if opt_w " -w" "")
+					(if opt_c " -c" "")
+					" " (slice (str %0) 1 -2)))
+					(partition files opt_j)))))))
