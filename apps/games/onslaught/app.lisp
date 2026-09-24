38c900626431a7259112754f3ebe8fbb30277be3
Chris Hinsley <chris.hinsley@gmail.com>
2026-09-24 21:57:52 +0100

    starting to get the Field battle together...

--- apps/games/onslaught/app.lisp
+++ apps/games/onslaught/app.lisp
@@ -18,16 +18,18 @@
 (import "./widgets.inc")
 (import "./actions.inc")
 (import "./title.inc")
+(import "./field.inc")
 
 (defq
-	*running* :t)
+	*running* :t
+	*game_state* :title)
 
 (defun dispatch-action (&rest action)
 	(catch (eval action) (progn (prin _) (print) :t)))
 
 (defun main ()
 	(defq select (task-mboxes +select_size))
-	(setq *running* :t)
+	(setq *running* :t *game_state* :title)
 
 	; Load panel canvas directly and position at bottom (0 168 320 72)
 	(when (defq panel (canvas-load (cat *app_root* "image/panel.cpm") +load_flag_shared))
@@ -44,7 +46,6 @@
 
 	; Start 30 FPS game loop timer
 	(mail-timeout (elem-get select +select_timer) +rate 0)
-
 	; Main event loop
 	(while *running*
 		(defq *msg* (mail-read (elem-get select (defq idx (mail-select select)))))
@@ -52,13 +53,42 @@
 			(+select_main
 				; Dispatch UI events (close, min, max, clicks, keys)
 				(cond
+					((and (or (= (getf *msg* +ev_msg_type) +ev_type_key_down)
+							  (= (getf *msg* +ev_msg_type) +ev_type_key_up))
+						  (not (Textfield? (. *window* :find_id (getf *msg* +ev_msg_target_id)))))
+						(defq
+							type (getf *msg* +ev_msg_type)
+							key (getf *msg* +ev_msg_key_key)
+							kmask :nil)
+						(cond
+							((or (= key (ascii-code "q")) (= key (ascii-code "Q")))
+								(setq kmask +fkey_up))
+							((or (= key (ascii-code "a")) (= key (ascii-code "A")))
+								(setq kmask +fkey_down))
+							((or (= key (ascii-code "o")) (= key (ascii-code "O")))
+								(setq kmask +fkey_left))
+							((or (= key (ascii-code "p")) (= key (ascii-code "P")))
+								(setq kmask +fkey_right))
+							((= key (ascii-code " "))
+								(setq kmask +fkey_keya)))
+						(when kmask
+							(if (= type +ev_type_key_down)
+								(setq *game_controls* (logior *game_controls* kmask))
+								(setq *game_controls* (logand *game_controls* (lognot kmask)))))
+						:t)
 					((. *window* :dispatch *msg*))
 					((. *window* :event *msg*))))
 			(+select_timer
 				; Re-arm 30 FPS timer
 				(mail-timeout (elem-get select +select_timer) +rate 0)
-				; Advance title animation frame
-				(title-sequence-update *playfield* +dt))))
+				(case *game_state*
+					(:title
+						(title-sequence-update *playfield* +dt)
+						(when (eql *title_state* :done)
+							(setq *game_state* :field)
+							(field-sequence-start *playfield* 1)))
+					(:field
+						(field-sequence-update *playfield* +dt))))))
 
 	; Unregister window and exit cleanly
 	(gui-sub-rpc *window*))
